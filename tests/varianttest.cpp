da534e4e401c60fe0502844e7e6f36cbf0a275e5
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2011-11-13 15:30:07 -0500

    Added COPYING file, and copyright headers.
    All source is licensed under the New BSD license (3-clause).
    
    Change-Id: I1667c0f8f51768f9c4456f4226605f86a6aa031c

--- tests/varianttest.cpp
+++ tests/varianttest.cpp
@@ -1,3 +1,19 @@
+/******************************************************************************
+
+  This source file is part of the MolCore project.
+
+  Copyright 2011 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
 #include <gtest/gtest.h>
 
 #include <variant.h>
