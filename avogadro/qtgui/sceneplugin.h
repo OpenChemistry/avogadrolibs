70828bc9e53845269c3eddcf8fbf175c94731fd8
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2012-06-02 20:06:38 -0400

    Adding plugin support to Avogadro
    Adds a plugin manager to coordinate loading plugins, some initial API
    for the factories that create them.
    
    Change-Id: I487d55486e3ed4858fc3cd3499b9e81ddf9f5391

---
+++ avogadro/qtgui/sceneplugin.h
@@ -0,0 +1,104 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2012 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#ifndef AVOGADRO_QTGUI_SCENEPLUGIN_H
+#define AVOGADRO_QTGUI_SCENEPLUGIN_H
+
+#include "avogadroqtguiexport.h"
+
+#include <QtCore/QObject>
+
+namespace Avogadro {
+
+namespace Core {
+class Molecule;
+}
+
+namespace Rendering {
+class Scene;
+}
+
+namespace QtGui {
+
+/*!
+ * \class ScenePluginFactory sceneplugin.h <avogadro/qtgui/sceneplugin.h>
+ * \brief The base class for scene plugin factories in Avogadro.
+ * \author Marcus D. Hanwell
+ */
+class AVOGADROQTGUI_EXPORT ScenePlugin : public QObject
+{
+  Q_OBJECT
+
+public:
+  explicit ScenePlugin(QObject *parent = 0);
+  ~ScenePlugin();
+
+  /*!
+   * Process the supplied atom, and add the necessary primitives to the scene.
+   */
+  virtual void process(const Core::Molecule &molecule, Rendering::Scene &scene) = 0;
+
+  /*!
+   * The name of the scene plugin, will be displayed in the user interface.
+   */
+  virtual QString name() const = 0;
+
+  /*!
+   * A description of the scene plugin, may be displayed in the user interface.
+   */
+  virtual QString description() const = 0;
+
+  /*!
+   * Returns true if the scene plugin has been enabled and is active.
+   */
+  virtual bool isEnabled() const = 0;
+
+  /*!
+   * Set the enabled state of the plugin (default should be false).
+   */
+  virtual void setEnabled(bool enable) = 0;
+};
+
+/*!
+ * \class ScenePluginFactory sceneplugin.h <avogadro/qtgui/sceneplugin.h>
+ * \brief The base class for scene plugin factories in Avogadro.
+ * \author Marcus D. Hanwell
+ */
+class AVOGADROQTGUI_EXPORT ScenePluginFactory
+{
+public:
+  virtual ~ScenePluginFactory();
+
+  virtual ScenePlugin * createSceneInstance() = 0;
+  virtual QString identifier() const = 0;
+};
+
+#define SCENE_PLUGIN_FACTORY(className, id) \
+public: \
+  explicit className##Factory(QObject *parent_ = 0) : QObject(parent_) {} \
+  Avogadro::QtGui::ScenePlugin * createSceneInstance() \
+  { \
+    return new className; \
+  } \
+  QString identifier() const { return id; }
+
+} // End QtGui namespace
+} // End Avogadro namespace
+
+Q_DECLARE_INTERFACE(Avogadro::QtGui::ScenePluginFactory,
+                    "net.openchemistry.avogadro.scenepluginfactory/2.0")
+
+#endif // AVOGADRO_QTGUI_SCENEPLUGIN_H
---
+++ avogadro/qtplugins/vanderwaals.cpp
@@ -0,0 +1,62 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2012 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#include "vanderwaals.h"
+
+#include <avogadro/core/molecule.h>
+#include <avogadro/core/elements.h>
+#include <avogadro/rendering/scene.h>
+
+#include <QtCore/QtPlugin>
+
+namespace Avogadro {
+namespace QtPlugins {
+
+VanDerWaals::VanDerWaals(QObject *p) : ScenePlugin(p), m_enabled(false)
+{
+}
+
+VanDerWaals::~VanDerWaals()
+{
+}
+
+void VanDerWaals::process(const Core::Molecule &molecule,
+                          Rendering::Scene &scene)
+{
+  for (size_t i = 0; i < molecule.atomCount(); ++i) {
+    Core::Atom atom = molecule.atom(i);
+    unsigned char atomicNumber = atom.atomicNumber();
+    const unsigned char *c = Core::Elements::color(atomicNumber);
+    Vector3ub color(c[0], c[1], c[2]);
+    scene.addSphere(atom.position3d().cast<float>(), color,
+                    static_cast<float>(Core::Elements::radiusVDW(atomicNumber)));
+  }
+}
+
+bool VanDerWaals::isEnabled() const
+{
+  return m_enabled;
+}
+
+void VanDerWaals::setEnabled(bool enable)
+{
+  m_enabled = enable;
+}
+
+}
+}
+
+Q_EXPORT_PLUGIN2(vanderwaals, Avogadro::QtPlugins::VanDerWaalsFactory)
