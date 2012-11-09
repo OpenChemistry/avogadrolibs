1cc79e86974e77a9530e6cb2e1cf8a74ee5a04d5
David C. Lonie <david.lonie@kitware.com>
2012-11-09 15:52:58 -0500

    Add ball and stick style.
    No sticks yet, need some changes in Scene before cylinders can be
    rendered.
    
    Change-Id: I32b6e41a01b6d9cff3e9882ded6fc8931489cee4

---
+++ avogadro/qtplugins/ballandstick.cpp
@@ -0,0 +1,78 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2012 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#include "ballandstick.h"
+
+#include <avogadro/core/molecule.h>
+#include <avogadro/core/elements.h>
+#include <avogadro/rendering/scene.h>
+
+#include <QtCore/QtPlugin>
+
+using Avogadro::Core::Molecule;
+using Avogadro::Rendering::Scene;
+
+namespace Avogadro {
+namespace QtPlugins {
+
+BallAndStick::BallAndStick(QObject *p) : ScenePlugin(p), m_enabled(true)
+{
+}
+
+BallAndStick::~BallAndStick()
+{
+}
+
+void BallAndStick::process(const Molecule &molecule, Scene &scene)
+{
+  for (size_t i = 0; i < molecule.atomCount(); ++i) {
+    Core::Atom atom = molecule.atom(i);
+    unsigned char atomicNumber = atom.atomicNumber();
+    const unsigned char *c = Core::Elements::color(atomicNumber);
+    Vector3ub color(c[0], c[1], c[2]);
+    scene.addSphere(atom.position3d().cast<float>(), color,
+                    static_cast<float>(Core::Elements::radiusVDW(
+                                         atomicNumber) * 0.3));
+  }
+
+  float bondRadius;
+  Vector3ub bondColor(0.5, 0.5, 0.5);
+  for (size_t i = 0; i < molecule.bondCount(); ++i) {
+    Core::Bond bond = molecule.bond(i);
+    /// @todo multicylinders for bond orders
+    Vector3f pos1 = bond.atom1().position3d().cast<float>();
+    Vector3f pos2 = bond.atom2().position3d().cast<float>();
+    Vector3f bondVector = pos2 - pos1;
+    float bondLength = bondVector.norm();
+//    scene.addCylinder(pos1, bondVector.normalized(), bondLength, color,
+//                      bondRadius);
+  }
+}
+
+bool BallAndStick::isEnabled() const
+{
+  return m_enabled;
+}
+
+void BallAndStick::setEnabled(bool enable)
+{
+  m_enabled = enable;
+}
+
+}
+}
+
+Q_EXPORT_PLUGIN2(ballandstick, Avogadro::QtPlugins::BallAndStickFactory)
