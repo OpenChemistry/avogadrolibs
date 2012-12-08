ec0c7c820e67aa3a206f860c10fd12c6abcb2090
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2012-12-08 13:30:45 -0500

    Ported the periodic table view from Avogadro 1.x
    This brings across the QGraphicsView periodic table view, along with
    the support classes for items, detail etc. Ported the code to use the
    new Avogadro::Core::Elements class for atom data.
    
    Change-Id: I7b0d6f0b32408c08a271252928a479d409be035b

---
+++ avogadro/qtgui/elementtranslator.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2007 Geoffrey R. Hutchison
+  Copyright 2012 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#ifndef AVOGADRO_QTGUI_ELEMENTTRANSLATOR_H
+#define AVOGADRO_QTGUI_ELEMENTTRANSLATOR_H
+
+#include "avogadroqtguiexport.h"
+#include <QtCore/QObject>
+
+namespace Avogadro {
+namespace QtGui {
+
+/**
+ * @class ElementTranslator elementtranslator.h <avogadro/qtgui/elementtranslator.h>
+ * @brief Internationalization of element names.
+ * @author Geoff Hutchison
+ *
+ * This class provides for translation of element names: e.g., tr("Carbon")
+ * To use the global translator:
+ * @code
+ * #include <avogadro/elementtranslator.h>
+ * ...
+ * Avogadro::elementTranslator.name(6); // Return "carbon" in English
+ * @endcode
+ */
+class AVOGADROQTGUI_EXPORT ElementTranslator : public QObject
+{
+  Q_OBJECT
+
+public:
+  ElementTranslator();
+
+  /**
+   * Translate element names.
+   * @param element The atomic number of the element to be translated.
+   * @return a QString with the appropriate translation for the current locale.
+   */
+  static QString name(int element);
+};
+
+} // End namespace QtGui
+} // End namespace Avogadro
+
+#endif
---
+++ avogadro/qtgui/periodictableview.cpp
@@ -0,0 +1,103 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2007-2009 by Marcus D. Hanwell
+  Copyright 2012 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#include "periodictableview.h"
+#include "periodictablescene_p.h"
+#include <avogadro/core/elements.h>
+
+#include <QtCore/QTimer>
+#include <QtGui/QKeyEvent>
+
+namespace Avogadro {
+namespace QtGui {
+
+using Core::Elements;
+
+PeriodicTableView::PeriodicTableView(QWidget *parent) : QGraphicsView(parent)
+{
+  // Use a small title bar (Qt::Tool) with no minimize or maximise buttons
+  setWindowFlags(Qt::Dialog | Qt::Tool);
+
+  PeriodicTableScene *table = new PeriodicTableScene;
+  table->setSceneRect(-20, -20, 480, 260);
+  table->setItemIndexMethod(QGraphicsScene::NoIndex);
+  table->setBackgroundBrush(Qt::white);
+
+  setScene(table);
+  setRenderHint(QPainter::Antialiasing);
+  setWindowTitle(tr("Periodic Table"));
+  resize(490, 270);
+  setFixedSize(490, 270);
+  connect(table, SIGNAL(elementChanged(int)),
+          this, SLOT(elementClicked(int)));
+}
+
+PeriodicTableView::~PeriodicTableView()
+{
+  delete scene();
+}
+
+void PeriodicTableView::elementClicked(int id)
+{
+  emit(elementChanged(id));
+}
+
+void PeriodicTableView::mouseDoubleClickEvent(QMouseEvent *)
+{
+  close();
+}
+
+void PeriodicTableView::clearKeyPressBuffer()
+{
+  m_keyPressBuffer.clear();
+}
+
+void PeriodicTableView::keyPressEvent(QKeyEvent *event)
+{
+  if (m_keyPressBuffer.isEmpty()) {
+    // This is the first character typed.
+    // Qait for 2 seconds, then clear the buffer,
+    // this ensures we can get multi-character elements.
+    QTimer::singleShot(2000, this, SLOT(clearKeyPressBuffer()));
+  }
+
+  m_keyPressBuffer.append(event->text());
+  // Try setting an element symbol from this string.
+  int element = m_keyPressBuffer.toInt();
+  if (element <= 0 || element > 119) {
+    // Not a valid number, have we tried 2- and 3-character symbols?
+    if (m_keyPressBuffer.length() > 3) {
+      clearKeyPressBuffer();
+    }
+    else {
+      // try parsing as a symbol
+      element = Elements::atomicNumberFromSymbol(m_keyPressBuffer.toAscii().data());
+    }
+  }
+
+  if (element > 0 && element < 119) { // got a valid symbol
+    // Notify the scene
+    PeriodicTableScene *table = qobject_cast<PeriodicTableScene *>(scene());
+    if (table)
+      table->changeElement(element);
+  }
+
+  QGraphicsView::keyPressEvent(event);
+}
+
+} // End QtGui namespace
+} // End Avogadro namespace
