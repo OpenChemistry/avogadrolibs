1afc83945e3c82fe0bc75492d42e4025c20ff7de
David C. Lonie <david.lonie@kitware.com>
2012-12-20 13:13:04 -0500

    Add a molecular properties extension.
    Change-Id: I03102d454aebaaed239b33ea776d799feed3e02d

---
+++ avogadro/qtplugins/molecularproperties/molecularpropertiesdialog.h
@@ -0,0 +1,69 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2012 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#ifndef AVOGADRO_QTGUI_MOLECULARPROPERTIESDIALOG_H
+#define AVOGADRO_QTGUI_MOLECULARPROPERTIESDIALOG_H
+
+#include <QtGui/QDialog>
+
+namespace Avogadro {
+
+namespace Core {
+class Molecule;
+}
+
+namespace QtPlugins {
+
+namespace Ui {
+class MolecularPropertiesDialog;
+}
+
+/**
+ * @class MolecularPropertiesDialog molecularpropertiesdialog.h <avogadrolibs/qtgui/molecularpropertiesdialog.h>
+ * @brief The MolecularPropertiesDialog class provides a dialog which displays
+ * basic molecular properties.
+ * @author David C. Lonie
+ *
+ * @todo This class will need to be updated with the QObject molecule: connect
+ * destroyed() to moleculeDestroyed(), and moleculeChanged() to updateLabels().
+ * @todo IUPAC name fetch (need inchi key).
+ */
+class MolecularPropertiesDialog : public QDialog
+{
+  Q_OBJECT
+
+public:
+  explicit MolecularPropertiesDialog(Core::Molecule *mol, QWidget *parent_ = 0);
+  ~MolecularPropertiesDialog();
+
+  Core::Molecule* molecule() { return m_molecule; }
+
+public slots:
+  void setMolecule(Core::Molecule *mol);
+
+private slots:
+  void updateLabels();
+  void moleculeDestroyed();
+
+private:
+  Core::Molecule *m_molecule;
+  Ui::MolecularPropertiesDialog *m_ui;
+};
+
+
+} // namespace QtPlugins
+} // namespace Avogadro
+#endif // AVOGADRO_QTGUI_MOLECULARPROPERTIESDIALOG_H
