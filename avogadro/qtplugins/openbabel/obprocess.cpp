d0a61af96275b9fa350bc3d010a4548a10cc2bbe
David C. Lonie <david.lonie@kitware.com>
2013-01-30 14:53:37 -0500

    Add openbabel file reader extension.
    Change-Id: I6c7fb59d761d95e11abca47b4e432729eba3a94e

---
+++ avogadro/qtplugins/openbabel/obprocess.cpp
@@ -0,0 +1,157 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2013 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#include "obprocess.h"
+
+#include <QtCore/QDebug>
+#include <QtCore/QFileInfo>
+#include <QtCore/QProcess>
+
+namespace Avogadro {
+namespace QtPlugins {
+
+OBProcess::OBProcess(QObject *parent_) :
+  QObject(parent_),
+  m_processLocked(false),
+  m_aborted(false),
+  m_process(new QProcess(this)),
+  m_obabelExecutable("obabel")
+{
+  // Read the AVO_OBABEL_EXECUTABLE env var to optionally override the
+  // executable used for obabel.
+  QByteArray obabelExec = qgetenv("AVO_OBABEL_EXECUTABLE");
+  if (!obabelExec.isEmpty())
+    m_obabelExecutable = obabelExec;
+}
+
+void OBProcess::abort()
+{
+  m_aborted = true;
+  emit aborted();
+}
+
+bool OBProcess::queryReadFormats()
+{
+  if (!tryLockProcess()) {
+    qWarning() << "OBProcess::queryReadFormats: process already in use.";
+    return false;
+  }
+
+  // Setup options
+  QStringList options;
+  options << "-L" << "formats" << "read";
+
+  executeObabel(options, this, SLOT(queryReadFormatsPrepare()));
+  return true;
+}
+
+void OBProcess::queryReadFormatsPrepare()
+{
+  if (m_aborted) {
+    releaseProcess();
+    return;
+  }
+
+  QMap<QString, QString> result;
+
+  QString output = QString::fromLatin1(m_process->readAllStandardOutput());
+
+  QRegExp parser("\\s*([^\\s]+)\\s+--\\s+([^\\n]+)\\n");
+  int pos = 0;
+  while ((pos = parser.indexIn(output, pos)) != -1) {
+    QString extension = parser.cap(1);
+    QString description = parser.cap(2);
+    result.insertMulti(description, extension);
+    pos += parser.matchedLength();
+  }
+
+  releaseProcess();
+  emit queryReadFormatsFinished(result);
+  return;
+}
+
+bool OBProcess::readFile(const QString &filename,
+                         const QString &outputFormat,
+                         const QString &inputFormatOverride)
+{
+  if (!tryLockProcess()) {
+    qWarning() << "OBProcess::readFile: process already in use.";
+    return false;
+  }
+
+  QStringList options;
+
+  // Setup input options
+  options << QString("-i%1").arg(!inputFormatOverride.isEmpty()
+                                 ? inputFormatOverride
+                                 : QFileInfo(filename).suffix());
+  options << filename;
+
+  // Setup output options
+  options << QString("-o%1").arg(outputFormat);
+
+  executeObabel(options, this, SLOT(readFilePrepareOutput()));
+  return true;
+}
+
+void OBProcess::readFilePrepareOutput()
+{
+  if (m_aborted) {
+    releaseProcess();
+    return;
+  }
+
+  // Keep this empty if an error occurs:
+  QByteArray output;
+
+  // Check for errors.
+  QString errorOutput = QString::fromLatin1(m_process->readAllStandardError());
+  QRegExp errorChecker("\\b0 molecules converted\\b" "|"
+                       "obabel: cannot read input format!");
+  if (!errorOutput.contains(errorChecker)) {
+    if (m_process->exitStatus() == QProcess::NormalExit)
+      output = m_process->readAllStandardOutput();
+  }
+
+  /// Print any meaningful warnings @todo This should go to a log at some point.
+  if (!errorOutput.isEmpty() && errorOutput != "1 molecule converted\n")
+    qDebug() << m_obabelExecutable << " stderr:\n" << errorOutput;
+
+  emit readFileFinished(output);
+  releaseProcess();
+}
+
+void OBProcess::executeObabel(const QStringList &options,
+                              QObject *receiver, const char *slot)
+{
+  // Setup exit handler
+  connect(m_process, SIGNAL(finished(int)), receiver, slot);
+  connect(m_process, SIGNAL(error(QProcess::ProcessError)), receiver, slot);
+
+  // Start process
+  m_process->start(m_obabelExecutable, options);
+}
+
+void OBProcess::resetState()
+{
+  m_aborted = false;
+  m_process->disconnect(this);
+  disconnect(m_process);
+  connect(this, SIGNAL(aborted()), m_process, SLOT(kill()));
+}
+
+} // namespace QtPlugins
+} // namespace Avogadro
---
+++ avogadro/qtplugins/openbabel/obprocess.h
@@ -0,0 +1,210 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2013 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#ifndef AVOGADRO_QTPLUGINS_OBPROCESS_H
+#define AVOGADRO_QTPLUGINS_OBPROCESS_H
+
+#include <QtCore/QMap>
+#include <QtCore/QObject>
+
+class QProcess;
+
+namespace Avogadro {
+namespace QtPlugins {
+
+/**
+ * @brief The OBProcess class provides an interface to the `obabel` executable,
+ * which is run in a separate process.
+ *
+ * The `obabel` executable used by this class can be overridden by setting the
+ * AVO_OBABEL_EXECUTABLE environment variable.
+ */
+class OBProcess : public QObject
+{
+  Q_OBJECT
+public:
+  explicit OBProcess(QObject *parent_ = 0);
+
+  /**
+   * @name Process Management
+   * Methods, slots, and signals used to interact with the OpenBabel process.
+   * @{
+   */
+public:
+  /**
+   * The `obabel` executable used by the process.
+   */
+  QString obabelExecutable() const { return m_obabelExecutable; }
+
+  /**
+   * @return True if the process is in use, false otherwise.
+   */
+  bool inUse() const { return m_processLocked; }
+
+public slots:
+  /**
+   * Abort any currently running processes.
+   *
+   * This will cause aborted() to be emitted, but not any of the
+   * operation-specific "finished" signals.
+   */
+  void abort();
+
+signals:
+  /**
+   * Emitted when the abort() method has been called.
+   */
+  void aborted();
+
+  // end Process Management doxygen group
+  /**@}*/
+
+  /**
+   * @name File Format Support
+   * Query the obabel executable for supported file formats.
+   * @{
+   */
+public slots:
+  /**
+   * Request a list of all supported input formats from obabel.
+   *
+   * After calling this method, the queryReadFormatsFinished signal will be
+   * emitted. This method executes
+   *
+   * `obabel -L formats read`
+   *
+   * and parses the output into a map (keys are format descriptions, values are
+   * format extensions).
+   *
+   * If an error occurs, queryReadFormatsFinished will be emitted with an empty
+   * argument.
+   *
+   * @return True if the process started successfully, false otherwise.
+   */
+  bool queryReadFormats();
+
+signals:
+  /**
+   * Triggered when the process started by queryReadFormats() completes.
+   * @param readFormats The input file formats that OpenBabel understands. Keys
+   * are non-translated (english), human-readable descriptions of the formats,
+   * and the values are the corresponding file extensions.
+   *
+   * @note readFormats will usually contain more than one extensions per format,
+   * so accessing the values with QMap::values() (instead of QMap::value()) is
+   * required.
+   *
+   * If an error occurs, readFormats will be empty.
+   */
+  void queryReadFormatsFinished(QMap<QString, QString> readFormats);
+
+private slots:
+  void queryReadFormatsPrepare();
+
+  // end File Format Support doxygen group
+  /**@}*/
+
+  /**
+   * @name File Reading
+   * Used to open a file with obabel and return a CML representation
+   * of the molecule.
+   * @{
+   */
+public slots:
+  /**
+   * Request that obabel read a file from disk.
+   * @param filename The output file to read.
+   * @param outputFormat The format used to represent the molecule. Default: cml
+   * @param inputFormatOverride Optional override to the input file's format.
+   * If not specified, the format is guessed from @a filename's extension.
+   *
+   * After calling this method, the readFileFinished signal will be emitted to
+   * indicate return status along with the requested representation of the
+   * molecule, or an error message.
+   *
+   * The conversion is performed as:
+   * `obabel -i<filename extension> <filename> -o<outputFormat>`
+   *
+   * The standard output is recorded and returned by readFileFinished.
+   *
+   * @return True if the process started successfully, false otherwise.
+   */
+  bool readFile(const QString &filename, const QString &outputFormat = "cml",
+                const QString &inputFormatOverride = QString());
+
+signals:
+  /**
+   * Emitted after a call to readFile.
+   * @param output The molecule in the requested format, or an empty QByteArray
+   * if an error occurred.
+   */
+  void readFileFinished(const QByteArray &output);
+
+private slots:
+  void readFilePrepareOutput();
+
+  // end File Reading doxygen group
+  /**@}*/
+
+private:
+  /**
+   * Internal method for launching the obabel executable.
+   * @param options List of options to pass to QProcess::start
+   * @param receiver A QObject subclass instance that has @a slot as a member.
+   * @param slot The slot to call when completed. Must have no arguments.
+   *
+   * Call this method like so:
+@code
+QStringList options;
+<Populate options>
+executeObabel(options, this, SLOT(mySlot()));
+@endcode
+   *
+   * @a slot will be connected to QProcess::finished(int) and
+   * QProcess::error(QProcess::ProcessError) with @a receiver as receiver and
+   * @a m_process as sender. @a m_process is then started using
+   * m_obabelExecutable and options as arguments.
+   */
+  void executeObabel(const QStringList &options, QObject *receiver,
+                     const char *slot);
+
+  void resetState();
+
+  // Not thread safe -- just uses a bool.
+  bool tryLockProcess()
+  {
+    if (m_processLocked)
+      return false;
+    m_processLocked = true;
+    resetState();
+    return true;
+  }
+
+  void releaseProcess()
+  {
+    m_processLocked = false;
+  }
+
+  bool m_processLocked;
+  bool m_aborted;
+  QProcess *m_process;
+  QString m_obabelExecutable;
+};
+
+} // namespace QtPlugins
+} // namespace Avogadro
+
+#endif // AVOGADRO_QTPLUGINS_OBPROCESS_H
