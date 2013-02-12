25d230c34220720ea951c4e1a8fe798c1414d532
David C. Lonie <david.lonie@kitware.com>
2013-02-12 13:46:09 -0500

    Add an XYZ format reader/writer.
    I'll need this for obabel bond perception.
    
    The new test will fail without the new methane.xyz file
    in avogadrodata.
    
    Change-Id: Ib44686aa475e61210b4a456067e4e8a6510fc0a3

---
+++ tests/io/xyztest.cpp
@@ -0,0 +1,78 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2013 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#include "iotests.h"
+
+#include <gtest/gtest.h>
+
+#include <avogadro/core/atom.h>
+#include <avogadro/core/molecule.h>
+#include <avogadro/core/vector.h>
+
+#include <avogadro/io/xyzformat.h>
+
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using Avogadro::Core::Atom;
+using Avogadro::Core::Molecule;
+using Avogadro::Io::XyzFormat;
+using Avogadro::Vector3;
+
+TEST(XyzTest, read)
+{
+  XyzFormat xyz;
+  Molecule molecule;
+  xyz.readFile(AVOGADRO_DATA "/data/methane.xyz", molecule);
+
+  EXPECT_EQ(molecule.atomCount(), 5);
+  EXPECT_EQ(molecule.bondCount(), 0);
+
+  EXPECT_EQ(molecule.atom(0).atomicNumber(), 6);
+  EXPECT_EQ(molecule.atom(1).atomicNumber(), 1);
+  EXPECT_EQ(molecule.atom(2).atomicNumber(), 1);
+  EXPECT_EQ(molecule.atom(3).atomicNumber(), 1);
+  EXPECT_EQ(molecule.atom(4).atomicNumber(), 1);
+
+  EXPECT_EQ(molecule.atom(4).position3d().x(), -0.51336);
+  EXPECT_EQ(molecule.atom(4).position3d().y(),  0.88916);
+  EXPECT_EQ(molecule.atom(4).position3d().z(), -0.36300);
+}
+
+TEST(XyzTest, write)
+{
+  XyzFormat xyz;
+  Molecule molecule;
+
+  molecule.addAtom(6).setPosition3d(Vector3(0.0, 0.0, 0.0));
+  molecule.addAtom(1).setPosition3d(Vector3(0.0, 0.0, 1.089));
+  molecule.addAtom(1).setPosition3d(Vector3(1.026719, 0.0, -0.363));
+  molecule.addAtom(1).setPosition3d(Vector3(-0.51336, -0.889165, -0.363));
+  molecule.addAtom(1).setPosition3d(Vector3(-0.51336, 0.889165, -0.363));
+  std::string output;
+  EXPECT_EQ(xyz.writeString(output, molecule), true);
+
+  // The output should be an exact match with the sample file.
+  std::istringstream outputStream(output);
+  std::ifstream refStream(AVOGADRO_DATA "/data/methane.xyz");
+  char outputChar = '\0';
+  char refChar = '\0';
+  outputStream >> std::noskipws;
+  refStream >> std::noskipws;
+  while ((outputStream >> outputChar) && (refStream >> refChar))
+    ASSERT_EQ(refChar, outputChar);
+}
