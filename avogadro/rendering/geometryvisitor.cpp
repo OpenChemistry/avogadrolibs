519b6bf79c00da8788420a0506245c0bfc6ea7cb
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2013-03-25 16:52:28 -0400

    Make the geometry visitor accumulate geometry
    Attempt to accumulate geometry, this has received limited testing (and
    more should be added to ensure the averaging is sensible) but it seems
    to work with a large range of the molecules I tested it with.
    
    Change-Id: Ie065a9710b314f711606c9a8b292ea21abf49cfd

--- avogadro/rendering/geometryvisitor.cpp
+++ avogadro/rendering/geometryvisitor.cpp
@@ -21,7 +21,8 @@
 namespace Avogadro {
 namespace Rendering {
 
-GeometryVisitor::GeometryVisitor() : m_center(Vector3f::Zero()), m_radius(5)
+GeometryVisitor::GeometryVisitor() : m_center(Vector3f::Zero()), m_radius(0.0f),
+  m_dirty(false)
 {
 }
 
@@ -36,26 +37,80 @@ void GeometryVisitor::visit(Drawable &)
 void GeometryVisitor::visit(SphereGeometry &geometry)
 {
   const std::vector<SphereColor> &spheres = geometry.spheres();
-  m_center = Vector3f::Zero();
   if (!spheres.size())
     return;
 
+  m_dirty = true;
+
+  Vector3f tmpCenter(Vector3f::Zero());
   // First find the center of the sphere geometry.
   std::vector<SphereColor>::const_iterator it = spheres.begin();
   for (; it != spheres.end(); ++it)
-    m_center += it->center;
-  m_center /= spheres.size();
+    tmpCenter += it->center;
+  tmpCenter /= spheres.size();
 
   // Now find its radius.
-  m_radius = 2.0f; // Minumim radius to be used.
-  if (spheres.size() < 2)
-    return;
-  for (it = spheres.begin(); it != spheres.end(); ++it) {
-    float distance = (it->center - m_center).squaredNorm();
-    if (distance > m_radius)
-      m_radius = distance;
+  float tmpRadius(0.0f);
+  if (spheres.size() > 1) {
+    for (it = spheres.begin(); it != spheres.end(); ++it) {
+      float distance = (it->center - tmpCenter).squaredNorm();
+      if (distance > tmpRadius)
+        tmpRadius = distance;
+    }
   }
-  m_radius = sqrt(m_radius);
+  tmpRadius = sqrt(tmpRadius);
+  m_centers.push_back(tmpCenter);
+  m_radii.push_back(tmpRadius);
+}
+
+void GeometryVisitor::clear()
+{
+  m_center = Vector3f::Zero();
+  m_radius = 0.0f;
+  m_dirty = false;
+  m_centers.clear();
+  m_radii.clear();
+}
+
+Vector3f GeometryVisitor::center()
+{
+  average();
+  return m_center;
+}
+
+float GeometryVisitor::radius()
+{
+  average();
+  return m_radius;
+}
+
+void GeometryVisitor::average()
+{
+  if (!m_dirty)
+    return;
+
+  // Find the average position of the center, then the minimal enclosing radius.
+  m_dirty = false;
+  if (m_centers.size() == 1) {
+    m_center = m_centers[0];
+    m_radius = m_radii[0];
+  }
+  else {
+    m_center = Vector3f::Zero();
+    std::vector<Vector3f>::const_iterator cit;
+    for (cit = m_centers.begin(); cit != m_centers.end(); ++cit)
+      m_center += *cit;
+    m_center /= m_centers.size();
+    // Now find the smallest enclosing radius for the new center.
+    m_radius = 0.0f;
+    std::vector<float>::const_iterator rit;
+    for (cit = m_centers.begin(), rit = m_radii.begin();
+         cit != m_centers.end(), rit != m_radii.end(); ++cit, ++rit) {
+      float distance = (m_center - (*cit)).norm() + (*rit);
+      if (distance > m_radius)
+        m_radius = distance;
+    }
+  }
 }
 
 } // End namespace Rendering
