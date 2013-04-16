31da22295ad8290e2a50ce8e4b95c04399e3d587
David C. Lonie <david.lonie@kitware.com>
2013-04-16 11:14:48 -0400

    Add an extension with Fixup/Remove Hydrogens menu items.
    Change-Id: I6750d99604e1b2a84bc7541c743c44bfd33ea24b

---
+++ avogadro/qtplugins/hydrogens/hydrogens.h
@@ -0,0 +1,57 @@
+/******************************************************************************
+
+  This source file is part of the MoleQueue project.
+
+  Copyright 2013 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#ifndef AVOGADRO_QTPLUGINS_HYDROGENS_H
+#define AVOGADRO_QTPLUGINS_HYDROGENS_H
+
+#include <avogadro/qtgui/extensionplugin.h>
+
+namespace Avogadro {
+namespace QtPlugins {
+
+/**
+ * @brief The Hydrogens class is an extension to modify hydrogens.
+ */
+class Hydrogens : public Avogadro::QtGui::ExtensionPlugin
+{
+  Q_OBJECT
+public:
+  explicit Hydrogens(QObject *parent_ = 0);
+  ~Hydrogens();
+
+  QString name() const { return tr("Hydrogens"); }
+  QString description() const;
+  QList<QAction*> actions() const;
+  QStringList menuPath(QAction *) const;
+
+public slots:
+  void setMolecule(QtGui::Molecule *mol);
+
+private slots:
+  void adjustHydrogens();
+  void addHydrogens();
+  void removeHydrogens();
+  void removeAllHydrogens();
+
+private:
+  QList<QAction *> m_actions;
+  QtGui::Molecule *m_molecule;
+};
+
+} // namespace QtPlugins
+} // namespace Avogadro
+
+#endif // AVOGADRO_QTPLUGINS_HYDROGENS_H
