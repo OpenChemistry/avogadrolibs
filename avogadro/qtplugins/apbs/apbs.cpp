c39cdf8bdd1b7bcae7bdb96bf24a921b2f899da7
Kyle Lutz <kyle.lutz@kitware.com>
2013-05-24 09:55:51 -0400

    Add APBS output file reader
    This adds a new extension for reading output files (in the OpenDX
    format) from APBS.
    
    Change-Id: I91b15a5d1a8145679c69a71307c7ce53b08c26f4

---
+++ avogadro/qtplugins/apbs/apbs.cpp
@@ -0,0 +1,137 @@
+
+#include "apbs.h"
+#include "opendxreader.h"
+
+#include <avogadro/qtgui/cube.h>
+#include <avogadro/qtgui/mesh.h>
+#include <avogadro/qtgui/molecule.h>
+#include <avogadro/qtgui/meshgenerator.h>
+
+#include <QAction>
+#include <QFileDialog>
+#include <QProgressDialog>
+#include <QMessageBox>
+#include <QApplication>
+
+namespace Avogadro {
+namespace QtPlugins {
+
+Apbs::Apbs(QObject *parent_)
+  : QtGui::ExtensionPlugin(parent_),
+    m_molecule(0)
+{
+  QAction *action = new QAction(this);
+  action->setText(tr("Open Output File"));
+  connect(action, SIGNAL(triggered()), this, SLOT(onOpenOutputFile()));
+  m_actions.append(action);
+
+  m_progressDialog = new QProgressDialog(qobject_cast<QWidget*>(parent_));
+  m_progressDialog->hide();
+}
+
+Apbs::~Apbs()
+{
+  delete m_progressDialog;
+}
+
+void Apbs::setMolecule(QtGui::Molecule *mol)
+{
+  if (mol != m_molecule)
+    m_molecule = mol;
+}
+
+void Apbs::onOpenOutputFile()
+{
+  QString fileName =
+    QFileDialog::getOpenFileName(qobject_cast<QWidget *>(parent()),
+                                 tr("Open Output File"), QString(),
+                                 tr("OpenDX File (*.dx)"));
+  if (fileName.isEmpty())
+    return;
+
+  m_progressDialog->show();
+  m_progressDialog->setMinimumDuration(0);
+  m_progressDialog->setRange(0, 0);
+  m_progressDialog->setLabelText(tr("Reading OpenDX File"));
+  qApp->processEvents();
+
+  OpenDxReader reader;
+  bool ok = reader.readFile(fileName);
+  if (!ok) {
+    QMessageBox::critical(qobject_cast<QWidget *>(parent()),
+                          tr("APBS Error"),
+                          tr("Error: %1").arg(reader.errorString()));
+    return;
+  }
+
+  const QtGui::Cube *cube = reader.cube();
+  if (!cube) {
+    QMessageBox::critical(qobject_cast<QWidget *>(parent()),
+                          tr("APBS Error"),
+                          tr("Error: No Cube Found"));
+    return;
+  }
+
+  if (!m_molecule) {
+    QMessageBox::critical(qobject_cast<QWidget *>(parent()),
+                          tr("APBS Error"),
+                          tr("Error: No Molecule Found"));
+    return;
+  }
+
+  // generate positive potential mesh
+  m_progressDialog->setLabelText("Generating Positive Potential Mesh");
+  m_progressDialog->setRange(0, 100);
+  m_progressDialog->setValue(1);
+  qApp->processEvents();
+
+  QtGui::Mesh *positivePotentialMesh = m_molecule->addMesh();
+  QtGui::MeshGenerator *positiveMeshGenerator =
+    new QtGui::MeshGenerator(cube, positivePotentialMesh, 0.1f);
+  connect(positiveMeshGenerator, SIGNAL(finished()),
+          this, SLOT(cubeGeneratorFinished()));
+  connect(positiveMeshGenerator, SIGNAL(progressValueChanged(int)),
+          this, SLOT(onMeshGeneratorProgress(int)));
+  positiveMeshGenerator->run();
+
+  m_progressDialog->setLabelText("Generating Negative Potential Mesh");
+  m_progressDialog->setValue(1);
+  qApp->processEvents();
+
+  // generate negative potential mesh
+  QtGui::Mesh *negativePotentialMesh = m_molecule->addMesh();
+  QtGui::MeshGenerator *negativeMeshGenerator =
+    new QtGui::MeshGenerator(cube, negativePotentialMesh, -0.1f);
+  connect(negativeMeshGenerator, SIGNAL(finished()),
+          this, SLOT(cubeGeneratorFinished()));
+  connect(negativeMeshGenerator, SIGNAL(progressValueChanged(int)),
+          this, SLOT(onMeshGeneratorProgress(int)));
+  negativeMeshGenerator->run();
+
+  m_progressDialog->setValue(100);
+  m_progressDialog->hide();
+}
+
+void Apbs::cubeGeneratorFinished()
+{
+  QtGui::MeshGenerator *generator =
+    qobject_cast<QtGui::MeshGenerator *>(sender());
+  if (!generator) {
+    return;
+  }
+
+  // delete the generator
+  generator->deleteLater();
+
+  m_progressDialog->setValue(m_progressDialog->maximum());
+  m_progressDialog->hide();
+}
+
+void Apbs::onMeshGeneratorProgress(int value)
+{
+  m_progressDialog->setValue(value);
+  qApp->processEvents();
+}
+
+}
+}
---
+++ avogadro/qtplugins/apbs/opendxreader.cpp
@@ -0,0 +1,117 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2013 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#include "opendxreader.h"
+
+#include <QDebug>
+#include <QFile>
+
+namespace Avogadro {
+namespace QtPlugins {
+
+OpenDxReader::OpenDxReader()
+  : m_cube(0)
+{
+}
+
+OpenDxReader::~OpenDxReader()
+{
+}
+
+bool OpenDxReader::readFile(const QString &fileName)
+{
+  QFile file(fileName);
+  if (!file.open(QFile::ReadOnly)) {
+    m_errorString = "Failed to open file for reading";
+    return false;
+  }
+
+  delete m_cube;
+
+  Vector3i dim(0, 0, 0);
+  Vector3 origin(0, 0, 0);
+  QVector<Vector3> spacings;
+  std::vector<double> values;
+
+  while (!file.atEnd()) {
+    QByteArray line = file.readLine();
+    QTextStream stream(line);
+
+    if (line.isEmpty()) {
+      // skip empty line
+      continue;
+    }
+    else if (line[0] == '#') {
+      // skip comment line
+      continue;
+    }
+    else if (line.startsWith("object")) {
+      if (dim[0] != 0)
+        continue;
+      QString unused;
+      stream >> unused >> unused >> unused >> unused >> unused;
+      stream >> dim[0] >> dim[1] >> dim[2];
+    }
+    else if (line.startsWith("origin")) {
+      QString unused;
+      stream >> unused >> origin[0] >> origin[1] >> origin[2];
+    }
+    else if (line.startsWith("delta")) {
+      QString unused;
+      Vector3 delta;
+      stream >> unused >> delta[0] >> delta[1] >> delta[2];
+      spacings.append(delta);
+    }
+    else if (line.startsWith("attribute")) {
+      continue;
+    }
+    else if (line.startsWith("component")) {
+      continue;
+    }
+    else {
+      // data line
+      while (!stream.atEnd()) {
+        double value;
+        stream >> value;
+        values.push_back(value);
+        stream.skipWhiteSpace();
+      }
+    }
+  }
+
+  Vector3 spacing(spacings[0][0], spacings[1][1], spacings[2][2]);
+
+  // create potential cube
+  m_cube = new QtGui::Cube();
+  m_cube->setCubeType(QtGui::Cube::ESP);
+  m_cube->setLimits(origin, dim, spacing);
+  m_cube->setData(values);
+
+  return true;
+}
+
+QString OpenDxReader::errorString() const
+{
+  return m_errorString;
+}
+
+QtGui::Cube* OpenDxReader::cube() const
+{
+  return m_cube;
+}
+
+}
+}
