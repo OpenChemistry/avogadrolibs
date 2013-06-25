acf6aeb7f3dd8641621807f6dec6b052c36b2e0b
David C. Lonie <david.lonie@kitware.com>
2013-06-25 13:37:51 -0400

    Add FileFormatDialog to get open/save filenames and FileFormats.
    Change-Id: Id8cd878092e50e88525fb579d76c79100255e68b

---
+++ avogadro/qtgui/fileformatdialog.cpp
@@ -0,0 +1,293 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2013 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#include "fileformatdialog.h"
+
+#include <avogadro/core/molecule.h>
+#include <avogadro/io/fileformatmanager.h>
+
+#include <QtGui/QInputDialog>
+#include <QtGui/QMessageBox>
+
+#include <QtCore/QSettings>
+
+#include <vector>
+
+using std::vector;
+using Avogadro::Io::FileFormatManager;
+using Avogadro::Io::FileFormat;
+
+namespace Avogadro {
+namespace QtGui {
+
+FileFormatDialog::FileFormatDialog(QWidget *parentW) :
+  QFileDialog(parentW)
+{
+}
+
+FileFormatDialog::~FileFormatDialog()
+{
+}
+
+FileFormatDialog::FormatFilePair
+FileFormatDialog::fileToRead(QWidget *parent, const QString &caption,
+                             const QString &dir, const QString &filter)
+{
+  FormatFilePair result(NULL, QString());
+  // Use the default read filter if none specified:
+  const QString &realFilter = filter.isEmpty() ? readFileFilter() : filter;
+
+
+  do { // jump point for continue statements on retry
+    QString fileName = QFileDialog::getOpenFileName(parent, caption, dir,
+                                                    realFilter);
+
+    if (fileName.isEmpty()) // user cancel
+      return result;
+
+    // Extract extension from filename.
+    QFileInfo fileInfo(fileName);
+    QString extension = fileInfo.suffix();
+    if (extension.isEmpty())
+      extension = fileInfo.fileName();
+    extension.toLower();
+
+    // Lookup matching file formats.
+    vector<const FileFormat*> matches(
+          FileFormatManager::instance().fileFormatsFromFileExtension(
+            extension.toStdString(), FileFormat::Read | FileFormat::File));
+
+    // If none found, give user the option to retry.
+    if (matches.empty()) {
+      QMessageBox::StandardButton reply =
+          QMessageBox::question(parent, caption,
+                                tr("Unable to find a suitable file reader for "
+                                   "the selected file."),
+                                QMessageBox::Abort | QMessageBox::Retry,
+                                QMessageBox::Retry);
+      switch (reply) {
+      default:
+      case QMessageBox::Retry:
+        continue;
+      case QMessageBox::Abort:
+        return result;
+      }
+    }
+
+    result.first = selectFileFormat(parent, matches, caption,
+                                    tr("Multiple readers found that can read "
+                                       "this file. Which should be used?"),
+                                    QString("FileFormatDialog/fileToRead/%1"
+                                            "/lastUsed").arg(extension));
+    result.second = fileInfo.absoluteFilePath();
+
+  } while (false);
+
+  return result;
+}
+
+FileFormatDialog::FormatFilePair FileFormatDialog::fileToWrite(
+    QWidget *parent, const QString &caption, const QString &dir,
+    const QString &filter)
+{
+  FormatFilePair result(NULL, QString());
+  // Use the default read filter if none specified:
+  const QString &realFilter = filter.isEmpty() ? writeFileFilter() : filter;
+
+
+  do { // jump point for continue statements on retry
+    QString fileName = QFileDialog::getSaveFileName(parent, caption, dir,
+                                                    realFilter);
+
+    if (fileName.isEmpty()) // user cancel
+      return result;
+
+    // Extract extension from filename.
+    QFileInfo fileInfo(fileName);
+    QString extension = fileInfo.suffix();
+    if (extension.isEmpty())
+      extension = fileInfo.fileName();
+    extension.toLower();
+
+    // Lookup matching file formats.
+    vector<const FileFormat*> matches(
+          FileFormatManager::instance().fileFormatsFromFileExtension(
+            extension.toStdString(), FileFormat::Write | FileFormat::File));
+
+    // If none found, give user the option to retry.
+    if (matches.empty()) {
+      QMessageBox::StandardButton reply =
+          QMessageBox::question(parent, caption,
+                                tr("Unable to find a suitable file writer for "
+                                   "the selected format."),
+                                QMessageBox::Abort | QMessageBox::Retry,
+                                QMessageBox::Retry);
+      switch (reply) {
+      default:
+      case QMessageBox::Retry:
+        continue;
+      case QMessageBox::Abort:
+        return result;
+      }
+    }
+
+    result.first = selectFileFormat(parent, matches, caption,
+                                    tr("Multiple writers found that can write "
+                                       "this file. Which should be used?"),
+                                    QString("FileFormatDialog/fileToWrite/%1"
+                                            "/lastUsed").arg(extension));
+    result.second = fileInfo.absoluteFilePath();
+
+  } while (false);
+
+  return result;
+}
+
+const QString &FileFormatDialog::readFileFilter()
+{
+  static QString readFilter;
+  if (readFilter.isEmpty()) {
+    vector<const FileFormat *> formats =
+        FileFormatManager::instance().fileFormats(FileFormat::Read
+                                                  | FileFormat::File);
+
+    readFilter = generateFilterString(formats, AllFiles | AllFormats);
+  }
+
+  return readFilter;
+}
+
+const QString &FileFormatDialog::writeFileFilter()
+{
+  static QString writeFilter;
+  if (writeFilter.isEmpty()) {
+    vector<const FileFormat *> formats =
+        FileFormatManager::instance().fileFormats(FileFormat::Write
+                                                  | FileFormat::File);
+
+    writeFilter = generateFilterString(formats, AllFiles);
+  }
+
+  return writeFilter;
+}
+
+QString FileFormatDialog::generateFilterString(
+    const std::vector<const Io::FileFormat*> &ffs,
+    FileFormatDialog::FilterStringOptions options)
+{
+  QString filterString;
+  // Create a map that groups the file extensions by name:
+  QMap<QString, QString> formatMap;
+  for (std::vector<const Io::FileFormat*>::const_iterator it = ffs.begin(),
+       itEnd = ffs.end(); it != itEnd; ++it) {
+    QString name(QString::fromStdString((*it)->name()));
+    std::vector<std::string> exts = (*it)->fileExtensions();
+    for (std::vector<std::string>::const_iterator eit = exts.begin(),
+         eitEnd = exts.end(); eit != eitEnd; ++eit) {
+      QString ext(QString::fromStdString(*eit));
+      if (!formatMap.values(name).contains(ext)) {
+        formatMap.insertMulti(name, ext);
+      }
+    }
+  }
+
+  // This is a list of "extensions" returned by OB that are not actually
+  // file extensions, but rather the full filename of the file. These
+  // will be used as-is in the filter string, while others will be prepended
+  // with "*.".
+  QStringList nonExtensions;
+  nonExtensions
+      << "POSCAR"  // VASP input geometry
+      << "CONTCAR" // VASP output geometry
+      << "HISTORY" // DL-POLY history file
+      << "CONFIG"  // DL-POLY config file
+         ;
+
+  // This holds all known extensions:
+  QStringList allExtensions;
+
+  foreach (const QString &desc, formatMap.uniqueKeys()) {
+    QStringList extensions;
+    foreach (QString extension, formatMap.values(desc)) {
+      if (!nonExtensions.contains(extension))
+        extension.prepend("*.");
+      extensions << extension;
+    }
+    if (options & AllFormats)
+      allExtensions << extensions;
+    filterString += QString("%1 (%2);;").arg(desc, extensions.join(" "));
+  }
+
+  if (options & AllFiles)
+    filterString.prepend(tr("All files (*);;"));
+
+  if (options & AllFormats) {
+    filterString.prepend(tr("All supported formats (%1);;")
+                         .arg(allExtensions.join(" ")));
+  }
+
+  return filterString;
+}
+
+const Io::FileFormat *FileFormatDialog::selectFileFormat(
+    QWidget *parentWidget,
+    const std::vector<const Io::FileFormat *> &ffs,
+    const QString &caption, const QString &prompt,
+    const QString &settingsKey)
+{
+  if (ffs.empty()) {
+    return NULL;
+  }
+  else if (ffs.size() == 1) {
+    return ffs[0];
+  }
+
+  // If more than one format found, prompt user to select one.
+  QStringList idents;
+  for (std::vector<const Io::FileFormat*>::const_iterator
+       it = ffs.begin(), itEnd = ffs.end(); it != itEnd; ++it) {
+    idents << QString::fromStdString((*it)->identifier());
+  }
+
+  // See if they used one before:
+  QString lastIdent = settingsKey.isNull()
+      ? QString() : QSettings().value(settingsKey).toString();
+
+  int lastIdentIndex = idents.indexOf(lastIdent);
+  if (lastIdentIndex < 0)
+    lastIdentIndex = 0;
+
+  bool ok;
+  QString item =
+      QInputDialog::getItem(parentWidget, caption, prompt, idents,
+                            lastIdentIndex, false, &ok);
+  int index = idents.indexOf(item);
+
+  // user cancel
+  if (!ok
+      || index < 0
+      || index + 1 > static_cast<int>(ffs.size()))
+    return NULL;
+
+  // Store chosen reader for next time
+  if (!settingsKey.isNull())
+    QSettings().setValue(settingsKey, item);
+
+  return ffs[index];
+}
+
+} // namespace QtGui
+} // namespace Avogadro
---
+++ avogadro/qtgui/fileformatdialog.h
@@ -0,0 +1,159 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2013 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#ifndef AVOGADRO_QTGUI_FILEFORMATDIALOG_H
+#define AVOGADRO_QTGUI_FILEFORMATDIALOG_H
+
+#include <QtGui/QFileDialog>
+#include "avogadroqtguiexport.h"
+
+#include <vector>
+
+namespace Avogadro {
+namespace Core {
+class Molecule;
+}
+
+namespace Io {
+class FileFormat;
+}
+
+namespace QtGui {
+/**
+ * @class FileFormatDialog fileformatdialog.h
+ * <avogadro/qtgui/fileformatdialog.h>
+ *
+ * Allow users to select save/open filenames that can be handled by registered
+ * FileFormats.
+ */
+class AVOGADROQTGUI_EXPORT FileFormatDialog : private QFileDialog
+{
+  Q_OBJECT
+public:
+  /**
+   * Container for a filename and a compatible file format, used as return
+   * values in static functions.
+   * @note The FileFormat will be set to NULL to indicate an error.
+   * @note The FileFormat points to the reference instance held by the
+   * FileFormatManager. Use FileFormat::newInstance() to create a usable copy.
+   */
+  typedef QPair<const Io::FileFormat*, QString> FormatFilePair;
+
+  /**
+   * @brief Show a QFileDialog to prompt the user for a file to open and resolve
+   * any file format conflicts. This method returns the selected file and
+   * FileFormat reader.
+   * @param parent The parent of the dialog windows.
+   * @param caption The dialog window titles.
+   * @param dir The initial directory shown to the user.
+   * @param filter A list of filters for limiting the files shown to the user.
+   * See the QFileDialog documentation for format. If the string is empty, a
+   * default list of all suitable registered formats will be used.
+   * @return A FormatFilePair object containing the absolute file path and a
+   * compatible file reader. If an error occurs, the format pointer will be
+   * NULL.
+   */
+  static FormatFilePair fileToRead(
+      QWidget *parent, const QString &caption = QString(),
+      const QString &dir = QString(), const QString &filter = QString());
+
+  /**
+   * @brief Show a QFileDialog to prompt the user for a file to save and resolve
+   * any file format conflicts. This method returns the selected file and
+   * FileFormat writer.
+   * @param parent The parent of the dialog windows.
+   * @param caption The dialog window titles.
+   * @param dir The initial directory shown to the user.
+   * @param filter A list of filters for limiting the files shown to the user.
+   * See the QFileDialog documentation for format. If the string is empty, a
+   * default list of all suitable registered formats will be used.
+   * @return A FormatFilePair object containing the absolute file path and a
+   * compatible file writer. If an error occurs, the format pointer will be
+   * NULL.
+   */
+  static FormatFilePair fileToWrite(
+      QWidget *parent, const QString &caption = QString(),
+      const QString &dir = QString(), const QString &filter = QString());
+
+private:
+  /**
+   * Constructor is private for now to force use of static methods. This
+   * may be made public at some point if additional API is needed or more
+   * complex use cases arise.
+   */
+  explicit FileFormatDialog(QWidget *parent = NULL);
+  ~FileFormatDialog();
+
+  /**
+   * @return A filter string for use with a QFileDialog, containing entries
+   * for all file extensions registered with FileFormatManager, as well as a
+   * "catch-all" entry with all known formats, and an "All files (*)" entry.
+   * Only formats registered with (Read | File) will be used.
+   */
+  static const QString& readFileFilter();
+
+  /**
+   * @return A filter string for use with a QFileDialog, containing entries
+   * for all file extensions registered with FileFormatManager. An
+   * "All files (*)" entry is added as well.
+   * Only formats registered with (Write | File) will be used.
+   */
+  static const QString& writeFileFilter();
+
+  /**
+   * Used internally by readFileFilter() and writeFileFilter().
+   * @{
+   */
+public: // Must be public for operator declarations
+  enum FilterStringOption {
+    NoFilterStringOption = 0x0,
+    AllFormats = 0x1,
+    AllFiles = 0x2
+  };
+  Q_DECLARE_FLAGS(FilterStringOptions, FilterStringOption)
+private:
+  static QString generateFilterString(
+      const std::vector<const Io::FileFormat*> &ffs,
+      FilterStringOptions options);
+  /** @} */
+
+  /**
+   * Show a dialog to resolve file format conflicts. This allows the user to
+   * select between multiple instances of FileFormat that can handle their
+   * chosen file extension.
+   * @param parentWidget Widget to use as dialog parent.
+   * @param ffs Conflicting FileFormats that the user should choose from.
+   * @param caption The title of the dialog window.
+   * @param prompt The text in the dialog window.
+   * @param settingsKey An optional QSettings key that will be used to
+   * store the user's choice for initializing the dialog next time.
+   * @return The selected FileFormat instance.
+   */
+  static const Io::FileFormat* selectFileFormat(
+      QWidget *parentWidget,
+      const std::vector<const Io::FileFormat*> &ffs,
+      const QString &caption,
+      const QString &prompt,
+      const QString &settingsKey = QString());
+
+};
+
+} // namespace QtGui
+} // namespace Avogadro
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(Avogadro::QtGui::FileFormatDialog::FilterStringOptions)
+
+#endif // AVOGADRO_QTGUI_FILEFORMATDIALOG_H
