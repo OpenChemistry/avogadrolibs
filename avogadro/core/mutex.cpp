6e4e450cc0d512b83f88831aeec000b901e512b2
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2013-06-28 14:48:48 -0400

    Added a simple wrapper for mutex
    This allows us to use mutex in the core API, enabling the migration of
    Cube and Mesh classes into core without the loss of simple threading
    constructs.
    
    Note that try_lock requires us to link to pthread when using the Boost
    fallback, added logic to the CMake code in STL to make that happen.
    
    Change-Id: Id2681529ee24501f879b15a958cdbed4e45ee82c

---
+++ avogadro/core/mutex.cpp
@@ -0,0 +1,61 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2013 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#include "mutex.h"
+
+#include <avogadro/stl/mutex_p.h>
+
+namespace Avogadro {
+namespace Core {
+
+using Stl::mutex;
+
+class Mutex::PIMPL
+{
+public:
+  PIMPL()
+  {
+  }
+
+  mutex lock;
+};
+
+Mutex::Mutex() : d(new PIMPL)
+{
+}
+
+Mutex::~Mutex()
+{
+  delete d;
+}
+
+void Mutex::lock()
+{
+  d->lock.lock();
+}
+
+bool Mutex::tryLock()
+{
+  return d->lock.try_lock();
+}
+
+void Mutex::unlock()
+{
+  d->lock.unlock();
+}
+
+}
+}
