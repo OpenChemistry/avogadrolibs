8b27e41e2b26293e5511a5910aa5375ee47327e6
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2013-07-06 17:05:39 +0200

    Improve color computations in fragment shaders.
    Change-Id: I7c787787a6a862100fbaf7420b0602e3e966787f

--- avogadro/rendering/spheres_fs.glsl
+++ avogadro/rendering/spheres_fs.glsl
@@ -17,18 +17,17 @@ void main()
   vec3 L = normalize(vec3(0, 1, 1));
   vec3 E = vec3(0, 0, 1);
   vec3 H = normalize(L + E);
-  float df = max(0.0, dot(N, L));
-  float sf = max(0.0, dot(N, H));
-  sf = pow(sf, 69.0);
-  vec3 ambient = fColor / 3.0;
-  vec3 diffuse = fColor;
-  vec3 specular = fColor * 3.0;
-  vec3 color = ambient + df * diffuse + sf * specular;
+  float df = max(0.0, dot(N, L)); // cos_alpha
+  float sf = max(0.0, dot(N, H)); // cos_beta
+  vec3 ambient = 0.4 * fColor;
+  vec3 diffuse = 0.55 * fColor;
+  vec3 specular = 0.5 * (vec3(1, 1, 1) - fColor);
+  vec3 color = ambient + df * diffuse + pow(sf, 20.0) * specular;
+  gl_FragColor = vec4(color, 1.0);
+
+  // determine fragment depth
   vec4 pos = eyePosition;
   pos.z += N.z * radius;//The radius is 1.0
   pos = projection * pos;
   gl_FragDepth = (pos.z / pos.w + 1.0) / 2.0;
-
-//  gl_FragColor = vec4(normalize(position), 1.0);
-  gl_FragColor = vec4(color, 1.0);
 }
