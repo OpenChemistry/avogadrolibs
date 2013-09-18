495da6c7994e0ad762ecdaa3da01620c32c70b66
David C. Lonie <david.lonie@kitware.com>
2013-09-18 14:41:35 -0400

    Add scriptable file format extension.
    Change-Id: Ibfdaa801def936b1ecf7fe25d119ad23cfd17782

---
+++ avogadro/qtplugins/scriptfileformats/scriptfileformats.h
@@ -0,0 +1,63 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2013 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#ifndef AVOGADRO_QTPLUGINS_SCRIPTFILEFORMATS_H
+#define AVOGADRO_QTPLUGINS_SCRIPTFILEFORMATS_H
+
+#include <avogadro/qtgui/extensionplugin.h>
+
+#include <avogadro/core/avogadrocore.h>
+
+namespace Avogadro {
+namespace QtPlugins {
+
+/**
+ * @brief This extension registers FileFormat reader/writers that are
+ * implemented as external scripts.
+ */
+class ScriptFileFormats : public QtGui::ExtensionPlugin
+{
+  Q_OBJECT
+
+public:
+  explicit ScriptFileFormats(QObject *parent = 0);
+  ~ScriptFileFormats() AVO_OVERRIDE;
+
+  QString name() const AVO_OVERRIDE { return tr("Script File Formats"); }
+
+  QString description() const AVO_OVERRIDE
+  {
+    return tr("Load file reader/writers from external scripts.");
+  }
+
+  QList<QAction *> actions() const AVO_OVERRIDE;
+
+  QStringList menuPath(QAction *) const AVO_OVERRIDE;
+
+  void setMolecule(QtGui::Molecule *mol) AVO_OVERRIDE;
+
+private:
+  QList<Io::FileFormat *> m_formats;
+
+  void refreshFileFormats();
+  void unregisterFileFormats();
+  void registerFileFormats();
+};
+
+}
+}
+
+#endif // AVOGADRO_QTPLUGINS_QUANTUMOUTPUT_H
