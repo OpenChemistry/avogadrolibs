5f18a080827b6990c369e3cdb2e176060213cf84
David C. Lonie <david.lonie@kitware.com>
2013-10-18 10:38:07 -0400

    Squashed history of QTAIM plugins. Do not merge.
    The full history will be merged after the rest of this topic is reviewed.
    
    Change-Id: I04052cca5053c67c0944fc06f98b732e32b6a66a

---
+++ avogadro/qtplugins/qtaim/qtaimcubature.h
@@ -0,0 +1,147 @@
+/**********************************************************************
+  QTAIM - Extension for Quantum Theory of Atoms In Molecules Analysis
+
+  Copyright (C) 2010 Eric C. Brown
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.openmolecules.net/>
+
+  Avogadro is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Avogadro is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+**********************************************************************/
+
+/* Based on */
+
+/* Adaptive multidimensional integration of a vector of integrands.
+ *
+ * Copyright (c) 2005-2009 Steven G. Johnson
+ *
+ * Portions (see comments) based on HIntLib (also distributed under
+ * the GNU GPL, v2 or later), copyright (c) 2002-2005 Rudolf Schuerer.
+ *     (http://www.cosy.sbg.ac.at/~rschuer/hintlib/)
+ *
+ * Portions (see comments) based on GNU GSL (also distributed under
+ * the GNU GPL, v2 or later), copyright (c) 1996-2000 Brian Gough.
+ *     (http://www.gnu.org/software/gsl/)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef QTAIMCUBATURE_H
+#define QTAIMCUBATURE_H
+
+#include <QPair>
+
+//#ifdef __cplusplus
+//extern "C"
+//{
+//#endif /* __cplusplus */
+
+/* USAGE: Call adapt_integrate with your function as described below.
+
+    To compile a test program, compile cubature.c with
+    -DTEST_INTEGRATOR as described at the end. */
+
+/* a vector integrand - evaluates the function at the given point x
+   (an array of length ndim) and returns the result in fval (an array
+   of length fdim).   The void* parameter is there in case you have
+   to pass any additional data through to your function (it corresponds
+   to the fdata parameter you pass to adapt_integrate). */
+typedef void (*integrand) (unsigned int ndim, const double *x, void *,
+                           unsigned int fdim, double *fval);
+
+/* a vector integrand of a vector of npt points: x[i*ndim + j] is the
+   j-th coordinate of the i-th point, and the k-th function evaluation
+   for the i-th point is returned in fval[k*npt + i]. */
+typedef void (*integrand_v) (unsigned int ndim, unsigned int npt,
+                             const double *x, void *,
+                             unsigned int fdim, double *fval);
+
+/* Integrate the function f from xmin[dim] to xmax[dim], with at most
+   maxEval function evaluations (0 for no limit), until the given
+   absolute or relative error is achieved.  val returns the integral,
+   and err returns the estimate for the absolute error in val; both
+   of these are arrays of length fdim, the dimension of the vector
+   integrand f(x). The return value of the function is 0 on success
+   and non-zero if there  was an error. */
+int adapt_integrate(unsigned int fdim, integrand f, void *fdata,
+                    unsigned int dim, const double *xmin, const double *xmax,
+                    unsigned int maxEval, double reqAbsError, double reqRelError,
+                    double *val, double *err);
+
+/* as adapt_integrate, but vectorized integrand */
+int adapt_integrate_v(unsigned int fdim, integrand_v f, void *fdata,
+                      unsigned int dim, const double *xmin, const double *xmax,
+                      unsigned int maxEval, double reqAbsError, double reqRelError,
+                      double *val, double *err);
+
+//#ifdef __cplusplus
+//}  /* extern "C" */
+//#endif /* __cplusplus */
+
+#include "qtaimwavefunction.h"
+#include "qtaimwavefunctionevaluator.h"
+#include "qtaimcriticalpointlocator.h"
+#include "qtaimodeintegrator.h"
+#include "qtaimlsodaintegrator.h"
+#include "qtaimmathutilities.h"
+
+namespace Avogadro
+{
+
+  class QTAIMCubature
+  {
+  public:
+    enum
+    {
+      ElectronDensity=0,
+      ElectronDensityLaplacian=1
+                             };
+
+    explicit QTAIMCubature(QTAIMWavefunction &wfn);
+    ~QTAIMCubature();
+
+    QList<QPair<qreal,qreal> > integrate(qint64 mode, QList<qint64> basins );
+
+    void setMode(qint64 mode);
+
+  private:
+    QTAIMWavefunction *m_wfn;
+    qint64 m_mode;
+    QList<qint64> m_basins;
+
+    QString m_temporaryFileName;
+    QString temporaryFileName();
+
+    QList<QVector3D> m_ncpList;
+
+  };
+
+} /* namespace Avogadro */
+
+#endif /* QTAIMCUBATURE_H */
---
+++ avogadro/qtplugins/qtaim/qtaimmathutilities.cpp
@@ -0,0 +1,327 @@
+/**********************************************************************
+  QTAIM - Extension for Quantum Theory of Atoms In Molecules Analysis
+
+  Copyright (C) 2010 Eric C. Brown
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.openmolecules.net/>
+
+  Avogadro is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Avogadro is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+**********************************************************************/
+
+#include "qtaimmathutilities.h"
+
+#include <cmath>
+#include <Eigen/QR>
+
+namespace Avogadro {
+  namespace QTAIMMathUtilities {
+
+  Matrix<qreal,3,1> eigenvaluesOfASymmetricThreeByThreeMatrix(const Matrix<qreal,3,3> &A)
+  {
+    SelfAdjointEigenSolver<Matrix<qreal, 3, 3> > eigensolver(A);
+    return eigensolver.eigenvalues();
+  }
+
+  Matrix<qreal,3,3> eigenvectorsOfASymmetricThreeByThreeMatrix(const Matrix<qreal,3,3> &A)
+  {
+    SelfAdjointEigenSolver<Matrix<qreal, 3, 3> > eigensolver(A);
+    return eigensolver.eigenvectors();
+  }
+
+  Matrix<qreal,4,1> eigenvaluesOfASymmetricFourByFourMatrix(const Matrix<qreal,4,4> &A)
+  {
+    SelfAdjointEigenSolver<Matrix<qreal, 4, 4> > eigensolver(A);
+    return eigensolver.eigenvalues();
+  }
+
+  Matrix<qreal,4,4> eigenvectorsOfASymmetricFourByFourMatrix(const Matrix<qreal,4,4> &A)
+  {
+    SelfAdjointEigenSolver<Matrix<qreal, 4, 4> > eigensolver(A);
+    return eigensolver.eigenvectors();
+  }
+
+  qint64 signOfARealNumber(qreal x)
+  {
+    if (x > 0.)
+      return  1;
+    else if (x == 0.)
+      return  0;
+    else
+      return -1;
+  }
+
+  qint64 signatureOfASymmetricThreeByThreeMatrix(const Matrix<qreal,3,3> &A)
+  {
+    SelfAdjointEigenSolver<Matrix<qreal, 3, 3> > eigensolver(A);
+    Matrix<qreal,3,1> eigenvalues=eigensolver.eigenvalues();
+
+    return signOfARealNumber(eigenvalues(0)) +
+        signOfARealNumber(eigenvalues(1)) +
+        signOfARealNumber(eigenvalues(2));
+  }
+
+  qreal ellipticityOfASymmetricThreeByThreeMatrix(const Matrix<qreal,3,3> &A)
+  {
+    SelfAdjointEigenSolver<Matrix<qreal, 3, 3> > eigensolver(A);
+    Matrix<qreal, 3, 1> eigenvalues=eigensolver.eigenvalues();
+
+    return (eigenvalues(0) / eigenvalues(1)) - 1.0 ;
+  }
+
+  qreal distance(const Matrix<qreal, 3, 1> &a, const Matrix<qreal, 3, 1> &b)
+  {
+    return sqrt(pow(a(0) - b(0), 2) +
+                pow(a(1) - b(1), 2) +
+                pow(a(2) - b(2), 2));
+  }
+
+  Matrix<qreal,3,1> sphericalToCartesian(const Matrix<qreal,3,1> &rtp,
+                                         const Matrix<qreal,3,1> &x0y0z0)
+  {
+    qreal r=rtp(0);
+    qreal theta=rtp(1);
+    qreal phi=rtp(2);
+
+    qreal x0=x0y0z0(0);
+    qreal y0=x0y0z0(1);
+    qreal z0=x0y0z0(2);
+
+    qreal costheta = cos(theta);
+    qreal cosphi   = cos(phi);
+    qreal sintheta = sin(theta);
+    qreal sinphi   = sin(phi);
+
+    Matrix<qreal, 3, 1> xyz(r * cosphi * sintheta + x0,
+                            r * sintheta * sinphi + y0,
+                            r * costheta          + z0);
+
+    return xyz;
+  }
+
+  Matrix<qreal, 3, 1> sphericalToCartesian(const Matrix<qreal, 3, 1> &rtp)
+  {
+    Matrix<qreal, 3, 1> x0y0z0(0., 0., 0.);
+
+    return  sphericalToCartesian(rtp, x0y0z0);
+  }
+
+  Matrix<qreal,3,1> cartesianToSpherical(const Matrix<qreal, 3, 1> &xyz,
+                                         const Matrix<qreal, 3, 1> &x0y0z0 )
+  {
+    qreal x=xyz(0);
+    qreal y=xyz(1);
+    qreal z=xyz(2);
+
+    qreal x0=x0y0z0(0);
+    qreal y0=x0y0z0(1);
+    qreal z0=x0y0z0(2);
+
+    qreal xshift = x - x0;
+    qreal yshift = y - y0;
+    qreal zshift = z - z0;
+
+    qreal length = sqrt(pow(xshift, 2) + pow(yshift, 2) + pow(zshift, 2));
+
+    Matrix<qreal, 3, 1> rtp;
+
+    if (length == 0.)
+      rtp << x0, y0, z0 ;
+    else if (xshift == 0. && yshift == 0.)
+      rtp << length, acos(zshift / length), 0.;
+    else
+      rtp << length, acos(zshift / length), atan2(xshift, yshift);
+
+    return rtp;
+  }
+
+  Matrix<qreal, 3, 1> cartesianToSpherical(const Matrix<qreal, 3, 1> &xyz)
+  {
+    Matrix<qreal, 3, 1> x0y0z0(0., 0., 0.);
+
+    return  cartesianToSpherical(xyz, x0y0z0);
+  }
+
+
+  // Cerjan-Miller-Baker-Popelier Methods
+  //
+  // Based on:
+  // Popelier, P.L.A. Comput. Phys. Comm. 1996, 93, 212.
+
+  Matrix<qreal, 3, 1> minusThreeSignatureLocatorGradient(const Matrix<qreal, 3, 1> &g,
+                                                         const Matrix<qreal, 3, 3> &H)
+  {
+    Matrix<qreal, 3, 1> value;
+
+    Matrix<qreal, 3, 1> b = eigenvaluesOfASymmetricThreeByThreeMatrix(H);
+    Matrix<qreal, 3, 3> U = eigenvectorsOfASymmetricThreeByThreeMatrix(H);
+
+    Matrix<qreal, 3, 1> F = U.transpose() * g;
+
+    Matrix<qreal, 4, 4> A;
+    A <<  b(0), 0.  , 0.  , F(0),
+    0.  , b(1), 0.  , F(1),
+    0.  , 0.  , b(2), F(2),
+    F(0), F(1), F(2), 0.   ;
+
+    Matrix<qreal, 4, 1> eval = eigenvaluesOfASymmetricFourByFourMatrix(A);
+    Matrix<qreal, 4, 4> evec = eigenvectorsOfASymmetricFourByFourMatrix(A);
+
+    Matrix<qreal, 3, 1> lambda;
+    lambda << eval(3), eval(3), eval(3);
+
+    Matrix<qreal, 3, 1> denom;
+    denom = b - lambda;
+
+    for (qint64 i=0; i < 3; ++i)
+      if( denom(i) < SMALL )
+        denom(i)=denom(i)+SMALL;
+
+    Matrix<qreal, 3, 1> h;
+    h << 0., 0., 0.;
+
+    for (qint64 j = 0; j < 3; ++j)
+      for (qint64 i = 0; i < 3; ++i)
+        h(j) = h(j) + ( -F(i) * U(j, i) ) / denom(i);
+
+    value = h;
+
+    return value;
+  }
+
+  Matrix<qreal, 3, 1> minusOneSignatureLocatorGradient(const Matrix<qreal, 3, 1> &g,
+                                                       const Matrix<qreal,3,3> &H)
+  {
+    Matrix<qreal, 3, 1> value;
+
+    Matrix<qreal, 3, 1> b = eigenvaluesOfASymmetricThreeByThreeMatrix(H);
+    Matrix<qreal, 3, 3> U = eigenvectorsOfASymmetricThreeByThreeMatrix(H);
+
+    Matrix<qreal, 3, 1> F = U.transpose() * g;
+
+    Matrix<qreal, 3, 3> A;
+    A <<  b(0), 0.  ,  F(0),
+    0.  , b(1),  F(1),
+    F(0), F(1),  0.   ;
+
+    Matrix<qreal, 3, 1> eval = eigenvaluesOfASymmetricThreeByThreeMatrix(A);
+    Matrix<qreal, 3, 3> evec = eigenvectorsOfASymmetricThreeByThreeMatrix(A);
+
+    Matrix<qreal, 3, 1> lambda;
+    lambda << eval(2), eval(2), (0.5) * (b(2) - sqrt(pow(b(2), 2) + 4.0 * pow(F(2), 2)));
+
+    Matrix<qreal, 3, 1> denom;
+    denom = b - lambda;
+
+    for (qint64 i = 0; i < 3; ++i)
+      if (denom(i) < SMALL)
+        denom(i) = denom(i) + SMALL;
+
+    Matrix<qreal, 3, 1> h;
+    h << 0., 0., 0.;
+
+    for (qint64 j = 0; j < 3; ++j)
+      for (qint64 i = 0; i < 3; ++i)
+        h(j) = h(j) + (-F(i) * U(j,i)) / denom(i);
+
+    value = h;
+
+    return value;
+  }
+
+  Matrix<qreal, 3, 1> plusOneSignatureLocatorGradient(const Matrix<qreal, 3, 1> &g,
+                                                      const Matrix<qreal,3,3> &H)
+  {
+    Matrix<qreal, 3, 1> value;
+
+    Matrix<qreal, 3, 1> b = eigenvaluesOfASymmetricThreeByThreeMatrix(H);
+    Matrix<qreal, 3, 3> U = eigenvectorsOfASymmetricThreeByThreeMatrix(H);
+
+    Matrix<qreal, 3, 1> F = U * g;
+
+    Matrix<qreal, 3, 3> A;
+    A <<  b(1), 0.  ,  F(1),
+    0.  , b(2),  F(2),
+    F(1), F(2),  0.;
+
+    Matrix<qreal, 3, 1> eval = eigenvaluesOfASymmetricThreeByThreeMatrix(A);
+    Matrix<qreal, 3, 3> evec = eigenvectorsOfASymmetricThreeByThreeMatrix(A);
+
+    Matrix<qreal, 3, 1> lambda;
+    lambda << eval(2), eval(2), (0.5) * (b(0) + sqrt(pow(b(0), 2) + 4.0 * pow(F(0), 2)));
+
+    Matrix<qreal, 3, 1> denom;
+    denom = b - lambda;
+
+    for (qint64 i = 0; i < 3; ++i)
+      if (denom(i) < SMALL)
+        denom(i) = denom(i) + SMALL;
+
+    Matrix<qreal, 3, 1> h;
+    h << 0., 0., 0.;
+
+    for (qint64 j = 0; j < 3; ++j)
+      for (qint64 i = 0; i < 3; ++i)
+        h(j) = h(j) + (-F(i) * U(i, j)) / denom(i);
+
+    value = h;
+
+    return value;
+  }
+
+  Matrix<qreal,3,1> plusThreeSignatureLocatorGradient(const Matrix<qreal, 3, 1> &g,
+                                                      const Matrix<qreal, 3, 3> &H)
+  {
+    Matrix<qreal, 3, 1> value;
+
+    Matrix<qreal, 3, 1> b = eigenvaluesOfASymmetricThreeByThreeMatrix(H);
+    Matrix<qreal, 3, 3> U = eigenvectorsOfASymmetricThreeByThreeMatrix(H);
+
+    Matrix<qreal, 3, 1> F = U * g;
+
+    Matrix<qreal, 4, 4> A;
+    A <<  b(0), 0.  , 0.  , F(0),
+    0.  , b(1), 0.  , F(1),
+    0.  , 0.  , b(2), F(2),
+    F(0), F(1), F(2), 0.;
+
+    Matrix<qreal, 4, 1> eval = eigenvaluesOfASymmetricFourByFourMatrix(A);
+    Matrix<qreal, 4, 4> evec = eigenvectorsOfASymmetricFourByFourMatrix(A);
+
+    Matrix<qreal, 3, 1> lambda;
+    lambda << eval(0), eval(0), eval(0);
+
+    Matrix<qreal, 3, 1> denom;
+    denom = b - lambda;
+
+    for (qint64 i = 0; i < 3; ++i)
+      if (denom(i) < SMALL)
+        denom(i) = denom(i) + SMALL;
+
+    Matrix<qreal, 3, 1> h;
+    h << 0., 0., 0.;
+
+    for (qint64 j = 0; j < 3; ++j)
+      for (qint64 i = 0; i < 3; ++i)
+        h(j) = h(j) + (-F(i) * U(i, j) ) / denom(i);
+
+    value = h;
+
+    return value;
+  }
+
+  } // namespace QTAIMMathUtilities
+} // namespace Avogadro
