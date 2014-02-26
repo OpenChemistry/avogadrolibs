219d3cc42923af57a133da1ac2892f79fa87df0d
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2014-02-26 14:52:28 -0500

    Minor style changes, copyright header, etc.
    Change-Id: Ib8c387426474891f770652404376dce97edb270b

--- avogadro/command/cjsontocml.cpp
+++ avogadro/command/cjsontocml.cpp
@@ -1,3 +1,18 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2013-2014 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
 #include "avogadro/io/fileformatmanager.h"
 #include "avogadro/core/molecule.h"
 
@@ -5,9 +20,12 @@
 #include <sstream>
 #include <string>
 
-using namespace Avogadro::Io;
-using namespace Avogadro::Core;
-using namespace std;
+using Avogadro::Io::FileFormatManager;
+using Avogadro::Core::Molecule;
+using std::cin;
+using std::cout;
+using std::string;
+using std::ostringstream;
 
 int main() {
 
