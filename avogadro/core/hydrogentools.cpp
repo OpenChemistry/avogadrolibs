10fa7941f15b6d37001cb77050bc858776e91091
Geoff Hutchison <geoff.hutchison@gmail.com>
2014-07-16 17:50:50 -0400

    First pass at better hydrogen additon. Works well for many sp3 cases.
    There are still plenty of corner cases and sp2, etc. to improve.
    
    Change-Id: Iaa587629883c7a8851a7b6c31b02f44c926986db

--- avogadro/core/hydrogentools.cpp
+++ avogadro/core/hydrogentools.cpp
@@ -29,10 +29,15 @@
 #include <cmath>
 #include <vector>
 
+#include <iostream>
+
 // C'mon windows....
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
+
+// Angle of tetrahedron
+#define M_TETRAHED 109.47122063449069389
 
 using Avogadro::Vector3;
 using Avogadro::Core::atomValence;
@@ -149,6 +154,7 @@ int HydrogenTools::valencyAdjustment(const Atom &atom)
   int result = 0;
   if (atom.isValid()) {
     const NeighborListType bonds(atom.molecule()->bonds(atom));
+    // sum of bond orders
     const unsigned int numberOfBonds(countExistingBonds(bonds));
     const unsigned int valency(lookupValency(atom, numberOfBonds));
     result = static_cast<int>(valency) - static_cast<int>(numberOfBonds);
@@ -177,6 +183,39 @@ int HydrogenTools::extraHydrogenIndices(const Atom &atom,
   return result;
 }
 
+  signed char HydrogenTools::perceiveHybridization(const Atom &atom)
+  {
+    const NeighborListType bonds(atom.molecule()->bonds(atom));
+    const unsigned int numberOfBonds(countExistingBonds(bonds)); // bond order sum
+
+    int hybridization = 3; // default to sp3
+
+    // TODO: Handle hypervalent species, SO3, SO4, lone pairs, etc.
+
+    if (numberOfBonds > 4) {
+      hybridization = numberOfBonds; // e.g., octahedral, trig. bipyr., etc.
+    } else {
+      // Count multiple bonds
+      unsigned int numTripleBonds = 0;
+      unsigned int numDoubleBonds = 0;
+
+      for (NeighborListType::const_iterator it = bonds.begin(), itEnd = bonds.end();
+       it != itEnd; ++it) {
+        if (it->order() == 2)
+          numDoubleBonds++;
+        else if (it->order() == 3)
+          numTripleBonds++;
+      }
+
+      if (numTripleBonds > 0 || numDoubleBonds > 1)
+        hybridization = 1; // sp
+      else if (numDoubleBonds > 0)
+        hybridization = 2; // sp2
+    }
+
+    return hybridization;
+  }
+
 void HydrogenTools::generateNewHydrogenPositions(
     const Atom &atom, int numberOfHydrogens,
     std::vector<Vector3> &positions)
@@ -184,29 +223,167 @@ void HydrogenTools::generateNewHydrogenPositions(
   if (!atom.isValid())
     return;
 
+  // Get the hybridization
+  signed char hybridization = atom.hybridization();
+  if (hybridization == 0) {
+    // Perceive it
+    hybridization = perceiveHybridization(atom);
+    //    atom.setHybridization(-1 * hybridization);
+  } else if (hybridization < 0) { // perceived
+    hybridization *= -1; // let's use the positive version
+  }
+
+  const Avogadro::Real bondLength = hydrogenBondDistance(atom.atomicNumber());
+
   // Get a list of all bond vectors (normalized, pointing away from 'atom')
   std::vector<Vector3> allVectors;
   const NeighborListType bonds(atom.molecule()->bonds(atom));
+  const int explicitBonds = bonds.size();
   allVectors.reserve(bonds.size() + static_cast<size_t>(numberOfHydrogens));
   for (NeighborListType::const_iterator it = bonds.begin(), itEnd = bonds.end();
        it != itEnd; ++it) {
     Atom otherAtom = getOtherAtom(atom, *it);
     Vector3 delta = otherAtom.position3d() - atom.position3d();
-    if (!delta.isZero(1e-5))
+    if (!delta.isZero(1e-5)) {
       allVectors.push_back(delta.normalized());
+    }
   }
 
-  // Tolerance for two vectors being "too close" in radians (pi/8).
-  const Avogadro::Real cosRadTol =
-      cos(static_cast<Avogadro::Real>(M_PI) / static_cast<Avogadro::Real>(8.));
-
-  const Avogadro::Real bondLength = hydrogenBondDistance(atom.atomicNumber());
-
-  // Try 10 times to generate a random vector that doesn't overlap with
-  // an existing bond. If we can't, just give up and let the overlap occur.
   for (int impHIndex = 0; impHIndex < numberOfHydrogens; ++impHIndex) {
+    // First try to derive the bond vector based on the hybridization
+    // Fallback will be to a random vector
+    Vector3 newPos = generateNewBondVector(atom, allVectors, hybridization);
+    allVectors.push_back(newPos);
+    positions.push_back(atom.position3d() + (newPos * bondLength));
+  }
+}
+
+  // Generate bond geometries
+  // First, the default fallback (random vectors)
+  // Also applies when you have a linear geometry and just need one new vector
+  // (it doesn't matter where it goes).
+  Vector3 HydrogenTools::generateNewBondVector(const Atom &atom, std::vector<Vector3> &allVectors, int hybridization)
+  {
     Vector3 newPos;
     bool success = false;
+    int currentValence = allVectors.size();
+
+    // Tolerance for two vectors being "too close" in radians (pi/8).
+    const Avogadro::Real cosRadTol =
+      cos(static_cast<Avogadro::Real>(M_PI) / static_cast<Avogadro::Real>(8.));
+
+    // No bonded atoms, just pick a random vector
+    if (currentValence == 0) {
+      newPos = Vector3::Random().normalized();
+      return newPos;
+    } else if (currentValence == 1) {
+      // One bonded atom
+      Vector3 bond1 = allVectors[0];
+
+      // Check what's attached to our neighbor -- we want to set trans to the neighbor
+      Vector3 bond2(0.0, 0.0, 0.0);
+      const NeighborListType bonds(atom.molecule()->bonds(atom));
+      for (NeighborListType::const_iterator it = bonds.begin(), itEnd = bonds.end();
+           it != itEnd; ++it) {
+        Atom a1 = getOtherAtom(atom, *it);
+        const NeighborListType nbrBonds(atom.molecule()->bonds(a1));
+        for (NeighborListType::const_iterator nbIt = nbrBonds.begin(), nbItEnd = nbrBonds.end();
+             nbIt != nbItEnd; ++nbIt) {
+          Atom a2 = getOtherAtom(a1, *nbIt);
+          if (a2.index() == atom.index())
+            continue; // we want a *new* atom
+
+          Vector3 delta = a2.position3d() - a1.position3d();
+          if (!delta.isZero(1e-5))
+            bond2 = delta.normalized();
+        }
+      }
+
+      Vector3 v1, v2;
+      v1 = bond1.cross(bond2);
+      bool noA2 = false;
+      if (bond2.norm() < 1.0e-5 || v1.norm() < 1.0e-5) {
+        std::cout << " creating a random paired atom " << std::endl;
+
+        // there is no a-2 atom
+        noA2 = true;
+        v2 = Vector3::Random().normalized();
+
+        double angle = fabs(acos(bond1.dot(v2)));
+        while (angle < 45.0*DEG_TO_RAD || angle > 135.0*DEG_TO_RAD) {
+          v2 = Vector3::Random().normalized();
+          angle = fabs(acos(bond1.dot(v2)));
+          std::cout << "angle = " << angle*RAD_TO_DEG << std::endl;
+        }
+        v1 = bond1.cross(v2); // so find a perpendicular, given the random vector
+        v2 = bond1.cross(v1);
+      } else {
+        std::cout << " found a neighbor for trans " << std::endl;
+        v1 = bond1.cross(bond2);
+        v2 = -1.0*bond1.cross(v1);
+      }
+      v2.normalize();
+
+      switch (hybridization) {
+      case 1:
+      case 4:
+      case 5:
+        newPos = bond1; // 180 degrees away from the current neighbor
+        break;
+      case 2: // sp2
+        newPos = bond1 - v2 * tan(DEG_TO_RAD*120.0);
+        break;
+      case 6: // octahedral
+        newPos = bond1 - v2 * tan(DEG_TO_RAD*90.0);
+        break;
+      case 3:
+      default:
+        newPos = (bond1 - v2 * tan(DEG_TO_RAD*M_TETRAHED));
+        break;
+      }
+
+      std::cout << " one bond " << newPos.normalized() << std::endl;
+      return -1.0*newPos.normalized();
+    } // end one bond
+    else if (currentValence == 2) {
+      Vector3 bond1 = allVectors[0];
+      Vector3 bond2 = allVectors[1];
+
+      Vector3 v1 = bond1 + bond2;
+      v1.normalize();
+
+      switch (hybridization) {
+      case 1: // shouldn't happen, but maybe with metal atoms?
+      case 2:
+        newPos = v1;
+      case 3:
+      default:
+        Vector3 v2 = bond1.cross(bond2); // find the perpendicular
+        v2.normalize();
+        newPos = bond1 - v2 * tan(DEG_TO_RAD*(M_TETRAHED));
+        newPos = v2 + v1 * (sqrt(2.0) / 2.0);
+      }
+
+      std::cout << " two bonds " << newPos.normalized() << std::endl;
+      return -1.0*newPos.normalized();
+    } // end two bonds
+    else if (currentValence == 3) {
+      Vector3 bond1 = allVectors[0];
+      Vector3 bond2 = allVectors[1];
+      Vector3 bond3 = allVectors[2];
+
+      // since the base of the tetrahedron should be symmetric
+      // the sum of the three bond vectors should cancel the angular parts
+      // and point in the new direction.. just need to normalize and rescale
+      newPos = -1.0*(bond1 + bond2 + bond3);
+
+      std::cout << " three bonds " << newPos.normalized() << std::endl;
+      return newPos.normalized();
+    }
+
+    // Fallback:
+    // Try 10 times to generate a random vector that doesn't overlap with
+    // an existing bond. If we can't, just give up and let the overlap occur.
     for (int attempt = 0; !success && attempt < 10; ++attempt) {
       newPos = Vector3::Random().normalized();
       success = true;
@@ -215,11 +392,9 @@ void HydrogenTools::generateNewHydrogenPositions(
         success = newPos.dot(*it) < cosRadTol;
       }
     }
-
-    allVectors.push_back(newPos);
-    positions.push_back(atom.position3d() + (newPos * bondLength));
+    return newPos;
   }
-}
+
 
 } // namespace Core
 } // namespace Avogadro
