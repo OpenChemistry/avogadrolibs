1c967839db3f4f824b6e37a8acb86fd321410cf9
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2015-10-11 16:11:14 -0400

    Register the QM file formats, and make available in Python
    I think it might be time to merge these into the main io library,
    but registering them works for now.

--- boostpython/io.cpp
+++ boostpython/io.cpp
@@ -2,10 +2,19 @@
 #include <avogadro/core/molecule.h>
 #include <avogadro/io/fileformatmanager.h>
 
+#include <avogadro/quantumio/gamessus.h>
+#include <avogadro/quantumio/gaussianfchk.h>
+#include <avogadro/quantumio/gaussiancube.h>
+#include <avogadro/quantumio/molden.h>
+#include <avogadro/quantumio/mopacaux.h>
+#include <avogadro/quantumio/nwchemjson.h>
+#include <avogadro/quantumio/nwchemlog.h>
+
 using namespace boost::python;
 using namespace Avogadro;
 using namespace Avogadro::Core;
 using namespace Avogadro::Io;
+using namespace Avogadro::QuantumIO;
 
 /// No operation deleter.
 void noopDeleter(void*) {}
@@ -29,6 +38,15 @@ std::string ffmWriteString(FileFormatManager &ffm, const Molecule &mol,
 
 void exportIo()
 {
+  /// Add the quantum IO formats, we should probably move them over soon, but
+  /// get things working for now...
+  Io::FileFormatManager::registerFormat(new GaussianFchk);
+  Io::FileFormatManager::registerFormat(new GaussianCube);
+  Io::FileFormatManager::registerFormat(new MoldenFile);
+  Io::FileFormatManager::registerFormat(new MopacAux);
+  Io::FileFormatManager::registerFormat(new NWChemJson);
+  Io::FileFormatManager::registerFormat(new NWChemLog);
+
   /// This class uses a singleton pattern, make it accessible through Python.
   class_<FileFormatManager, boost::shared_ptr<FileFormatManager>,
          boost::noncopyable> ("FileFormatManager", no_init)
