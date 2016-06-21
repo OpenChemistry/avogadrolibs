ddf16f21b8bc98663eb2d7735791265a47c8eb7d
Patrick Avery <psavery@buffalo.edu>
2016-06-21 00:48:06 -0400

    Added several spglib functions for crystals
    Added space group perception, primitive reduction, and cell conventionalization.
    In addition, the user can specify the tolerance for all of these features.
    All of the features use spglib to perform them. When performing any action,
    the program tells the user what the tolerance is currently set to.

---
+++ avogadro/qtplugins/spacegroup/spacegroup.cpp
@@ -0,0 +1,256 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2016 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#include "spacegroup.h"
+
+#include <avogadro/core/avospglib.h>
+#include <avogadro/core/unitcell.h>
+#include <avogadro/core/crystaltools.h>
+#include <avogadro/core/spacegroups.h>
+
+#include <avogadro/qtgui/molecule.h>
+
+#include <QtWidgets/QAction>
+#include <QtWidgets/QInputDialog>
+#include <QtWidgets/QMessageBox>
+
+#include <QtCore/QStringList>
+
+#include <sstream>
+
+using Avogadro::Core::AvoSpglib;
+using Avogadro::Core::CrystalTools;
+using Avogadro::Core::UnitCell;
+using Avogadro::QtGui::Molecule;
+
+namespace Avogadro {
+namespace QtPlugins {
+
+SpaceGroup::SpaceGroup(QObject *parent_) :
+  Avogadro::QtGui::ExtensionPlugin(parent_),
+  m_actions(QList<QAction *>()),
+  m_molecule(NULL),
+  m_spgTol(1e-5),
+  m_perceiveSpaceGroupAction(new QAction(this)),
+  m_reduceToPrimitiveAction(new QAction(this)),
+  m_conventionalizeCellAction(new QAction(this)),
+  m_setToleranceAction(new QAction(this))
+{
+  m_perceiveSpaceGroupAction->setText(tr("Perceive Space Group"));
+  connect(m_perceiveSpaceGroupAction, SIGNAL(triggered()),
+          SLOT(perceiveSpaceGroup()));
+  m_actions.push_back(m_perceiveSpaceGroupAction);
+  m_perceiveSpaceGroupAction->setProperty("menu priority", 90);
+
+  m_reduceToPrimitiveAction->setText(tr("Reduce to Primitive"));
+  connect(m_reduceToPrimitiveAction, SIGNAL(triggered()),
+          SLOT(reduceToPrimitive()));
+  m_actions.push_back(m_reduceToPrimitiveAction);
+  m_reduceToPrimitiveAction->setProperty("menu priority", 80);
+
+  m_conventionalizeCellAction->setText(tr("Conventionalize Cell"));
+  connect(m_conventionalizeCellAction, SIGNAL(triggered()),
+          SLOT(conventionalizeCell()));
+  m_actions.push_back(m_conventionalizeCellAction);
+  m_conventionalizeCellAction->setProperty("menu priority", 70);
+
+  m_setToleranceAction->setText(tr("Set Tolerance"));
+  connect(m_setToleranceAction, SIGNAL(triggered()), SLOT(setTolerance()));
+  m_actions.push_back(m_setToleranceAction);
+  m_setToleranceAction->setProperty("menu priority", 0);
+
+  updateActions();
+}
+
+SpaceGroup::~SpaceGroup()
+{
+  qDeleteAll(m_actions);
+  m_actions.clear();
+}
+
+QList<QAction *> SpaceGroup::actions() const
+{
+  return m_actions;
+}
+
+QStringList SpaceGroup::menuPath(QAction *) const
+{
+  return QStringList() << tr("&Crystal") << tr("Space Group");
+}
+
+void SpaceGroup::setMolecule(QtGui::Molecule *mol)
+{
+  if (m_molecule == mol)
+    return;
+
+  if (m_molecule)
+    m_molecule->disconnect(this);
+
+  m_molecule = mol;
+
+  if (m_molecule)
+    connect(m_molecule, SIGNAL(changed(uint)), SLOT(moleculeChanged(uint)));
+
+  updateActions();
+}
+
+void SpaceGroup::moleculeChanged(unsigned int c)
+{
+  Q_ASSERT(m_molecule == qobject_cast<Molecule*>(sender()));
+
+  Molecule::MoleculeChanges changes =
+      static_cast<Molecule::MoleculeChanges>(c);
+
+  if (changes & Molecule::UnitCell) {
+    if (changes & Molecule::Added || changes & Molecule::Removed)
+      updateActions();
+  }
+}
+
+void SpaceGroup::updateActions()
+{
+  // Disable everything for NULL molecules.
+  if (!m_molecule) {
+    foreach (QAction *action, m_actions)
+      action->setEnabled(false);
+    return;
+  }
+
+  if (m_molecule->unitCell()) {
+    foreach (QAction *action, m_actions)
+      action->setEnabled(true);
+
+  }
+  else {
+    foreach (QAction *action, m_actions)
+      action->setEnabled(false);
+  }
+}
+
+void SpaceGroup::perceiveSpaceGroup()
+{
+  unsigned short hallNumber = AvoSpglib::getHallNumber(*m_molecule, m_spgTol);
+  unsigned short intNum = Core::SpaceGroups::internationalNumber(hallNumber);
+  std::string hallSymbol = Core::SpaceGroups::hallSymbol(hallNumber);
+  std::string intShort =  Core::SpaceGroups::internationalShort(hallNumber);
+
+  // Success!
+  if (hallNumber != 0) {
+    // Let's make the message
+    std::stringstream ss;
+    ss << "Tolerance: " << m_spgTol << "  Å"
+       << "\nSpace Group: " << intNum
+       << "\nHall symbol: " << hallSymbol
+       << "\nInternational symbol: " << intShort;
+
+    // Now let's make the Message Box
+    QMessageBox retMsgBox;
+    retMsgBox.setText(tr(ss.str().c_str()));
+    retMsgBox.exec();
+  }
+  // Failure
+  else {
+    // Ask if the user wants to try again with a different tolerance
+    QMessageBox::StandardButton reply;
+    reply = QMessageBox::question(NULL, tr("Failure"),
+                                  tr("Space group perception failed.\n"
+                                     "Would you like to try again with a "
+                                     "different tolerance?"),
+                                  QMessageBox::Yes | QMessageBox::No);
+    if (reply == QMessageBox::Yes) {
+      setTolerance();
+      perceiveSpaceGroup(); // Recursion!
+    }
+  }
+}
+
+void SpaceGroup::reduceToPrimitive()
+{
+  // Confirm the tolerance
+  QMessageBox::StandardButton reply;
+  reply = QMessageBox::question(NULL, tr("Primitive Reduction"),
+                                tr("The tolerance is currently set to: %1.\n"
+                                   "Proceed with this tolerance?")
+                                  .arg(m_spgTol),
+                                QMessageBox::Yes | QMessageBox::No);
+  if (reply == QMessageBox::No)
+    setTolerance();
+
+  // Primitive reduction!
+  bool success = AvoSpglib::reduceToPrimitive(*m_molecule, m_spgTol);
+
+  if (!success) {
+    // Print an error message.
+    QMessageBox retMsgBox;
+    retMsgBox.setText(tr("Primitive reduction failed.\n"
+                         "Please check your crystal and try again "
+                         "with a different tolerance."));
+    retMsgBox.exec();
+  }
+  else {
+    m_molecule->emitChanged(Molecule::Added |
+                            Molecule::Atoms | Molecule::UnitCell);
+  }
+}
+
+void SpaceGroup::conventionalizeCell()
+{
+  // Confirm the tolerance
+  QMessageBox::StandardButton reply;
+  reply = QMessageBox::question(NULL, tr("Conventionalize Cell"),
+                                tr("The tolerance is currently set to: %1.\n"
+                                   "Proceed with this tolerance?")
+                                  .arg(m_spgTol),
+                                QMessageBox::Yes | QMessageBox::No);
+  if (reply == QMessageBox::No)
+    setTolerance();
+
+  // Conventionalize the cell!
+  bool success = AvoSpglib::conventionalizeCell(*m_molecule, m_spgTol);
+
+  if (!success) {
+    // Print an error message.
+    QMessageBox retMsgBox;
+    retMsgBox.setText(tr("Conventionalize cell failed.\n"
+                         "Please check your crystal and try again "
+                         "with a different tolerance."));
+    retMsgBox.exec();
+  }
+  else {
+    m_molecule->emitChanged(Molecule::Added |
+                            Molecule::Atoms | Molecule::UnitCell);
+  }
+}
+
+void SpaceGroup::setTolerance()
+{
+  bool ok;
+  double tol = QInputDialog::getDouble(NULL,
+                                       tr("Avogadro2"), // title
+                                       tr("Select tolerance in Å:"), // label
+                                       m_spgTol, // initial
+                                       1e-5, // min
+                                       0.5, // max
+                                       5, // decimals
+                                       &ok);
+  if (!ok)
+    return;
+
+  m_spgTol = tol;
+}
+
+} // namespace QtPlugins
+} // namespace Avogadro
--- tests/core/spglibtest.cpp
+++ tests/core/spglibtest.cpp
@@ -69,3 +69,237 @@ TEST(SpglibTest, getSpaceGroup)
   EXPECT_EQ(hallSymbol, std::string("-P 4n 2n"));
   EXPECT_EQ(intSymbol, std::string("P 4_2/m 2_1/n 2/m"));
 }
+
+// We're going to take a conventional cell, reduce it to the primitive form,
+// and check our results
+TEST(SpglibTest, reduceToPrimitive)
+{
+  // Let's build a primitive cell of corundum!
+  Molecule primMol;
+  Matrix3 primMat;
+  primMat.col(0) = Vector3(5.12980369, 0.00000000, 0.00000000); // A
+  primMat.col(1) = Vector3(2.92081932, 4.21707249, 0.00000000); // B
+  primMat.col(2) = Vector3(2.92081932, 1.52998182, 3.92973995); // C
+
+  UnitCell *primUC = new UnitCell(primMat);
+  primMol.setUnitCell(primUC);
+
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.94365, 0.55635, 0.25)));
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.25000, 0.94365, 0.55635)));
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.55635, 0.25000, 0.94365)));
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.05635, 0.44365, 0.75000)));
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.75000, 0.05635, 0.44365)));
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.44365, 0.75000, 0.05635)));
+  primMol.addAtom(13)
+    .setPosition3d(primUC->toCartesian(Vector3(0.35217, 0.35217, 0.35217)));
+  primMol.addAtom(13)
+    .setPosition3d(primUC->toCartesian(Vector3(0.14783, 0.14783, 0.14783)));
+  primMol.addAtom(13)
+    .setPosition3d(primUC->toCartesian(Vector3(0.64783,0.64783,0.64783)));
+  primMol.addAtom(13)
+    .setPosition3d(primUC->toCartesian(Vector3(0.85217,0.85217,0.85217)));
+
+  // Now, let's build a conventional cell of corundum
+  Molecule convMol;
+  Matrix3 convMat;
+  convMat.col(0) = Vector3(4.76060000, 0.00000000, 0.00000000); // A
+  convMat.col(1) = Vector3(-2.3803000, 4.12280054, 0.00000000); // B
+  convMat.col(2) = Vector3(0.00000000, 0.00000000,12.99400000); // C
+
+  UnitCell *convUC = new UnitCell(convMat);
+  convMol.setUnitCell(convUC);
+
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.69365000, 0.00000000, 0.25000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.69365000, 0.25000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.30635000, 0.30635000, 0.25000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.30635000, 0.00000000, 0.75000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.30635000, 0.75000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.69365000, 0.69365000, 0.75000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.36031667, 0.33333333, 0.58333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.02698333, 0.58333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.97301667, 0.63968333, 0.58333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.97301667, 0.33333333, 0.08333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.63968333, 0.08333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.36031667, 0.02698333, 0.08333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.02698333, 0.66666667, 0.91666667)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.36031667, 0.91666667)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.63968333, 0.97301667, 0.91666667)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.63968333, 0.66666667, 0.41666667)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.97301667, 0.41666667)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.02698333, 0.36031667, 0.41666667)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.00000000, 0.35217000)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.00000000, 0.14783000)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.00000000, 0.64783000)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.00000000, 0.85217000)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.33333333, 0.68550333)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.33333333, 0.48116333)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.33333333, 0.98116333)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.33333333, 0.18550333)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.66666667, 0.01883667)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.66666667, 0.81449667)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.66666667, 0.31449667)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.66666667, 0.51883667)));
+
+  // This should reduce the number of atoms in convMol to be
+  // equivalent to that of the primitive corundum
+  EXPECT_TRUE(AvoSpglib::reduceToPrimitive(convMol, 1e-5));
+
+  // Numbers of atoms and volumes should be equal
+  ASSERT_EQ(convMol.atomCount(), primMol.atomCount());
+  // We compare volumes as floats instead of doubles to allow for a little bit
+  // of a difference between them.
+  ASSERT_FLOAT_EQ(convMol.unitCell()->volume(), primMol.unitCell()->volume());
+}
+
+// We're going to take a primitive cell, conventionalize it,
+// and check our results
+TEST(SpglibTest, conventionalizeCell)
+{
+  // Let's build a primitive cell of corundum!
+  Molecule primMol;
+  Matrix3 primMat;
+  primMat.col(0) = Vector3(5.12980369, 0.00000000, 0.00000000); // A
+  primMat.col(1) = Vector3(2.92081932, 4.21707249, 0.00000000); // B
+  primMat.col(2) = Vector3(2.92081932, 1.52998182, 3.92973995); // C
+
+  UnitCell *primUC = new UnitCell(primMat);
+  primMol.setUnitCell(primUC);
+
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.94365, 0.55635, 0.25000)));
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.25000, 0.94365, 0.55635)));
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.55635, 0.25000, 0.94365)));
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.05635, 0.44365, 0.75000)));
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.75000, 0.05635, 0.44365)));
+  primMol.addAtom(8)
+    .setPosition3d(primUC->toCartesian(Vector3(0.44365, 0.75000, 0.05635)));
+  primMol.addAtom(13)
+    .setPosition3d(primUC->toCartesian(Vector3(0.35217, 0.35217, 0.35217)));
+  primMol.addAtom(13)
+    .setPosition3d(primUC->toCartesian(Vector3(0.14783, 0.14783, 0.14783)));
+  primMol.addAtom(13)
+    .setPosition3d(primUC->toCartesian(Vector3(0.64783, 0.64783, 0.64783)));
+  primMol.addAtom(13)
+    .setPosition3d(primUC->toCartesian(Vector3(0.85217, 0.85217, 0.85217)));
+
+  // Now, let's build a conventional cell of corundum
+  Molecule convMol;
+  Matrix3 convMat;
+  convMat.col(0) = Vector3(4.76060000, 0.00000000, 0.00000000); // A
+  convMat.col(1) = Vector3(-2.3803000, 4.12280054, 0.00000000); // B
+  convMat.col(2) = Vector3(0.00000000, 0.00000000,12.99400000); // C
+
+  UnitCell *convUC = new UnitCell(convMat);
+  convMol.setUnitCell(convUC);
+
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.69365000, 0.00000000, 0.25000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.69365000, 0.25000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.30635000, 0.30635000, 0.25000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.30635000, 0.00000000, 0.75000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.30635000, 0.75000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.69365000, 0.69365000, 0.75000000)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.36031667, 0.33333333, 0.58333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.02698333, 0.58333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.97301667, 0.63968333, 0.58333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.97301667, 0.33333333, 0.08333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.63968333, 0.08333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.36031667, 0.02698333, 0.08333333)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.02698333, 0.66666667, 0.91666667)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.36031667, 0.91666667)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.63968333, 0.97301667, 0.91666667)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.63968333, 0.66666667, 0.41666667)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.97301667, 0.41666667)));
+  convMol.addAtom(8).setPosition3d(
+    convUC->toCartesian(Vector3(0.02698333, 0.36031667, 0.41666667)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.00000000, 0.35217000)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.00000000, 0.14783000)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.00000000, 0.64783000)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.00000000, 0.00000000, 0.85217000)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.33333333, 0.68550333)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.33333333, 0.48116333)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.33333333, 0.98116333)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.66666667, 0.33333333, 0.18550333)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.66666667, 0.01883667)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.66666667, 0.81449667)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.66666667, 0.31449667)));
+  convMol.addAtom(13).setPosition3d(
+    convUC->toCartesian(Vector3(0.33333333, 0.66666667, 0.51883667)));
+
+  // This should increase the number of atoms in primMol to match
+  // that of convMol
+  EXPECT_TRUE(AvoSpglib::conventionalizeCell(primMol, 1e-5));
+
+  // Numbers of atoms and volumes should be identical
+  ASSERT_EQ(convMol.atomCount(), primMol.atomCount());
+  // We compare volumes as floats instead of doubles to allow for a little bit
+  // of a difference between them.
+  ASSERT_FLOAT_EQ(convMol.unitCell()->volume(), primMol.unitCell()->volume());
+}
