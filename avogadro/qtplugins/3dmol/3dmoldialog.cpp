f13db3e3260d34573d5bb3cdabd8c929b28424fe
Barry E. Moore II <bmooreii@Barrys-MacBook-Pro.local>
2016-08-11 17:42:27 -0400

    Finish scaffold text

--- avogadro/qtplugins/3dmol/3dmoldialog.cpp
+++ avogadro/qtplugins/3dmol/3dmoldialog.cpp
@@ -56,12 +56,10 @@ void ThreeDMolDialog::updateLabels()
 void ThreeDMolDialog::updateTextBrowser()
 {
   QString text = "<script src='http://3Dmol.csb.pitt.edu/build/3Dmol-min.js'></script>\n";
-  /*
   text.append("<div style='height: 400px; width: 400px; position: relative;' class='viewer_3Dmoljs' data-element='moleculeXYZ' data-type='xyz' data-backgroundcolor='0xffffff' data-style='stick'></div>\n");
   text.append("<textarea id='moleculeXYZ' style='display: none;'>");
   // Now put in the XYZ coords
   text.append("<textarea>");
-  */
   m_ui->textBrowser->setText(text.toHtmlEscaped());
 }
 
