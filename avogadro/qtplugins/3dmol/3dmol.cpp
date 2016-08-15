ae82767fa6e0f5a715a2c084138b5a3c817081cf
Barry E. Moore II <bmooreii@Barrys-MacBook-Pro.local>
2016-08-15 13:03:28 -0400

    Add copyright headers back

--- avogadro/qtplugins/3dmol/3dmol.cpp
+++ avogadro/qtplugins/3dmol/3dmol.cpp
@@ -1,3 +1,19 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2016 Barry E Moore II
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
 #include "3dmol.h"
 
 #include "3dmoldialog.h"
