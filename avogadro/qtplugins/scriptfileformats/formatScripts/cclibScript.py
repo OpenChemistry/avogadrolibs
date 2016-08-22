e6fa758f7ac560ad6b7a579d67ac3cd01b8cc803
Schamnad <sanjeed063@gmail.com>
2016-08-22 20:54:38 +0530

    Add license to cclibScript-fileformatscript

--- avogadro/qtplugins/scriptfileformats/formatScripts/cclibScript.py
+++ avogadro/qtplugins/scriptfileformats/formatScripts/cclibScript.py
@@ -1,7 +1,17 @@
 """
 /******************************************************************************
 
-    License information
+  This source file is part of the Avogadro project.
+
+  Copyright 2016 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 
 ******************************************************************************/
 """
