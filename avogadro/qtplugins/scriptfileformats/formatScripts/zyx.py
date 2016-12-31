211f473d7f72a72d2452fb843965476a057e6a8b
Geoff Hutchison <geoff.hutchison@gmail.com>
2016-12-31 17:20:30 -0500

    Clean up Python formatting with autopep8 command

--- avogadro/qtplugins/scriptfileformats/formatScripts/zyx.py
+++ avogadro/qtplugins/scriptfileformats/formatScripts/zyx.py
@@ -20,36 +20,39 @@ import argparse
 import json
 import sys
 
+
 def getMetaData():
-  metaData = {}
-  metaData['inputFormat'] = 'xyz'
-  metaData['outputFormat'] = 'xyz'
-  metaData['operations'] = ['read', 'write']
-  metaData['identifier'] = 'ZYX Example Format'
-  metaData['name'] = 'ZYX Example Format'
-  metaData['description'] = "Mostly useless file format that reads xyz-style " +\
-                            "files with reversed coordinates. Demonstrates " +\
-                            "the implementation of a user-scripted file format."
-  metaData['fileExtensions'] = ['zyx']
-  metaData['mimeTypes'] = ['chemical/x-zyx']
-  return metaData
+    metaData = {}
+    metaData['inputFormat'] = 'xyz'
+    metaData['outputFormat'] = 'xyz'
+    metaData['operations'] = ['read', 'write']
+    metaData['identifier'] = 'ZYX Example Format'
+    metaData['name'] = 'ZYX Example Format'
+    metaData['description'] = "Mostly useless file format that reads xyz-style " +\
+                              "files with reversed coordinates. Demonstrates " +\
+                              "the implementation of a user-scripted file format."
+    metaData['fileExtensions'] = ['zyx']
+    metaData['mimeTypes'] = ['chemical/x-zyx']
+    return metaData
+
 
 def write():
-  result = ""
+    result = ""
 
-  # Just copy the first two lines: numAtoms and comment/title
-  result += sys.stdin.readline()
-  result += sys.stdin.readline()
+    # Just copy the first two lines: numAtoms and comment/title
+    result += sys.stdin.readline()
+    result += sys.stdin.readline()
 
-  for line in sys.stdin:
-    words = line.split()
-    result += '%-3s %9.5f %9.5f %9.5f'%\
-      (words[0], float(words[3]), float(words[2]), float(words[1]))
-    if len(words) > 4:
-      result += words[4:].join(' ')
-    result += '\n'
+    for line in sys.stdin:
+        words = line.split()
+        result += '%-3s %9.5f %9.5f %9.5f' %\
+            (words[0], float(words[3]), float(words[2]), float(words[1]))
+        if len(words) > 4:
+            result += words[4:].join(' ')
+        result += '\n'
 
-  return result
+    return result
+
 
 def read():
     result = ""
@@ -59,25 +62,25 @@ def read():
     result += sys.stdin.readline()
 
     for line in sys.stdin:
-      words = line.split()
-      result += '%-3s %9.5f %9.5f %9.5f'%\
-        (words[0], float(words[3]), float(words[2]), float(words[1]))
-      if len(words) > 4:
-        result += words[4:].join(' ')
-      result += '\n'
+        words = line.split()
+        result += '%-3s %9.5f %9.5f %9.5f' %\
+            (words[0], float(words[3]), float(words[2]), float(words[1]))
+        if len(words) > 4:
+            result += words[4:].join(' ')
+        result += '\n'
 
     return result
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser('Testing file format script.')
-  parser.add_argument('--metadata', action='store_true')
-  parser.add_argument('--read', action='store_true')
-  parser.add_argument('--write', action='store_true')
-  args = vars(parser.parse_args())
+    parser = argparse.ArgumentParser('Testing file format script.')
+    parser.add_argument('--metadata', action='store_true')
+    parser.add_argument('--read', action='store_true')
+    parser.add_argument('--write', action='store_true')
+    args = vars(parser.parse_args())
 
-  if args['metadata']:
-    print(json.dumps(getMetaData()))
-  elif args['read']:
-    print(read())
-  elif args['write']:
-    print(write())
+    if args['metadata']:
+        print(json.dumps(getMetaData()))
+    elif args['read']:
+        print(read())
+    elif args['write']:
+        print(write())
