170190f99ef29a56a93e3027bc92a40c5769b5f1
Ethan Pavolik <etp12@pitt.edu>
2017-02-25 12:41:38 -0500

    Copyright indenting

--- avogadro/qtplugins/plugindownloader/downloaderwidget.cpp
+++ avogadro/qtplugins/plugindownloader/downloaderwidget.cpp
@@ -4,9 +4,9 @@
 
   Copyright 2017 Kitware, Inc.
 
-	This source code is released under the New BSD License, (the "License").
+  This source code is released under the New BSD License, (the "License").
 
-	Unless required by applicable law or agreed to in writing, software
+  Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
