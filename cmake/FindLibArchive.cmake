88aaf8d31faef88c5420cbcf13562f61afbbbfce
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2017-03-12 14:59:36 -0400

    Fixed case in LibArchive, and find call for headers
    This now works on Linux in that it sees the FindLibArhive.cmake, and is
    able to find the headers and libraries. I am getting an issue where it
    cannot resolve the symbols in libarhive and looking into why that is.

--- cmake/FindLibArchive.cmake
+++ cmake/FindLibArchive.cmake
@@ -4,19 +4,20 @@
 #
 #  LIBARCHIVE_FOUND        - system has LIBARCHIVE
 #  LIBARCHIVE_INCLUDE_DIRS - the LIBARCHIVE include directories
-#  LIBARCHIVE_LIBRARY      - The LIBARCHIVE library
+#  LIBARCHIVE_LIBRARIES    - The LIBARCHIVE library
 #
-find_path(LIBARCHIVE_INCLUDE_DIR libarchive/archive.h)
+find_path(LIBARCHIVE_INCLUDE_DIR archive.h)
 # libarchive/archive_entry.h)
 find_library(LIBARCHIVE_LIBRARY NAMES archive)
 
 set(LIBARCHIVE_INCLUDE_DIRS "${LIBARCHIVE_INCLUDE_DIR}")
+set(LIBARCHIVE_LIBRARIES "${LIBARCHIVE_LIBRARY}")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LIBARCHIVE DEFAULT_MSG LIBARCHIVE_INCLUDE_DIR
                                   LIBARCHIVE_LIBRARY)
 
-message("LM_SENSORS include dir = ${LIBARCHIVE_INCLUDE_DIR}")
-message("LM_SENSORS lib = ${LIBARCHIVE_LIBRARY}")
+message("LIBARCHIVE include dir = ${LIBARCHIVE_INCLUDE_DIR}")
+message("LIBARCHIVE lib = ${LIBARCHIVE_LIBRARY}")
 
 mark_as_advanced(LIBARCHIVE_INCLUDE_DIR LIBARCHIVE_LIBRARY)
