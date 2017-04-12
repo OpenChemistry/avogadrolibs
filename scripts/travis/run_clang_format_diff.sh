feed7a9c47068632e9980312d8168763f848e01d
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2017-04-12 15:33:52 -0400

    Use Travis to validate code style with clang-format

---
+++ scripts/travis/run_clang_format_diff.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIFF=`git diff -U0 $1...$2 -- '*.h' '*.cxx' | clang-format-diff-3.8 -p1`
+if [ -z "$DIFF" ]; then
+  exit 0
+else
+  printf "ERROR: clang-format-diff detected formatting issues. Please run clang-format on your branch.\nThe following formatting changes are suggested:\n\n%s" "$DIFF"
+  exit 1
+fi
