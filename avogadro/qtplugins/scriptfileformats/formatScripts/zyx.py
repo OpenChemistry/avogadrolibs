00d64c9c3e60e2fdaded2528d7f8bbd2012da237
Geoff Hutchison <geoff.hutchison@gmail.com>
2018-07-14 17:37:50 -0400

    Update format scripts to use --display-name and unified loader
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- avogadro/qtplugins/scriptfileformats/formatScripts/zyx.py
+++ avogadro/qtplugins/scriptfileformats/formatScripts/zyx.py
@@ -27,7 +27,7 @@ def getMetaData():
     metaData['outputFormat'] = 'xyz'
     metaData['operations'] = ['read', 'write']
     metaData['identifier'] = 'ZYX Example Format'
-    metaData['name'] = 'ZYX Example Format'
+    metaData['name'] = 'ZYX'
     metaData['description'] = "Mostly useless file format that reads xyz-style " +\
                               "files with reversed coordinates. Demonstrates " +\
                               "the implementation of a user-scripted file format."
@@ -72,14 +72,18 @@ def read():
     return result
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser('Testing file format script.')
+    parser = argparse.ArgumentParser('Example file format script.')
     parser.add_argument('--metadata', action='store_true')
     parser.add_argument('--read', action='store_true')
     parser.add_argument('--write', action='store_true')
+    parser.add_argument('--display-name', action='store_true')
+    parser.add_argument('--lang', nargs='?', default='en')
     args = vars(parser.parse_args())
 
     if args['metadata']:
         print(json.dumps(getMetaData()))
+    elif args['display_name']:
+        print(getMetaData()['name'])
     elif args['read']:
         print(read())
     elif args['write']:
