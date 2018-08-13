f65c275397b5bda93779dea41fc1d03eb5721adc
Sean <Sean.Donahue.5311@gmail.com>
2018-08-12 20:57:08 -0400

    Created a bitVector class, maybe not threadsafe

---
+++ avogadro/qtplugins/surfaces/bitvector.cpp
@@ -0,0 +1,82 @@
+#include "bitvector.h"
+#include <Eigen/Dense>
+
+namespace Avogadro{
+
+namespace QtPlugins{
+
+  //Constructor - takes a vector representing dimensions
+  BitVector::BitVector(Vector3i dimensions){
+    BitVector(dimensions[0], dimensions[1], dimensions[2]);
+  }
+
+  //Constructor - takes ints length, width, and height
+  BitVector::BitVector(int length, int width, int height){
+    m_length = length;
+    m_width = width;
+    m_height = height;
+    byteArray = new unsigned char[length * width * height / 8 + 1];
+  }
+
+  BitVector::~BitVector(){
+    delete[] byteArray;
+  }
+
+  //returns value at location
+  bool BitVector::value(Vector3i location){
+    return value(location[0], location[1], location[2]);
+  }
+
+  //returns value at x, y, z
+  bool BitVector::value(int x, int y, int z){
+    int byteIndex = findIndex(x, y, z) / 8;
+    int bitIndex = findIndex(x, y, z) % 8;
+    unsigned char bitMask = pow(2, bitIndex);
+    unsigned char a = byteArray[byteIndex] & bitMask;
+    unsigned char b = a >> bitIndex;
+    if(b == 1){
+      return true;
+    }
+    else if(b == 0){
+      return false;
+    }
+    return false;
+    //shouldn't ever happen, but we have to make the compiler happy
+    //we could also avoid the shifting and use greater than less than
+    //rather than equal to
+
+  }
+
+  //sets value at location equal to value
+  void BitVector::setValue(Vector3i location, bool value){
+    setValue(location[0], location[1], location[2], value);
+  }
+
+  //sets value at x, y, z equal to value
+  void BitVector::setValue(int x, int y, int z, bool value){
+    int byteIndex = findIndex(x, y, z) / 8;
+    int bitIndex = findIndex(x, y, z) % 8;
+    unsigned char bitMask = pow(2, bitIndex);
+    //to set a 1
+    if(value){
+      byteArray[byteIndex] = byteArray[byteIndex] | bitMask;
+    }
+
+    //to set a zero
+    else{
+      bitMask = ~bitMask;
+      byteArray[byteIndex] = byteArray[byteIndex] & bitMask;
+    }
+  }
+
+  int BitVector::findIndex(Vector3i location){
+    return findIndex(location[0], location[1], location[2]);
+  }
+
+  int BitVector::findIndex(int x, int y, int z){
+    return x * m_width * m_height + y * m_height % m_width + z;
+  }
+
+}//namespace QtPlugins
+
+}//namespace Avogadro
