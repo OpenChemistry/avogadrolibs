8362d76b8c166fd7c837e5d92c10ec23fa9f9bda
Chris Harris <chris.harris@kitware.com>
2018-09-27 15:32:55 -0400

    Fix up Appveyor path
    Signed-off-by: Chris Harris <chris.harris@kitware.com>

--- scripts/appveyor/build-wheels.ps1
+++ scripts/appveyor/build-wheels.ps1
@@ -2,7 +2,7 @@ trap { Write-Error $_; Exit 1 }
 
 set-alias sz "$env:ProgramFiles\7-Zip\7z.exe"
 Invoke-WebRequest -Uri "https://github.com/pybind/pybind11/archive/v2.2.4.zip" -OutFile "v2.2.4.zip"
-sz x v2.2.4.zip -oC:\projects -aoa -r
+sz x v2.2.4.zip -oC:\openchemistry -aoa -r
 Invoke-WebRequest -Uri "http://bitbucket.org/eigen/eigen/get/3.3.5.zip" -OutFile "3.3.5.zip"
-sz x  3.3.5.zip -oC:\projects -aoa -r
-C:\Python35-x64\python.exe C:\projects\avogadrolibs\scripts\appveyor\windows_build_wheels.py
+sz x  3.3.5.zip -oC:\openchemistry -aoa -r
+C:\Python35-x64\python.exe C:\openchemistry\avogadrolibs\scripts\appveyor\windows_build_wheels.py
