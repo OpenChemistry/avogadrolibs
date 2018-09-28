58561efcb1ae56f6c27cb58da5779a0f8e82b202
Chris Harris <chris.harris@kitware.com>
2018-09-28 09:11:41 -0400

    Add url and classifiers to setup.py
    Signed-off-by: Chris Harris <chris.harris@kitware.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 from skbuild import setup
 
 setup(
-    name="avogadro",
+    name='avogadro',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    description="Avogadro provides analysis and data processing useful in computational chemistry, molecular modeling, bioinformatics, materials science, and related areas.",
+    description='Avogadro provides analysis and data processing useful in computational chemistry, molecular modeling, bioinformatics, materials science, and related areas.',
     author='Kitware',
-    license="BSD",
+    license='BSD',
+    url='https://github.com/OpenChemistry/avogadrolibs',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: C++',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Operating System :: MacOS'
+        ],
     packages=['avogadro'],
     cmake_args=[
         '-DUSE_SPGLIB:BOOL=FALSE',
