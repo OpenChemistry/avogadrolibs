4dfca352a85d019775ac1b839196c30cb9a55fa1
Chris Harris <chris.harris@kitware.com>
2018-10-09 15:35:58 -0400

    Remove need to source install script on Travis
    Signed-off-by: Chris Harris <chris.harris@kitware.com>

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -8,7 +8,6 @@ if [[ $TASKS != "clang-format" && $TRAVIS_OS_NAME == "linux" ]]; then
   sudo add-apt-repository ppa:beineri/opt-qt542-trusty -y
   sudo apt-get update -qq
   sudo apt-get install -qq qt54base
-  source /opt/qt54/bin/qt54-env.sh
   sudo apt-get install libeigen3-dev libglew-dev libhdf5-dev \
                        libxml2-dev zlib1g-dev
 
@@ -18,7 +17,7 @@ if [[ $TASKS != "clang-format" && $TRAVIS_OS_NAME == "linux" ]]; then
   CMAKE_NAME="cmake-3.10.0-Linux-x86_64"
   wget https://cmake.org/files/v3.10/${CMAKE_NAME}.tar.gz
   tar -xzf ${CMAKE_NAME}.tar.gz
-  export CMAKE_EXE=${PWD}/${CMAKE_NAME}/bin/cmake
+  mv ${CMAKE_NAME} cmake
   cd avogadrolibs
 elif [[ $TASKS != "clang-format" && $TRAVIS_OS_NAME == "osx" ]]; then
   brew install qt eigen glew open-babel
