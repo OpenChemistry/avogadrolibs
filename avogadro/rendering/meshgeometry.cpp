0682a46843ccf57b6968f7a73f6480a13b5ed853
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2018-10-18 13:34:27 -0400

    Fix compile error on FreeBSD (NULL -> nullptr)
    This fixes #400 reported in GitHub where the NULL macro is defined to
    nullptr on FreeBSD.
    
    Signed-off-by: Marcus D. Hanwell <marcus.hanwell@kitware.com>

--- avogadro/rendering/meshgeometry.cpp
+++ avogadro/rendering/meshgeometry.cpp
@@ -171,7 +171,7 @@ void MeshGeometry::render(const Camera& camera)
   glDrawRangeElements(GL_TRIANGLES, 0,
                       static_cast<GLuint>(d->numberOfVertices - 1),
                       static_cast<GLsizei>(d->numberOfIndices), GL_UNSIGNED_INT,
-                      reinterpret_cast<const GLvoid*>(NULL));
+                      reinterpret_cast<const GLvoid*>(0));
 
   d->vbo.release();
   d->ibo.release();
