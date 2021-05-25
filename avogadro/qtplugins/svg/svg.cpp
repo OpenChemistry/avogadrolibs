9a9b9577679c4cb8d1aabdf6ba7d2b11c3d785c8
serk12 <marc.prat.maso@estudiantat.upc.edu>
2021-05-25 09:18:39 +0200

    correct occlusion + classic frustum + fix scale + factor
    Signed-off-by: serk12 <marc.prat.maso@estudiantat.upc.edu>

--- avogadro/qtplugins/svg/svg.cpp
+++ avogadro/qtplugins/svg/svg.cpp
@@ -5,23 +5,20 @@
 #include <QFileDialog>
 #include <QSvgGenerator>
 #include <QtWidgets/QAction>
-#include <avogadro/qtgui/molecule.h>
 #include <avogadro/qtopengl/glwidget.h>
 #include <avogadro/rendering/camera.h>
 #include <avogadro/rendering/scene.h>
 #include <iostream>
 #include <string>
-#include <vector>
 
 namespace Avogadro {
 namespace QtPlugins {
 
-typedef Avogadro::Core::Array<Avogadro::Core::Bond> NeighborListType;
-
-const float SVG::RADIUS = 25;
-const float SVG::PEN_WIDTH_MOL = 1;
-const float SVG::PEN_WIDTH_BOND = 4;
-const float SVG::OFF_SET_PARALEL = 7;
+const float SVG::RADIUS = 35.0f;
+const float SVG::PEN_WIDTH_MOL = 1.0f;
+const float SVG::PEN_WIDTH_BOND = 4.0f;
+const float SVG::OFF_SET_PARALEL = 7.0f;
+const float SVG::IMAGE_PADDING = 1.0f;
 
 SVG::SVG(QObject* parent_)
   : Avogadro::QtGui::ExtensionPlugin(parent_), m_molecule(nullptr),
@@ -67,28 +64,153 @@ bool SVG::defaultChecks()
   if (m_molecule->atomPositions3d().size() != m_molecule->atomCount())
     return true;
 
-  // no need to animate when there are no atoms
+  // no need to create SVG when there are no atoms
   if (m_molecule->atomCount() == 0)
     return true;
 
   return false;
 }
 
-bool sortByDistance(Eigen::Vector3f i, Eigen::Vector3f j)
+// calculate the camera frustrum
+void SVG::calculateCamera()
 {
-  return i[2] < j[2];
+  Eigen::Matrix4f m = (m_camera->projection() * m_camera->modelView()).matrix();
+
+  m_frustrum = std::vector<Eigen::Vector4f>(6);
+  // right
+  m_frustrum[0] = Eigen::Vector4f(m(0, 3) + m(0, 0), m(1, 3) + m(1, 0),
+                                  m(2, 3) + m(2, 0), m(3, 3) + m(3, 0));
+  // left
+  m_frustrum[1] = Eigen::Vector4f(m(0, 3) - m(0, 0), m(1, 3) - m(1, 0),
+                                  m(2, 3) - m(2, 0), m(3, 3) - m(3, 0));
+  // top
+  m_frustrum[2] = Eigen::Vector4f(m(0, 3) - m(0, 1), m(1, 3) - m(1, 1),
+                                  m(2, 3) - m(2, 1), m(3, 3) - m(3, 1));
+  // bottom
+  m_frustrum[3] = Eigen::Vector4f(m(0, 3) + m(0, 1), m(1, 3) + m(1, 1),
+                                  m(2, 3) + m(2, 1), m(3, 3) + m(3, 1));
+  // far
+  m_frustrum[4] = Eigen::Vector4f(m(0, 2), m(1, 2), m(2, 2), m(3, 2));
+  // near
+  m_frustrum[5] = Eigen::Vector4f(m(0, 3) - m(0, 2), m(1, 3) - m(1, 2),
+                                  m(2, 3) - m(2, 2), m(3, 3) - m(3, 2));
 }
 
-Eigen::Vector3f SVG::possToImage(const Vector3f& mol, float r)
+// conservative frustrum culling
+bool SVG::frustrumCulling(const Mol& mol)
+{
+  for (const auto& p : m_frustrum) {
+    if ((p.head<3>()).dot(mol.pos_model) + p.w() + mol.r <= 0)
+      return true;
+  }
+  return false;
+}
+
+// transform view position to 2d image
+Eigen::Vector3f SVG::posToImage(const Vector3f& mol, float r)
 {
   if (r <= 0) {
     r = 1;
   }
-  Eigen::Vector3f poss = mol - m_min;
-  poss =
-    Eigen::Vector3f(poss[0] / m_max[0] * m_width, poss[1] / m_max[1] * m_height,
-                    SVG::RADIUS * r * (poss[2] / m_max[2]));
-  return poss;
+  Eigen::Vector3f pos = mol - m_min;
+  float scale = 1.0f + (pos.norm() / m_max.norm());
+  float r_scale = 2.0f - (1.0f / r);
+  Eigen::Vector3f result =
+    Eigen::Vector3f(pos[0] / m_max[0] * m_width, pos[1] / m_max[1] * m_height,
+                    SVG::RADIUS * r_scale * scale);
+  return result;
+}
+
+// get all model, view, image positions sort them and save id to vector index
+void SVG::getPositions()
+{
+  calculateCamera();
+  m_mols.clear();
+  const Core::Array<Vector3> mols = m_molecule->atomPositions3d();
+  Eigen::Vector3f dummy = m_camera->modelView() * mols[0].cast<float>();
+  dummy[1] *= -1.0f;
+  m_min = dummy;
+  m_max = dummy;
+
+  for (unsigned int i = 0; i < mols.size(); ++i) {
+    Eigen::Vector3f pos = m_camera->modelView() * mols[i].cast<float>();
+    pos[1] *= -1.0f;
+    Mol mol(mols[i].cast<float>(), pos, i, m_molecule->bonds(i).size());
+    m_mols.push_back(mol);
+    if (frustrumCulling(mol)) {
+      for (unsigned int j = 0; j < 3; ++j) {
+        if (m_min[j] > pos[j])
+          m_min[j] = pos[j];
+        if (m_max[j] < pos[j])
+          m_max[j] = pos[j];
+      }
+    }
+  }
+  m_min[0] -= IMAGE_PADDING;
+  m_min[1] -= IMAGE_PADDING;
+  m_max[0] += IMAGE_PADDING;
+  m_max[1] += IMAGE_PADDING;
+  m_max -= m_min;
+  std::sort(m_mols.begin(), m_mols.end(), [&](const Mol& a, const Mol& b) {
+    return a.pos_view.norm() > b.pos_view.norm();
+  });
+  int i = 0;
+  for (auto& m : m_mols) {
+    m_idToindex[m.id] = i;
+    m.pos_image = posToImage(m.pos_view, m.r);
+    ++i;
+  }
+}
+
+// paint all bondings only once
+void SVG::paintBonds(QPainter& painter, const Mol& mol, unsigned int i,
+                     const NeighborListType& bonds)
+{
+  painter.setPen(QPen(QColor(125, 125, 125), PEN_WIDTH_BOND));
+  for (auto it = bonds.begin(); it != bonds.end(); ++it) {
+    unsigned int j = it->atom2().index();
+    if (m_idToindex[j] == i) {
+      j = it->atom1().index();
+    }
+    if (m_idToindex[j] <= i) {
+      continue;
+    }
+    // calculate the inicial and final position, considering the mol middle
+    // and the middle of the pen width
+    auto mol_to = m_mols[m_idToindex[j]].pos_image;
+    Eigen::Vector2f from(mol.pos_image[0] - (PEN_WIDTH_BOND / 2.0f),
+                         mol.pos_image[1] - (PEN_WIDTH_BOND / 2.0f));
+    Eigen::Vector2f to(mol_to[0] - (PEN_WIDTH_BOND / 2.0f),
+                       mol_to[1] - (PEN_WIDTH_BOND / 2.0f));
+
+    float L = std::sqrt((from[0] - to[0]) * (from[0] - to[0]) +
+                        (from[1] - to[1]) * (from[1] - to[1]));
+    float offsetX = (to[1] - from[1]) / L;
+    float offsetY = (from[0] - to[0]) / L;
+    unsigned int order = int(it->order());
+    // for each bound offset it following the orthogonal direction
+    for (unsigned int o = 0; o < order; ++o) {
+      // if there is only one bond, don't displace
+      float x = 0, y = 0;
+      if (order > 1) {
+        x = (float(o) - (order / 2.0f)) * OFF_SET_PARALEL * offsetX;
+        y = (float(o) - (order / 2.0f)) * OFF_SET_PARALEL * offsetY;
+      }
+      QLineF line(from[0] + x, from[1] + y, to[0] + x, to[1] + y);
+      painter.drawLine(line);
+    }
+  }
+}
+
+void SVG::paintMol(QPainter& painter, const Mol& mol)
+{
+  Vector3ub color_mol = m_molecule->color(mol.id);
+  QColor color(color_mol[0], color_mol[1], color_mol[2]);
+  painter.setPen(QPen(QColor(0, 0, 0), PEN_WIDTH_MOL));
+  painter.setBrush(color);
+  painter.drawEllipse(mol.pos_image[0] - mol.pos_image[2] / 2.0f,
+                      mol.pos_image[1] - mol.pos_image[2] / 2.0f,
+                      mol.pos_image[2], mol.pos_image[2]);
 }
 
 void SVG::paintSVG(QPainter& painter)
@@ -97,77 +219,14 @@ void SVG::paintSVG(QPainter& painter)
   QColor backgroundColor(background[0], background[1], background[2],
                          background[3]);
   painter.fillRect(QRect(0, 0, m_width, m_height), backgroundColor);
-
-  const Eigen::Affine3f modelView = m_camera->modelView();
-  const Eigen::Affine3f projection = m_camera->projection();
-  const Core::Array<Vector3> mols = m_molecule->atomPositions3d();
-  Eigen::Vector3f dummy = projection * modelView * mols[0].cast<float>();
-  dummy[1] *= -1.0f;
-  dummy[2] *= -1.0f;
-  m_min = dummy;
-  m_max = dummy;
-
-  std::vector<Eigen::Vector3f> mols_d;
-  for (unsigned int i = 0; i < mols.size(); ++i) {
-    Eigen::Vector3f poss = projection * modelView * mols[i].cast<float>();
-    poss[1] *= -1.0f;
-    poss[2] *= -1.0f;
-    mols_d.push_back(poss);
-    if (m_min[2] > poss[2])
-      m_min[2] = poss[2];
-    if (m_max[2] < poss[2])
-      m_max[2] = poss[2];
-  }
-
-  m_min[0] = m_min[1] = -15.0f;
-  m_max[0] = m_max[1] = 15.0f;
-  m_max -= m_min;
-
-  for (unsigned int i = 0; i < mols_d.size(); ++i) {
-    const NeighborListType bonds = m_molecule->bonds(i);
-    Eigen::Vector3f poss = possToImage(mols_d[i], bonds.size());
-    painter.setPen(QPen(QColor(125, 125, 125), PEN_WIDTH_BOND));
-    for (NeighborListType::const_iterator it = bonds.begin(); it != bonds.end();
-         ++it) {
-      unsigned int j = it->atom2().index();
-      if (j <= i) {
-        continue;
-      }
-
-      Eigen::Vector3f poss_to =
-        possToImage(mols_d[j], m_molecule->bonds(j).size());
-      // calculate the inicial and final position, considering the circle midle
-      // point and the middle of the pen width
-      Eigen::Vector2f from(poss[0] + (poss[2] / 2.0f) - (PEN_WIDTH_BOND / 2.0f),
-                           poss[1] + (poss[2] / 2.0f) -
-                             (PEN_WIDTH_BOND / 2.0f));
-      Eigen::Vector2f to(
-        poss_to[0] + (poss_to[2] / 2.0f) - (PEN_WIDTH_BOND / 2.0f),
-        poss_to[1] + (poss_to[2] / 2.0f) - (PEN_WIDTH_BOND / 2.0f));
-
-      float L = std::sqrt((from[0] - to[0]) * (from[0] - to[0]) +
-                          (from[1] - to[1]) * (from[1] - to[1]));
-      float offsetX = (to[1] - from[1]) / L;
-      float offsetY = (from[0] - to[0]) / L;
-      unsigned int order = int(it->order());
-      // for each bound offset it following the orthogonal direction
-      for (unsigned int o = 0; o < order; ++o) {
-        float x = 0, y = 0;
-        if (order > 1) {
-          x = (float(o) - (order / 2.0f)) * OFF_SET_PARALEL * offsetX;
-          y = (float(o) - (order / 2.0f)) * OFF_SET_PARALEL * offsetY;
-        }
-        QLineF line(from[0] + x, from[1] + y, to[0] + x, to[1] + y);
-        painter.drawLine(line);
-      }
+  getPositions();
+  for (unsigned int i = 0; i < m_mols.size(); ++i) {
+    auto mol = m_mols[i];
+    const NeighborListType bonds = m_molecule->bonds(mol.id);
+    if (frustrumCulling(mol)) {
+      paintBonds(painter, mol, i, bonds);
+      paintMol(painter, mol);
     }
-
-    Vector3ub color_mol = m_molecule->color(i);
-    QColor color(color_mol[0], color_mol[1], color_mol[2]);
-    painter.setPen(QPen(QColor(0, 0, 0), PEN_WIDTH_MOL));
-    painter.setBrush(color);
-
-    painter.drawEllipse(poss[0], poss[1], poss[2], poss[2]);
   }
 }
 
--- avogadro/qtplugins/svg/svg.h
+++ avogadro/qtplugins/svg/svg.h
@@ -21,9 +21,14 @@
 #include <QPainter>
 #include <avogadro/core/vector.h>
 #include <avogadro/qtgui/extensionplugin.h>
+#include <avogadro/qtgui/molecule.h>
+#include <map>
+#include <vector>
 
 namespace Avogadro {
 namespace QtPlugins {
+typedef Avogadro::Core::Array<Avogadro::Core::Bond> NeighborListType;
+
 class SVG : public Avogadro::QtGui::ExtensionPlugin
 {
   Q_OBJECT
@@ -50,6 +55,18 @@ private slots:
   void render();
 
 private:
+  struct Mol
+  {
+    Eigen::Vector3f pos_model, pos_view, pos_image;
+    unsigned int r;
+    unsigned int id;
+
+    Mol(Vector3f m, Vector3f v, unsigned int i, unsigned int bonds)
+      : pos_model(m), pos_view(v), id(i)
+    {
+      r = (bonds <= 0 ? 1 : bonds);
+    }
+  };
   QtGui::Molecule* m_molecule;
   Rendering::Scene* m_scene;
   Rendering::Camera* m_camera;
@@ -57,13 +74,23 @@ private:
   QAction* m_action;
   int m_width, m_height;
   Eigen::Vector3f m_min, m_max;
+  std::vector<Mol> m_mols;
+  std::vector<Eigen::Vector4f> m_frustrum;
+  std::map<unsigned int, unsigned int> m_idToindex;
 
-  static const float RADIUS;
-  static const float PEN_WIDTH_MOL, PEN_WIDTH_BOND, OFF_SET_PARALEL;
+  static const float RADIUS, PEN_WIDTH_MOL, PEN_WIDTH_BOND, OFF_SET_PARALEL,
+    IMAGE_PADDING;
+
+  void paintSVG(QPainter& painter);
+  void paintMol(QPainter& painter, const Mol& mol);
+  void paintBonds(QPainter& painter, const Mol& mol, unsigned int i,
+                  const NeighborListType& bonds);
 
   bool defaultChecks();
-  Eigen::Vector3f possToImage(const Vector3f& mol, float r);
-  void paintSVG(QPainter& painter);
+  void getPositions();
+  void calculateCamera();
+  bool frustrumCulling(const Mol& mol);
+  Eigen::Vector3f posToImage(const Vector3f& mol, float r);
 };
 } // namespace QtPlugins
 } // namespace Avogadro
