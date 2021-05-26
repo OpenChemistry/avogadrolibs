2df232c04e55ad4e85e04b419135677629d9d251
serk12 <marc.prat.maso@estudiantat.upc.edu>
2021-05-26 22:13:11 +0200

    renaming, setOptions template, and fix proportion with camera project
    Signed-off-by: serk12 <marc.prat.maso@estudiantat.upc.edu>

--- avogadro/qtplugins/svg/svg.cpp
+++ avogadro/qtplugins/svg/svg.cpp
@@ -14,11 +14,12 @@
 namespace Avogadro {
 namespace QtPlugins {
 
-const float SVG::RADIUS = 35.0f;
-const float SVG::PEN_WIDTH_MOL = 1.0f;
-const float SVG::PEN_WIDTH_BOND = 4.0f;
-const float SVG::OFF_SET_PARALEL = 7.0f;
-const float SVG::IMAGE_PADDING = 1.0f;
+const float SVG::DEFAULT_RADIUS = 35.0f;
+const float SVG::DEFAULT_PEN_WIDTH_MOL = 1.0f;
+const float SVG::DEFAULT_PEN_WIDTH_BOND = 4.0f;
+const float SVG::DEFAULT_OFF_SET_PARALEL = 7.0f;
+const float SVG::DEFAULT_IMAGE_PADDING = 1.0f;
+const Vector3ub SVG::DEFAULT_BOND_COLOR = Vector3ub(125, 125, 125);
 
 SVG::SVG(QObject* parent_)
   : Avogadro::QtGui::ExtensionPlugin(parent_), m_molecule(nullptr),
@@ -71,6 +72,16 @@ bool SVG::defaultChecks()
   return false;
 }
 
+void SVG::setOptions()
+{
+  m_radius = DEFAULT_RADIUS;
+  m_penWidthMol = DEFAULT_PEN_WIDTH_MOL;
+  m_penWidthBond = DEFAULT_PEN_WIDTH_BOND;
+  m_offSetParalel = DEFAULT_OFF_SET_PARALEL;
+  m_ImagePadding = DEFAULT_IMAGE_PADDING;
+  m_BondColor = DEFAULT_BOND_COLOR;
+}
+
 // calculate the camera frustrum
 void SVG::calculateCamera()
 {
@@ -97,47 +108,47 @@ void SVG::calculateCamera()
 }
 
 // conservative frustrum culling
-bool SVG::frustrumCulling(const Mol& mol)
+bool SVG::frustrumCulling(const SVGAtom& atom)
 {
+  return true;
   for (const auto& p : m_frustrum) {
-    if ((p.head<3>()).dot(mol.pos_model) + p.w() + mol.r <= 0)
+    Eigen::Vector4f pos_model;
+    pos_model << atom.pos_model, 1.0f;
+    if (p.dot(pos_model) + atom.radius <= 0)
       return true;
   }
   return false;
 }
 
-// transform view position to 2d image
-Eigen::Vector3f SVG::posToImage(const Vector3f& mol, float r)
+// transform view position to 2d SVG image
+Eigen::Vector3f SVG::posToSVGImage(const SVGAtom& atom)
 {
-  if (r <= 0) {
-    r = 1;
-  }
-  Eigen::Vector3f pos = mol - m_min;
-  float scale = 1.0f + (pos.norm() / m_max.norm());
-  float r_scale = 2.0f - (1.0f / r);
-  Eigen::Vector3f result =
-    Eigen::Vector3f(pos[0] / m_max[0] * m_width, pos[1] / m_max[1] * m_height,
-                    SVG::RADIUS * r_scale * scale);
+  Eigen::Vector3f result = m_camera->project(atom.pos_model);
+  float scale = 1.0f + ((atom.pos_view - m_min).norm() / m_max.norm());
+  float r_scale = 2.0f - (1.0f / atom.radius);
+
+  result[1] = ((result[1] - (m_height / 2.0f)) * -1.0f) + (m_height / 2.0f);
+  result[2] = m_radius * r_scale * scale;
   return result;
 }
 
 // get all model, view, image positions sort them and save id to vector index
 void SVG::getPositions()
 {
+  m_atoms.clear();
   calculateCamera();
-  m_mols.clear();
-  const Core::Array<Vector3> mols = m_molecule->atomPositions3d();
-  Eigen::Vector3f dummy = m_camera->modelView() * mols[0].cast<float>();
-  dummy[1] *= -1.0f;
-  m_min = dummy;
-  m_max = dummy;
+  m_max[0] = m_max[1] = m_max[2] = Eigen::Infinity;
+  m_min[0] = m_min[1] = m_min[2] = Eigen::Infinity * -1.0f;
 
-  for (unsigned int i = 0; i < mols.size(); ++i) {
-    Eigen::Vector3f pos = m_camera->modelView() * mols[i].cast<float>();
+  const Core::Array<Vector3> atomPositions = m_molecule->atomPositions3d();
+  for (unsigned int i = 0; i < atomPositions.size(); ++i) {
+    Eigen::Vector3f pos =
+      m_camera->modelView() * atomPositions[i].cast<float>();
     pos[1] *= -1.0f;
-    Mol mol(mols[i].cast<float>(), pos, i, m_molecule->bonds(i).size());
-    m_mols.push_back(mol);
-    if (frustrumCulling(mol)) {
+    SVGAtom atom(atomPositions[i].cast<float>(), pos, i,
+                 m_molecule->bonds(i).size());
+    m_atoms.push_back(atom);
+    if (frustrumCulling(atom)) {
       for (unsigned int j = 0; j < 3; ++j) {
         if (m_min[j] > pos[j])
           m_min[j] = pos[j];
@@ -146,27 +157,30 @@ void SVG::getPositions()
       }
     }
   }
-  m_min[0] -= IMAGE_PADDING;
-  m_min[1] -= IMAGE_PADDING;
-  m_max[0] += IMAGE_PADDING;
-  m_max[1] += IMAGE_PADDING;
+  m_min[0] -= m_ImagePadding;
+  m_min[1] -= m_ImagePadding;
+  m_max[0] += m_ImagePadding;
+  m_max[1] += m_ImagePadding;
   m_max -= m_min;
-  std::sort(m_mols.begin(), m_mols.end(), [&](const Mol& a, const Mol& b) {
-    return a.pos_view.norm() > b.pos_view.norm();
-  });
+  std::sort(m_atoms.begin(), m_atoms.end(),
+            [&](const SVGAtom& a, const SVGAtom& b) {
+              // sort using the distance to the camera
+              return a.pos_view.norm() > b.pos_view.norm();
+            });
   int i = 0;
-  for (auto& m : m_mols) {
+  for (auto& m : m_atoms) {
     m_idToindex[m.id] = i;
-    m.pos_image = posToImage(m.pos_view, m.r);
+    m.pos_image = posToSVGImage(m);
     ++i;
   }
 }
 
 // paint all bondings only once
-void SVG::paintBonds(QPainter& painter, const Mol& mol, unsigned int i,
+void SVG::paintBonds(QPainter& painter, const SVGAtom& atom, unsigned int i,
                      const NeighborListType& bonds)
 {
-  painter.setPen(QPen(QColor(125, 125, 125), PEN_WIDTH_BOND));
+  painter.setPen(QPen(QColor(m_BondColor[0], m_BondColor[1], m_BondColor[2]),
+                      m_penWidthBond));
   for (auto it = bonds.begin(); it != bonds.end(); ++it) {
     unsigned int j = it->atom2().index();
     if (m_idToindex[j] == i) {
@@ -175,13 +189,13 @@ void SVG::paintBonds(QPainter& painter, const Mol& mol, unsigned int i,
     if (m_idToindex[j] <= i) {
       continue;
     }
-    // calculate the inicial and final position, considering the mol middle
+    // calculate the inicial and final position, considering the atom middle
     // and the middle of the pen width
-    auto mol_to = m_mols[m_idToindex[j]].pos_image;
-    Eigen::Vector2f from(mol.pos_image[0] - (PEN_WIDTH_BOND / 2.0f),
-                         mol.pos_image[1] - (PEN_WIDTH_BOND / 2.0f));
-    Eigen::Vector2f to(mol_to[0] - (PEN_WIDTH_BOND / 2.0f),
-                       mol_to[1] - (PEN_WIDTH_BOND / 2.0f));
+    auto mol_to = m_atoms[m_idToindex[j]].pos_image;
+    Eigen::Vector2f from(atom.pos_image[0] - (m_penWidthBond / 2.0f),
+                         atom.pos_image[1] - (m_penWidthBond / 2.0f));
+    Eigen::Vector2f to(mol_to[0] - (m_penWidthBond / 2.0f),
+                       mol_to[1] - (m_penWidthBond / 2.0f));
 
     float L = std::sqrt((from[0] - to[0]) * (from[0] - to[0]) +
                         (from[1] - to[1]) * (from[1] - to[1]));
@@ -193,8 +207,8 @@ void SVG::paintBonds(QPainter& painter, const Mol& mol, unsigned int i,
       // if there is only one bond, don't displace
       float x = 0, y = 0;
       if (order > 1) {
-        x = (float(o) - (order / 2.0f)) * OFF_SET_PARALEL * offsetX;
-        y = (float(o) - (order / 2.0f)) * OFF_SET_PARALEL * offsetY;
+        x = (float(o) - (order / 2.0f)) * m_offSetParalel * offsetX;
+        y = (float(o) - (order / 2.0f)) * m_offSetParalel * offsetY;
       }
       QLineF line(from[0] + x, from[1] + y, to[0] + x, to[1] + y);
       painter.drawLine(line);
@@ -202,15 +216,15 @@ void SVG::paintBonds(QPainter& painter, const Mol& mol, unsigned int i,
   }
 }
 
-void SVG::paintMol(QPainter& painter, const Mol& mol)
+void SVG::paintCore(QPainter& painter, const SVGAtom& atom)
 {
-  Vector3ub color_mol = m_molecule->color(mol.id);
-  QColor color(color_mol[0], color_mol[1], color_mol[2]);
-  painter.setPen(QPen(QColor(0, 0, 0), PEN_WIDTH_MOL));
+  Vector3ub atom_color = m_molecule->color(atom.id);
+  QColor color(atom_color[0], atom_color[1], atom_color[2]);
+  painter.setPen(QPen(QColor(0, 0, 0), m_penWidthMol));
   painter.setBrush(color);
-  painter.drawEllipse(mol.pos_image[0] - mol.pos_image[2] / 2.0f,
-                      mol.pos_image[1] - mol.pos_image[2] / 2.0f,
-                      mol.pos_image[2], mol.pos_image[2]);
+  painter.drawEllipse(atom.pos_image[0] - atom.pos_image[2] / 2.0f,
+                      atom.pos_image[1] - atom.pos_image[2] / 2.0f,
+                      atom.pos_image[2], atom.pos_image[2]);
 }
 
 void SVG::paintSVG(QPainter& painter)
@@ -220,23 +234,27 @@ void SVG::paintSVG(QPainter& painter)
                          background[3]);
   painter.fillRect(QRect(0, 0, m_width, m_height), backgroundColor);
   getPositions();
-  for (unsigned int i = 0; i < m_mols.size(); ++i) {
-    auto mol = m_mols[i];
-    const NeighborListType bonds = m_molecule->bonds(mol.id);
-    if (frustrumCulling(mol)) {
-      paintBonds(painter, mol, i, bonds);
-      paintMol(painter, mol);
+  for (unsigned int i = 0; i < m_atoms.size(); ++i) {
+    auto atom = m_atoms[i];
+    const NeighborListType bonds = m_molecule->bonds(atom.id);
+    if (frustrumCulling(atom)) {
+      paintBonds(painter, atom, i, bonds);
+      paintCore(painter, atom);
     }
   }
 }
 
 void SVG::render()
 {
+  setOptions();
   if (defaultChecks())
     return;
   QString filename = QFileDialog::getSaveFileName(
     qobject_cast<QWidget*>(parent()), tr("Save File"), QDir::homePath(),
     tr("SVG (*.svg)"));
+  if (!filename.endsWith(".svg", Qt::CaseInsensitive))
+    filename.append(".svg");
+
   QFile file(filename);
   if (!file.open(QIODevice::WriteOnly))
     return;
--- avogadro/qtplugins/svg/svg.h
+++ avogadro/qtplugins/svg/svg.h
@@ -55,16 +55,16 @@ private slots:
   void render();
 
 private:
-  struct Mol
+  struct SVGAtom
   {
     Eigen::Vector3f pos_model, pos_view, pos_image;
-    unsigned int r;
+    unsigned int radius;
     unsigned int id;
 
-    Mol(Vector3f m, Vector3f v, unsigned int i, unsigned int bonds)
+    SVGAtom(Vector3f m, Vector3f v, unsigned int i, unsigned int bonds)
       : pos_model(m), pos_view(v), id(i)
     {
-      r = (bonds <= 0 ? 1 : bonds);
+      radius = (bonds <= 0 ? 1 : bonds);
     }
   };
   QtGui::Molecule* m_molecule;
@@ -74,23 +74,28 @@ private:
   QAction* m_action;
   int m_width, m_height;
   Eigen::Vector3f m_min, m_max;
-  std::vector<Mol> m_mols;
+  std::vector<SVGAtom> m_atoms;
   std::vector<Eigen::Vector4f> m_frustrum;
   std::map<unsigned int, unsigned int> m_idToindex;
 
-  static const float RADIUS, PEN_WIDTH_MOL, PEN_WIDTH_BOND, OFF_SET_PARALEL,
-    IMAGE_PADDING;
+  static const float DEFAULT_RADIUS, DEFAULT_PEN_WIDTH_MOL,
+    DEFAULT_PEN_WIDTH_BOND, DEFAULT_OFF_SET_PARALEL, DEFAULT_IMAGE_PADDING;
+  static const Vector3ub DEFAULT_BOND_COLOR;
 
+  float m_radius, m_penWidthMol, m_penWidthBond, m_offSetParalel,
+    m_ImagePadding;
+  Vector3ub m_BondColor;
   void paintSVG(QPainter& painter);
-  void paintMol(QPainter& painter, const Mol& mol);
-  void paintBonds(QPainter& painter, const Mol& mol, unsigned int i,
+  void paintCore(QPainter& painter, const SVGAtom& atom);
+  void paintBonds(QPainter& painter, const SVGAtom& atom, unsigned int i,
                   const NeighborListType& bonds);
 
   bool defaultChecks();
+  void setOptions();
   void getPositions();
   void calculateCamera();
-  bool frustrumCulling(const Mol& mol);
-  Eigen::Vector3f posToImage(const Vector3f& mol, float r);
+  bool frustrumCulling(const SVGAtom& atom);
+  Eigen::Vector3f posToSVGImage(const SVGAtom& atom);
 };
 } // namespace QtPlugins
 } // namespace Avogadro
