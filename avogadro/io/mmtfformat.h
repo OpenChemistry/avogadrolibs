3bc7fe1c1b2ab425b4092ed12b13b6a77c7f8bb7
Geoff Hutchison <geoff.hutchison@gmail.com>
2021-06-21 17:25:28 -0400

    Update headers
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- avogadro/io/mmtfformat.h
+++ avogadro/io/mmtfformat.h
@@ -1,17 +1,6 @@
 /******************************************************************************
-
   This source file is part of the Avogadro project.
-
-  Copyright 2012 Kitware, Inc.
-
-  This source code is released under the New BSD License, (the "License").
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
+  This source code is released under the 3-Clause BSD License, (see "LICENSE").
 ******************************************************************************/
 
 #ifndef AVOGADRO_IO_MMTFFORMAT_H
--- avogadro/io/pdbformat.cpp
+++ avogadro/io/pdbformat.cpp
@@ -1,3 +1,8 @@
+/******************************************************************************
+  This source file is part of the Avogadro project.
+  This source code is released under the 3-Clause BSD License, (see "LICENSE").
+******************************************************************************/
+
 #include "pdbformat.h"
 
 #include <avogadro/core/elements.h>
