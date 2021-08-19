ab61d575a2a595912659e86d7451fde89c00f820
Geoff Hutchison <geoff.hutchison@gmail.com>
2021-08-19 17:44:09 -0400

    Starting point - gives previews and inserts elements
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

---
+++ avogadro/qtplugins/templatetool/templatetool.h
@@ -0,0 +1,123 @@
+/******************************************************************************
+
+  This source file is part of the Avogadro project.
+
+  Copyright 2012-16 Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+
+#ifndef AVOGADRO_QTPLUGINS_TEMPLATE_H
+#define AVOGADRO_QTPLUGINS_TEMPLATE_H
+
+#include <avogadro/qtgui/toolplugin.h>
+
+#include <avogadro/core/avogadrocore.h>
+#include <avogadro/qtgui/molecule.h>
+#include <avogadro/rendering/primitive.h>
+
+#include <QtCore/QPoint>
+
+namespace Avogadro {
+namespace QtPlugins {
+class TemplateToolWidget;
+
+/**
+ * @class Template template.h <avogadro/qtplugins/template/template.h>
+ * @brief The Template tool inserts fragments, including metal centers.
+ * @author Geoffrey R. Hutchison
+ */
+class TemplateTool : public QtGui::ToolPlugin
+{
+  Q_OBJECT
+public:
+  explicit TemplateTool(QObject* parent_ = NULL);
+  ~TemplateTool();
+
+  QString name() const override { return tr("Template tool"); }
+  QString description() const override { return tr("Template tool"); }
+  unsigned char priority() const override { return 21; }
+  QAction* activateAction() const override { return m_activateAction; }
+  QWidget* toolWidget() const override;
+
+  void setMolecule(QtGui::Molecule* mol) override
+  {
+    if (mol)
+      m_molecule = mol->undoMolecule();
+  }
+
+  void setEditMolecule(QtGui::RWMolecule* mol) override
+  {
+    m_molecule = mol;
+  }
+
+  void setGLWidget(QtOpenGL::GLWidget* widget) override
+  {
+    m_glWidget = widget;
+  }
+
+  void setGLRenderer(Rendering::GLRenderer* renderer) override
+  {
+    m_renderer = renderer;
+  }
+
+  QUndoCommand* mousePressEvent(QMouseEvent* e) override;
+  QUndoCommand* mouseReleaseEvent(QMouseEvent* e) override;
+  QUndoCommand* mouseMoveEvent(QMouseEvent* e) override;
+  QUndoCommand* keyPressEvent(QKeyEvent* e) override;
+
+  void draw(Rendering::GroupNode& node) override;
+
+private slots:
+  void clearKeyPressBuffer() { m_keyPressBuffer.clear(); }
+
+private:
+  /**
+   * Update the currently pressed buttons, accounting for modifier keys.
+   * @todo Account for modifier keys.
+   */
+  void updatePressedButtons(QMouseEvent*, bool release);
+
+  /**
+   * Reset all state for this tool.
+   */
+  void reset();
+
+  void emptyLeftClick(QMouseEvent* e);
+  void atomLeftClick(QMouseEvent* e);
+  void bondLeftClick(QMouseEvent* e);
+
+  void atomRightClick(QMouseEvent* e);
+  void bondRightClick(QMouseEvent* e);
+
+  void atomLeftDrag(QMouseEvent* e);
+
+  QAction* m_activateAction;
+  QtGui::RWMolecule* m_molecule;
+  QtOpenGL::GLWidget* m_glWidget;
+  Rendering::GLRenderer* m_renderer;
+  TemplateToolWidget* m_toolWidget;
+  Rendering::Identifier m_clickedObject;
+  Rendering::Identifier m_newObject;
+  Rendering::Identifier m_bondedAtom;
+  Qt::MouseButtons m_pressedButtons;
+  QPoint m_clickPosition;
+  unsigned char m_clickedAtomicNumber;
+  bool m_bondAdded;
+  bool m_fixValenceLater;
+  QString m_keyPressBuffer;
+
+  Real m_bondDistance;
+};
+
+} // namespace QtPlugins
+} // namespace Avogadro
+
+#endif // AVOGADRO_QTPLUGINS_NAVIGATOR_H
