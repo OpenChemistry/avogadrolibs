0319260cd10107480999facef1d5ae08dcb9ce58
Aritz Erkiaga <aerkiaga3@gmail.com>
2022-03-03 19:31:55 +0100

    Implement O(n) bond perception
    Signed-off-by: Aritz Erkiaga <aerkiaga3@gmail.com>

--- avogadro/core/molecule.cpp
+++ avogadro/core/molecule.cpp
@@ -783,39 +783,90 @@ void Molecule::setVibrationLx(const Array<Array<Vector3>>& lx)
   m_vibrationLx = lx;
 }
 
-// bond perception code ported from VTK's vtkSimpleBondPerceiver class
 void Molecule::perceiveBondsSimple(const double tolerance, const double min)
 {
   // check for coordinates
-  if (m_positions3d.size() != atomCount())
+  if (m_positions3d.size() != atomCount() || m_positions3d.size() < 2)
     return;
 
   // cache atomic radii
   std::vector<double> radii(atomCount());
+  double max_radius = 0.0;
   for (size_t i = 0; i < radii.size(); i++) {
     radii[i] = Elements::radiusCovalent(atomicNumber(i));
     if (radii[i] <= 0.0)
       radii[i] = 2.0;
+    if (radii[i] > max_radius)
+      max_radius = radii[i];
+  }
+
+  // find molecule bounding box
+  Vector3 min_pos = m_positions3d[0];
+  Vector3 max_pos = m_positions3d[0];
+  for (Index i = 1; i < atomCount(); i++) {
+    Vector3 ipos = m_positions3d[i];
+    for (size_t c = 0; c < 3; c++) {
+      min_pos(c) = std::min(ipos(c), min_pos(c));
+      max_pos(c) = std::max(ipos(c), max_pos(c));
+    }
+  }
+
+  // group atoms into cubic bins so that each atom is only checked against
+  // other atoms inside bins within a 3-dimensional Moore neighborhood
+  double bin_size = 2.0 * max_radius + tolerance;
+  std::array<size_t, 3> bin_count;
+  for (size_t c = 0; c < 3; c++)
+    bin_count[c] = std::floor((max_pos(c) + 0.1 - min_pos(c)) / bin_size) + 1;
+  std::vector<std::vector<std::vector<std::vector<Index>>>> bins(
+    bin_count[0], std::vector<std::vector<std::vector<Index>>>(
+      bin_count[1], std::vector<std::vector<Index>>(
+        bin_count[2], std::vector<Index>()
+      )
+    )
+  );
+  std::vector<std::array<size_t, 3>> atoms_bin(atomCount());
+  for (Index i = 1; i < atomCount(); i++) {
+    Vector3 ipos = m_positions3d[i];
+    std::array<size_t, 3> bin_index;
+    for (size_t c = 0; c < 3; c++) {
+      bin_index[c] = std::floor((ipos(c) - min_pos(c)) / bin_size);
+    }
+    bins.at(bin_index[0]).at(bin_index[1]).at(bin_index[2]).push_back(i);
+    atoms_bin.push_back(bin_index);
   }
 
   // check for bonds
+  // O(n) average-case, O(n^2) worst-case
+  // note that the "worst case" here would need to be an invalid molecule
   for (Index i = 0; i < atomCount(); i++) {
     Vector3 ipos = m_positions3d[i];
-    for (Index j = i + 1; j < atomCount(); j++) {
-      double cutoff = radii[i] + radii[j] + tolerance;
-      Vector3 jpos = m_positions3d[j];
-      Vector3 diff = jpos - ipos;
+    std::array<size_t, 3> bin_index = atoms_bin[i];
+    for (Index xi = std::max(size_t(0), bin_index[0] - 1);
+        xi < std::min(bin_count[0], bin_index[0] + 2); xi++) {
+      for (Index yi = std::max(size_t(0), bin_index[1] - 1);
+          yi < std::min(bin_count[1], bin_index[1] + 2); yi++) {
+        for (Index zi = std::max(size_t(0), bin_index[2] - 1);
+            zi < std::min(bin_count[2], bin_index[2] + 2); zi++) {
+          std::vector<Index> bin = bins[xi][yi][zi];
+          for (size_t bj = 0; bj < bin.size(); ++bj) {
+            Index j = bin[bj];
+            double cutoff = radii[i] + radii[j] + tolerance;
+            Vector3 jpos = m_positions3d[j];
+            Vector3 diff = jpos - ipos;
 
-      if (std::fabs(diff[0]) > cutoff || std::fabs(diff[1]) > cutoff ||
-          std::fabs(diff[2]) > cutoff ||
-          (atomicNumber(i) == 1 && atomicNumber(j) == 1))
-        continue;
+            if (std::fabs(diff[0]) > cutoff || std::fabs(diff[1]) > cutoff ||
+                std::fabs(diff[2]) > cutoff ||
+                (atomicNumber(i) == 1 && atomicNumber(j) == 1))
+              continue;
 
-      // check radius and add bond if needed
-      double cutoffSq = cutoff * cutoff;
-      double diffsq = diff.squaredNorm();
-      if (diffsq < cutoffSq && diffsq > min * min)
-        addBond(atom(i), atom(j), 1);
+            // check radius and add bond if needed
+            double cutoffSq = cutoff * cutoff;
+            double diffsq = diff.squaredNorm();
+            if (diffsq < cutoffSq && diffsq > min * min)
+              addBond(atom(i), atom(j), 1);
+          }
+        }
+      }
     }
   }
 }
