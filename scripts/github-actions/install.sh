daa3b05077f7fd8df55bdec8edc510381cc933f4
Geoff Hutchison <geoff.hutchison@gmail.com>
2022-04-23 12:49:03 -0400

    Try to install Eigen on Windows and Ubuntu (#867)
    * Install Eigen before building Python wheels
    
    Should allow more modern CMake usage #866
    
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- scripts/github-actions/install.sh
+++ scripts/github-actions/install.sh
@@ -4,7 +4,14 @@ set -ev
 pip install cibuildwheel==2.0.0
 
 if [[ $RUNNER_OS == "Windows" ]]; then
-    git clone --recursive -b 3.3.9 --depth 1 https://gitlab.com/libeigen/eigen /c/eigen
+    git clone --recursive -b 3.4.0 --depth 1 https://gitlab.com/libeigen/eigen /c/eigen
+    cd /c/eigen
+      mkdir build
+      cd build
+        cmake .. -DCMAKE_INSTALL_PREFIX=/c/eigen-34
+        cmake --build . --target install 
 elif [[ $RUNNER_OS == "macOS" ]]; then
     brew install eigen
+else
+    sudo apt install libeigen3-dev
 fi
