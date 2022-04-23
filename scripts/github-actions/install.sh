f71c599a9a32e60d99f82e9a2b76d788afe4bd0a
Geoff Hutchison <geoff.hutchison@gmail.com>
2022-04-23 10:05:14 -0400

    Try to install Eigen on Windows and Ubuntu
    Should allow more modern CMake usage #866
    
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- scripts/github-actions/install.sh
+++ scripts/github-actions/install.sh
@@ -4,7 +4,15 @@ set -ev
 pip install cibuildwheel==2.0.0
 
 if [[ $RUNNER_OS == "Windows" ]]; then
-    git clone --recursive -b 3.3.9 --depth 1 https://gitlab.com/libeigen/eigen /c/eigen
+    git clone --recursive -b 3.4.0 --depth 1 https://gitlab.com/libeigen/eigen /c/eigen
+    cd /cd/eigen
+      mkdir build
+      cd build
+        cmake ..
+        make
+        make install 
 elif [[ $RUNNER_OS == "macOS" ]]; then
     brew install eigen
+else
+    sudo apt install libeigen3-dev
 fi
