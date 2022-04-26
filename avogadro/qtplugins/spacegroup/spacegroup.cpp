bdb8161e6aacda1f6b66a57d0d883adf7491376c
Geoff Hutchison <geoff.hutchison@gmail.com>
2022-04-25 23:54:18 -0400

    If the spacegroup / Hall number is set, don't ask what it is
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- avogadro/qtplugins/spacegroup/spacegroup.cpp
+++ avogadro/qtplugins/spacegroup/spacegroup.cpp
@@ -1,17 +1,6 @@
 /******************************************************************************
-
   This source file is part of the Avogadro project.
-
-  Copyright 2016 Kitware, Inc.
-
-  This source code is released under the New BSD License, (the "License").
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
+  This source code is released under the 3-Clause BSD License, (see "LICENSE").
 ******************************************************************************/
 
 #include "spacegroup.h"
@@ -277,8 +266,11 @@ void SpaceGroup::symmetrize()
 
 void SpaceGroup::fillUnitCell()
 {
-  // Ask the user to select a space group
-  unsigned short hallNumber = selectSpaceGroup();
+  unsigned short hallNumber = m_molecule->hallNumber();
+
+  // If it's not set, ask the user to select a space group
+  if (hallNumber == 0)
+    hallNumber = selectSpaceGroup();
   // If the hall number is zero, the user canceled
   if (hallNumber == 0)
     return;
