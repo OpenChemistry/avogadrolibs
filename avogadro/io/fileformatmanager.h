7514a59795df07b69ec3919446fcc048348165ba
Geoff Hutchison <geoff.hutchison@gmail.com>
2022-06-08 17:32:26 -0400

    Avoid strcasecmp which is only defined by POSIX
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- avogadro/io/fileformatmanager.h
+++ avogadro/io/fileformatmanager.h
@@ -1,17 +1,6 @@
 /******************************************************************************
-
   This source file is part of the Avogadro project.
-
-  Copyright 2013 Kitware, Inc.
-
-  This source code is released under the New BSD License, (the "License").
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
+  This source code is released under the 3-Clause BSD License, (see "LICENSE").
 ******************************************************************************/
 
 #ifndef AVOGADRO_IO_FILEFORMATMANAGER_H
@@ -31,15 +20,29 @@ class Molecule;
 }
 namespace Io {
 
+/**
+ * @struct CaseInsensitiveComparator fileformatmanager.h
+ * <avogadro/io/fileformatmanager.h>
+ * @brief Class to handle case-insensitive comparisons of file extensions.
+ * Adapted from https://stackoverflow.com/a/3009806/131896
+ **/
 struct CaseInsensitiveComparator
 {
-  bool operator()(const std::string& a, const std::string& b) const noexcept
+  // case-independent (ci) compare_less binary function
+  struct lowerCaseCompare
   {
-    return ::strcasecmp(a.c_str(), b.c_str()) < 0;
+    bool operator()(const unsigned char& c1, const unsigned char& c2) const
+    {
+      return tolower(c1) < tolower(c2);
+    }
+  };
+  bool operator()(const std::string& s1, const std::string& s2) const noexcept
+  {
+    return std::lexicographical_compare(s1.begin(), s1.end(), // source range
+                                        s2.begin(), s2.end(), // dest range
+                                        lowerCaseCompare());  // comparison
   }
 };
-
-
 
 /**
  * @class FileFormatManager fileformatmanager.h
@@ -251,7 +254,8 @@ public:
 
 private:
   typedef std::vector<size_t> FormatIdVector;
-  typedef std::map<std::string, FormatIdVector, CaseInsensitiveComparator> FormatIdMap;
+  typedef std::map<std::string, FormatIdVector, CaseInsensitiveComparator>
+    FormatIdMap;
 
   FileFormatManager();
   ~FileFormatManager();
@@ -336,7 +340,7 @@ private:
   std::string m_error;
 };
 
-} // end Io namespace
-} // end Avogadro namespace
+} // namespace Io
+} // namespace Avogadro
 
 #endif // AVOGADRO_IO_FILEFORMATMANAGER_H
