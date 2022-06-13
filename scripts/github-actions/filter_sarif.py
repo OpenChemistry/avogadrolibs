45e68cd8439d70ec5dacd8fe44e98299e640d4e5
Geoff Hutchison <geoff.hutchison@gmail.com>
2022-06-13 15:52:28 -0400

    Run black
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- scripts/github-actions/filter_sarif.py
+++ scripts/github-actions/filter_sarif.py
@@ -3,7 +3,6 @@
 # From https://github.com/zbazztian/filter-sarif/blob/master/filter_sarif.py
 # Some modifications by Geoffrey Hutchison
 
-import sys
 import argparse
 import json
 import re
@@ -19,20 +18,20 @@ def match_path_and_rule(path, rule, patterns):
 
 
 def parse_pattern(line):
-    sep_char = ':'
-    esc_char = '\\'
-    file_pattern = ''
-    rule_pattern = ''
+    sep_char = ":"
+    esc_char = "\\"
+    file_pattern = ""
+    rule_pattern = ""
     seen_separator = False
     sign = True
 
     # inclusion or exclusion pattern?
     uline = line
     if line:
-        if line[0] == '-':
+        if line[0] == "-":
             sign = False
             uline = line[1:]
-        elif line[0] == '+':
+        elif line[0] == "+":
             uline = line[1:]
 
     i = 0
@@ -41,21 +40,25 @@ def parse_pattern(line):
         i = i + 1
         if char == sep_char:
             if seen_separator:
-                raise Exception('Invalid pattern: "' + line + '" Contains more than one separator!')
+                raise Exception(
+                    'Invalid pattern: "' + line + '" Contains more than one separator!'
+                )
             seen_separator = True
             continue
-        elif char == esc_char:
+
+        if char == esc_char:
             next_char = uline[i] if (i < len(uline)) else None
-            if next_char in ['+' , '-', esc_char, sep_char]:
+            if next_char in ["+", "-", esc_char, sep_char]:
                 i = i + 1
                 char = next_char
+
         if seen_separator:
             rule_pattern = rule_pattern + char
         else:
             file_pattern = file_pattern + char
 
     if not rule_pattern:
-        rule_pattern = '**'
+        rule_pattern = "**"
 
     return sign, file_pattern, rule_pattern
 
@@ -64,79 +67,68 @@ def filter_sarif(args):
     if args.split_lines:
         tmp = []
         for pattern in args.patterns:
-            tmp = tmp + re.split('\r?\n', pattern)
+            tmp = tmp + re.split("\r?\n", pattern)
         args.patterns = tmp
 
     args.patterns = [parse_pattern(pattern) for pattern in args.patterns if pattern]
 
-    print('Given patterns:')
+    print("Given patterns:")
     for sign, file_pattern, rule_pattern in args.patterns:
-        sign_text = 'positive' if sign else 'negative'
-        print(f'files: {file_pattern}    rules: {rule_pattern} ({sign_text})')
+        sign_text = "positive" if sign else "negative"
+        print(f"files: {file_pattern}    rules: {rule_pattern} ({sign_text})")
 
-    with open(args.input, 'r') as file:
+    with open(args.input, "r", encoding="UTF-8") as file:
         sarif = json.load(file)
 
-    for run in sarif.get('runs', []):
-        if run.get('results', []):
+    for run in sarif.get("runs", []):
+        if run.get("results", []):
             new_results = []
-            for result in run['results']:
-                if result.get('locations', []):
+            for result in run["results"]:
+                if result.get("locations", []):
                     new_locations = []
-                    for location in result['locations']:
+                    for location in result["locations"]:
                         # TODO: The uri field is optional. We might have to fetch the actual uri from "artifacts" via "index"
                         # (see https://github.com/microsoft/sarif-tutorials/blob/main/docs/2-Basics.md#-linking-results-to-artifacts)
-                        uri = location.get('physicalLocation', {}).get('artifactLocation', {}).get('uri', None)
+                        uri = (
+                            location.get("physicalLocation", {})
+                            .get("artifactLocation", {})
+                            .get("uri", None)
+                        )
                         # TODO: The ruleId field is optional and potentially ambiguous. We might have to fetch the actual
                         # ruleId from the rule metadata via the ruleIndex field.
                         # (see https://github.com/microsoft/sarif-tutorials/blob/main/docs/2-Basics.md#rule-metadata)
-                        rule_id = result['ruleId']
-                        if uri is None or match_path_and_rule(uri, rule_id, args.patterns):
+                        rule_id = result["ruleId"]
+                        if uri is None or match_path_and_rule(
+                            uri, rule_id, args.patterns
+                        ):
                             new_locations.append(location)
-                    result['locations'] = new_locations
+                    result["locations"] = new_locations
                     if new_locations:
                         new_results.append(result)
                 else:
                     # locations array doesn't exist or is empty, so we can't match on anything
                     # therefore, we include the result in the output
                     new_results.append(result)
-            run['results'] = new_results
+            run["results"] = new_results
 
-    with open(args.output, 'w', encoding="UTF-8") as file:
+    with open(args.output, "w", encoding="UTF-8") as file:
         json.dump(sarif, file, indent=args.indent)
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        prog='filter-sarif'
-    )
+    parser = argparse.ArgumentParser(prog="filter-sarif")
+    parser.add_argument("--input", help="Input SARIF file", required=True)
+    parser.add_argument("--output", help="Output SARIF file", required=True)
     parser.add_argument(
-        '--input',
-        help='Input SARIF file',
-        required=True
-    )
-    parser.add_argument(
-        '--output',
-        help='Output SARIF file',
-        required=True
-    )
-    parser.add_argument(
-        '--split-lines',
+        "--split-lines",
         default=False,
-        action='store_true',
-        help='Split given patterns on newlines.'
+        action="store_true",
+        help="Split given patterns on newlines.",
     )
     parser.add_argument(
-        '--indent',
-        default=None, 
-        type=int,
-        help='Indentation level for JSON output.'
+        "--indent", default=None, type=int, help="Indentation level for JSON output."
     )
-    parser.add_argument(
-        'patterns',
-        help='Inclusion and exclusion patterns.',
-        nargs='+'
-    )
+    parser.add_argument("patterns", help="Inclusion and exclusion patterns.", nargs="+")
 
     def print_usage():
         print(parser.format_usage())
@@ -144,5 +136,6 @@ def main():
     args = parser.parse_args()
     filter_sarif(args)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
