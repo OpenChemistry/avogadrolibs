c29ba943b39790ae04861e7aafc81969daf92ef2
Geoff Hutchison <geoff.hutchison@gmail.com>
2022-06-17 15:30:31 -0400

    Fix script
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- avogadro/qtplugins/scriptfileformats/formatScripts/zyx.py
+++ avogadro/qtplugins/scriptfileformats/formatScripts/zyx.py
@@ -1,8 +1,5 @@
-/******************************************************************************
-  This source file is part of the Avogadro project.
-  This source code is released under the 3-Clause BSD License, (see "LICENSE").
-******************************************************************************/
-"""
+#  This source file is part of the Avogadro project.
+#  This source code is released under the 3-Clause BSD License, (see "LICENSE").
 
 import argparse
 import json
