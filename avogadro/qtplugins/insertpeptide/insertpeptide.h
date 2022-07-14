2bef579d77d6b89726a88f1076cb84d4b4d37d82
Geoff Hutchison <geoff.hutchison@gmail.com>
2022-07-14 18:03:06 -0400

    More work
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- avogadro/qtplugins/insertpeptide/insertpeptide.h
+++ avogadro/qtplugins/insertpeptide/insertpeptide.h
@@ -3,37 +3,31 @@
   This source code is released under the 3-Clause BSD License, (see "LICENSE").
 ******************************************************************************/
 
-#ifndef AVOGADRO_QTPLUGINS_INSERTFRAGMENT_H
-#define AVOGADRO_QTPLUGINS_INSERTFRAGMENT_H
+#ifndef AVOGADRO_QTPLUGINS_INSERTPEPTIDE_H
+#define AVOGADRO_QTPLUGINS_INSERTPEPTIDE_H
 
 #include <avogadro/qtgui/extensionplugin.h>
 
-#include <QtCore/QMap>
-
-#include <string>
-
 namespace Avogadro {
-namespace Io {
-class FileFormat;
-}
 namespace QtPlugins {
-class InsertFragmentDialog;  
-}
 
-namespace QtPlugins {
+class InsertPeptideDialog;
 
 /**
- * @brief Load single-line molecule descriptors through an input dialog.
+ * @brief Insert oligopeptide sequences
  */
-class InsertFragment : public QtGui::ExtensionPlugin
+class InsertPeptide : public QtGui::ExtensionPlugin
 {
   Q_OBJECT
 public:
-  explicit InsertFragment(QObject* parent_ = nullptr);
-  ~InsertFragment() override;
+  explicit InsertPeptide(QObject* parent_ = nullptr);
+  ~InsertPeptide() override;
 
-  QString name() const override { return tr("InsertFragment"); }
-  QString description() const override;
+  QString name() const override { return tr("InsertPeptide"); }
+  QString description() const override
+  {
+    return tr("Insert oligopeptide sequences.");
+  }
   QList<QAction*> actions() const override;
   QStringList menuPath(QAction*) const override;
 
@@ -42,25 +36,14 @@ public slots:
 
 private slots:
   void showDialog();
-  void performInsert(const QString &fileName, bool crystal);
+  void performInsert(const QString& sequence);
 
 private:
   QList<QAction*> m_actions;
-  InsertFragmentDialog* m_moleculeDialog;
-  InsertFragmentDialog* m_crystalDialog;
-  /// Maps identifier to extension:
-  QMap<QString, std::string> m_formats;
-
   QtGui::Molecule* m_molecule;
-  Io::FileFormat* m_reader;
 };
-
-inline QString InsertFragment::description() const
-{
-  return tr("Insert molecular fragments for building larger molecules.");
-}
 
 } // namespace QtPlugins
 } // namespace Avogadro
 
-#endif // AVOGADRO_QTPLUGINS_INSERTFRAGMENT_H
+#endif // AVOGADRO_QTPLUGINS_INSERTPEPTIDE_H
