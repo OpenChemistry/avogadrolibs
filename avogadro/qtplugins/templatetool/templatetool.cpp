020d117fd331802841c9fc45e9cdbe2ca2129529
Aritz Erkiaga <38228836+aerkiaga@users.noreply.github.com>
2022-08-16 02:32:09 +0200

    Estimate bond distances in template tool (#1099)
    * Estimate bond distances for all ligands
    
    Signed-off-by: Aritz Erkiaga <aerkiaga3@gmail.com>

--- avogadro/qtplugins/templatetool/templatetool.cpp
+++ avogadro/qtplugins/templatetool/templatetool.cpp
@@ -397,6 +397,25 @@ void TemplateTool::atomLeftClick(QMouseEvent *e)
       moleculeLigandOutVector += newPos - m_molecule->atomPosition3d(moleculeCenterIndex);
     }
     
+    // Estimate and try to realize bond distances
+    Vector3 displacement(0.0, 0.0, 0.0);
+    for (size_t i = 0; i < templateLigandIndices.size(); i++) {
+      unsigned char ligandAtomicNumber = templateMolecule.atomicNumber(templateLigandIndices[i]);
+      ligandAtomicNumber = (ligandAtomicNumber == 0)? 6 : ligandAtomicNumber;
+      // Estimate as the sum of covalent radii
+      double bondDistance = Elements::radiusCovalent(ligandAtomicNumber)
+        + Elements::radiusCovalent(m_molecule->atomicNumber(moleculeCenterIndex));
+      Vector3 inVector = templateMolecule.atomPosition3d(templateDummyIndex)
+        - templateMolecule.atomPosition3d(templateLigandIndices[i]);
+      Vector3 correctionVector = inVector;
+      correctionVector.normalize();
+      correctionVector *= bondDistance - inVector.norm();
+      displacement += correctionVector;
+    }
+    displacement *= 1.0 / templateLigandIndices.size();
+    Vector3 newPos = templateMolecule.atomPosition3d(templateDummyIndex) + displacement;
+    templateMolecule.setAtomPosition3d(templateDummyIndex, newPos);
+    
     // Translate template so dummy atom is brought to center atom
     for (size_t i = 0; i < templateMolecule.atomCount(); i++) {
       if (i != templateDummyIndex) {
