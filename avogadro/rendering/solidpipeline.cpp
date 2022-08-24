e831a4a8ec3835cf1136a6aab83608fd660c7b1f
Aritz Erkiaga <aerkiaga3@gmail.com>
2022-08-24 16:38:14 +0200

    Fix artifacts due to texture parameters
    Signed-off-by: Aritz Erkiaga <aerkiaga3@gmail.com>

--- avogadro/rendering/solidpipeline.cpp
+++ avogadro/rendering/solidpipeline.cpp
@@ -31,8 +31,10 @@ void initializeFramebuffer(GLuint *outFBO, GLuint *texRGB, GLuint *texDepth)
 
   glGenTextures(1, texDepth);
   glBindTexture(GL_TEXTURE_2D, *texDepth);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, *texDepth, 0);
 }
 
