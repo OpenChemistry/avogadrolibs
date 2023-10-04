84588cf587b1e9825be68707b718030109e1711d
aditya <adityaomar33@gmail.com>
2023-10-05 01:55:40 +0530

    Added cjson.py which can be used to read cjson current method added is get_atoms_coords which will get the co-ordinates of all atoms
    Signed-off-by: aditya <adityaomar33@gmail.com>
    Signed-off-by: Aditya Omar <adityaomar33@gmail.com>

---
+++ python/avogadro/cjson.py
@@ -0,0 +1,21 @@
+import json
+class Cjson:
+    def __init__(self):
+        print("cjson configured successfully start your operations")
+    def __open_file(self, filePath):
+        with open(filePath, 'r') as cjsonFile:
+            py_dict_data = json.load(cjsonFile)
+            return py_dict_data
+    def __to_cjson(self, element_coords):
+        dict = {"element-coordinates" :element_coords}
+        cjsonData = json.dumps(dict, indent=4)
+        print(cjsonData)
+    def get_atoms_coords(self,filePath):
+        data = self.__open_file(filePath)
+        coords = data["atoms"]["coords"]["3d"]
+        elements = data["atoms"]["elements"]["number"]
+        # element_coords = {}
+        element_coords = [(*coords[i*3:(i+1)*3], elements[i]) for i in range(0, int(len(coords) / 3))]
+        print(type(element_coords))
+        self.__to_cjson(element_coords)
+
