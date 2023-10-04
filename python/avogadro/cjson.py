5d8044a15c1a92d53714d6bd3c10f4e38d252072
aditya <adityaomar33@gmail.com>
2023-10-05 02:08:01 +0530

    changed dict name as it is a keyword
    Signed-off-by: aditya <adityaomar33@gmail.com>
    Signed-off-by: Aditya Omar <adityaomar33@gmail.com>

--- python/avogadro/cjson.py
+++ python/avogadro/cjson.py
@@ -7,15 +7,13 @@ class Cjson:
             py_dict_data = json.load(cjsonFile)
             return py_dict_data
     def __to_cjson(self, element_coords):
-        dict = {"element-coordinates" :element_coords}
-        cjsonData = json.dumps(dict, indent=4)
+        cjson_dict = {"element-coordinates" :element_coords}
+        cjsonData = json.dumps(cjson_dict, indent=4)
         print(cjsonData)
     def get_atoms_coords(self,filePath):
         data = self.__open_file(filePath)
         coords = data["atoms"]["coords"]["3d"]
         elements = data["atoms"]["elements"]["number"]
-        # element_coords = {}
         element_coords = [(*coords[i*3:(i+1)*3], elements[i]) for i in range(0, int(len(coords) / 3))]
         print(type(element_coords))
         self.__to_cjson(element_coords)
-
