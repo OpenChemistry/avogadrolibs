5b810163c9cc9b2d42051d3fc790b3f964ec3a98
Aditya Omar <adityaomar33@gmail.com>
2023-10-12 02:30:40 +0530

    updated .gitignore file for python and removed debugging statements
    Signed-off-by: Aditya Omar <adityaomar33@gmail.com>

--- python/avogadro/cjson.py
+++ python/avogadro/cjson.py
@@ -1,7 +1,7 @@
 import json
 class Cjson:
     def __init__(self):
-        print("cjson configured successfully start your operations")
+        pass
     def __open_file(self, filePath):
         with open(filePath, 'r') as cjsonFile:
             py_dict_data = json.load(cjsonFile)
@@ -9,11 +9,10 @@ class Cjson:
     def __to_cjson(self, element_coords):
         cjson_dict = {"element-coordinates" :element_coords}
         cjsonData = json.dumps(cjson_dict, indent=4)
-        print(cjsonData)
+        return (cjsonData)
     def get_atoms_coords(self,filePath):
         data = self.__open_file(filePath)
         coords = data["atoms"]["coords"]["3d"]
         elements = data["atoms"]["elements"]["number"]
         element_coords = [(*coords[i*3:(i+1)*3], elements[i]) for i in range(0, int(len(coords) / 3))]
-        print(type(element_coords))
-        self.__to_cjson(element_coords)
+        return self.__to_cjson(element_coords)
