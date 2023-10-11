a34b31c3b974be643abdaaf4927ed780a0d17621
Geoff Hutchison <geoff.hutchison@gmail.com>
2023-10-11 12:27:03 -0400

    Validating filenames before opening files for read / write (#1367)
    * First step towards validating filenames
    
    * Add validation to the file format dialog
    
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- avogadro/io/fileformat.h
+++ avogadro/io/fileformat.h
@@ -66,6 +66,18 @@ public:
   virtual Operations supportedOperations() const = 0;
 
   /**
+   * @brief Validates the given file name.
+   *
+   * Checks if the filename contains any invalid characters (e.g. ..)
+   * Also checks if the filename contains a restricted name on Windows.
+   * e.g., CON, PRN, AUX, NUL, COM1, COM2, COM3, COM4, COM5, etc.
+   *
+   * @param fileName The name of the file to be validated.
+   * @return true if the file name is valid, false otherwise.
+   */
+  static bool validateFileName(const std::string& fileName);
+
+  /**
    * @brief Open the specified file in Read or Write mode.
    * @return True on success, false on failure.
    */
@@ -252,7 +264,7 @@ inline FileFormat::Operation operator|(FileFormat::Operation a,
                                             static_cast<int>(b));
 }
 
-} // end Io namespace
-} // end Avogadro namespace
+} // namespace Io
+} // namespace Avogadro
 
 #endif // AVOGADRO_IO_FILEFORMAT_H
