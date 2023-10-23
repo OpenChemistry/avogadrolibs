1eb19d0901327a362adaf1262880524bfb694ea5
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2023-10-24 01:34:16 +0530

    Edited the macro
    Signed-off-by: Atharva-Kanherkar <atharvakanherkar25@gmail.com>

--- avogadro/core/avogadrocore.h
+++ avogadro/core/avogadrocore.h
@@ -1,4 +1,4 @@
- /******************************************************************************
+/******************************************************************************
   This source file is part of the Avogadro project.
   This source code is released under the 3-Clause BSD License, (see "LICENSE").
 ******************************************************************************/
@@ -9,53 +9,45 @@
 #include <cstddef>
 #include <limits>
 
+/** Prevent compiler error when using std::numeric_limits<T>::max() */
+#if defined(_MSC_VER) && defined(max)
+#undef max
+#endif
+
 /**
  * This macro marks a parameter as unused. Its purpose is to disable the
  * compiler from emitting unused parameter warnings.
  */
 #define AVO_UNUSED(variable) (void)variable
 
+/**
+ * This macro marks a class as not copyable. It should be used in the private
+ * section of a class's declaration.
+ */
+#define AVO_DISABLE_COPY(Class)                                                \
+  Class(const Class&);                                                         \
+  Class& operator=(const Class&);
+
+ 
 namespace Avogadro {
 
-/** Typedef for a real number. */
 using Real = double;
-
-/** Typedef for indices and sizes. */
 using Index = std::size_t;
 constexpr Index MaxIndex = std::numeric_limits<Index>::max();
 
-/** Used to represent an invalid atomic number. */
 constexpr unsigned char InvalidElement = 255;
-
-/**
- * Minimum value for atomic numbers that represent custom, non-elemental
- * particles.
- */
 constexpr unsigned char CustomElementMin = 128;
-
-/**
- * Maximum value for atomic numbers that represent custom, non-elemental
- * particles.
- */
 constexpr unsigned char CustomElementMax = 254;
+constexpr unsigned char CustomElementCount = CustomElementMax - CustomElementMin + 1;
 
-/**
- * Count of atomic number values that are used to represent custom,
- * non-elemental particles.
- */
-constexpr unsigned char CustomElementCount =
-    CustomElementMax - CustomElementMin + 1;
-
-/**
- * @return True if @a atomicNumber denotes a custom element type.
- */
 namespace Core {
-constexpr bool isCustomElement(unsigned char atomicNumber) {
-    return atomicNumber >= CustomElementMin && atomicNumber <= CustomElementMax;
+inline bool isCustomElement(unsigned char atomicNumber)
+{
+  return atomicNumber >= CustomElementMin && atomicNumber <= CustomElementMax;
 }
 }
 
-/** Unit conversion factors. @{ */
+/** Unit conversion factors. */
 constexpr double PI_D = 3.141592653589793238462643;
 constexpr float PI_F = static_cast<float>(PI_D);
 constexpr Real PI = static_cast<Real>(PI_D);
@@ -75,7 +67,6 @@ constexpr Real BOHR_TO_ANGSTROM = static_cast<Real>(BOHR_TO_ANGSTROM_D);
 constexpr double ANGSTROM_TO_BOHR_D = 1.0 / BOHR_TO_ANGSTROM_D;
 constexpr float ANGSTROM_TO_BOHR_F = static_cast<float>(ANGSTROM_TO_BOHR_D);
 constexpr Real ANGSTROM_TO_BOHR = static_cast<Real>(ANGSTROM_TO_BOHR_D);
-/** @} */
 
 } // end Avogadro namespace
 
