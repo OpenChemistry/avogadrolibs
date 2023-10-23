609c7d974bfa04f270f71fb6ad75a305988b0f93
ItshMoh <mohan.kumar.min.22@itbhu.ac.in>
2023-10-23 14:36:37 +0530

    Use of constexpr instead of define
    Signed-off-by: ItshMoh <mohan.kumar.min.22@itbhu.ac.in>

--- avogadro/io/dcdformat.cpp
+++ avogadro/io/dcdformat.cpp
@@ -35,7 +35,7 @@ using Core::UnitCell;
 #endif
 
 #define DCD_EOF -1
-#define DCD_MAGIC 84
+constexpr int DCD_MAGIC = 84;
 #define DCD_IS_CHARMM 0x01
 #define DCD_HAS_4DIMS 0x02
 #define DCD_HAS_EXTRA_BLOCK 0x04
