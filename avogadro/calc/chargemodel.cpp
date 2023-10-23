a18ce3ae031177269bee23f0cfd9f32ef21831bf
Kenshiro15 <22bcs051@iiitdmj.ac.in>
2023-10-23 21:40:03 +0530

    Added constexpr, removed useless if block, replaced typedef to using.

--- avogadro/calc/chargemodel.cpp
+++ avogadro/calc/chargemodel.cpp
@@ -1,10 +1,9 @@
-/******************************************************************************
+ /******************************************************************************
   This source file is part of the Avogadro project.
   This source code is released under the 3-Clause BSD License, (see "LICENSE").
 ******************************************************************************/
 
 #include "chargemodel.h"
-
 #include <avogadro/core/array.h>
 #include <avogadro/core/molecule.h>
 
@@ -16,7 +15,7 @@ using Core::Molecule;
 namespace Calc {
 
 #ifndef M_PI
-#define M_PI 3.14159265358979323846
+constexpr double M_PI = 3.14159265358979323846;
 #endif
 
 ChargeModel::ChargeModel() : m_dielectric(1.0) {}
@@ -28,40 +27,38 @@ double ChargeModel::potential(Molecule& mol, const Vector3& point) const
   // default is to get the set of partial atomic charges
   const MatrixX charges = partialCharges(mol);
   // also get the positions of the atoms
-  const Array<Vector3> positions = mol.atomPositions3d();
+ const Array<Vector3> positions = mol.atomPositions3d();
 
-  // @todo: this is naive and inefficient
-  //  calculate the atoms within a cutoff distance
-  //  and sum the potentials
+  // @todo: This is naive and inefficient.
+  // Calculate the atoms within a cutoff distance
+  // and sum the potentials
 
   double potential = 0.0;
   for (unsigned int i = 0; i < charges.size(); ++i) {
     double distance = (positions[i] - point).norm();
     if (distance > 0.01) {
-      // drop small distances to avoid overflow
-      potential += charges(i,0) / distance;
+      // Drop small distances to avoid overflow
+      potential += charges(i, 0) / distance;
     }
   }
 
   return potential / m_dielectric;
 }
 
-Array<double> ChargeModel::potentials(Core::Molecule& mol,
-                                      const Array<Vector3>& points) const
-{
-  // This is naive and slow, but can be re-implemented by methods
-  // for batching
+Array<double> ChargeModel::potentials(Molecule& mol, const Array<Vector3>& points) const {
+  // This is naive and slow but can be re-implemented by methods for batching
   Array<double> potentials(points.size(), 0.0);
-  for(unsigned int i = 0; i < points.size(); ++i)
+  for (size_t i = 0; i < points.size(); ++i) {
     potentials[i] = potential(mol, points[i]);
+  }
   return potentials;
 }
 
-void ChargeModel::appendError(const std::string& errorString, bool newLine) const
-{
+void ChargeModel::appendError(const std::string& errorString, bool newLine) const {
   m_error += errorString;
-  if (newLine)
-    m_error += "\n";
+  if (newLine) {
+    m_error += '\n';
+  }
 }
 
 } // namespace Calc
