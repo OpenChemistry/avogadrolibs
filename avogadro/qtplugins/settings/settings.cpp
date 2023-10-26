43df6cf6acd9f26aefeb5eadfc3972abf5acca4d
research11111 <urgent.research@mailo.fr>
2023-10-26 13:16:03 +0200

    general settings for avogadro

---
+++ avogadro/qtplugins/settings/settings.cpp
@@ -0,0 +1,111 @@
+#include "settings.h"
+
+//#include <libarchive/archive.h>
+namespace Avogadro::QtPlugins {
+
+Settings::Settings(QObject* parent_)
+  : ExtensionPlugin(parent_), m_action(new QAction(this)), m_widget(nullptr)
+{
+  m_action->setEnabled(true);
+  m_action->setText(tr("Manage"));
+  connect(m_action, SIGNAL(triggered()), SLOT(showDialog()));
+}
+
+Settings::~Settings() = default;
+
+QList<QAction*> Settings::actions() const
+{
+  return QList<QAction*>() << m_action;
+}
+
+QStringList Settings::menuPath(QAction*) const
+{
+  return QStringList() << tr("&Extensions");
+}
+
+void PluginDownloader::showDialog()
+{
+  if (m_widget == nullptr) {
+        QWidget window;
+        QVBoxLayout layout(&window);
+        for (const QString& interpreter : detectPythonInterpreters()) {
+            QCheckBox* checkbox = new QCheckBox(interpreter, &window);
+            QStringList parts = interpreter.split("(").last().split(")").first().split(":");
+            QString envType = parts.first();
+            QString envName = parts.last();
+            QObject::connect(checkbox, &QCheckBox::stateChanged, [=](int state) {
+                if (state == Qt::Checked) {
+                    activateEnvironment(envType, envName);
+                }
+            });
+            layout.addWidget(checkbox);
+        }
+        m_widget = window;
+  }
+  m_widget->show();
+}
+
+QStringList detectPythonInterpreters() {
+    QStringList interpreters;
+    QProcess process;
+    QString pythonCmd = (QSysInfo::productType() == "windows") ? "where python" : "which python";
+    process.start(pythonCmd);
+    process.waitForFinished();
+    QString systemPythonPath = process.readAllStandardOutput();
+    if (!systemPythonPath.isEmpty()) {
+        interpreters << QString("Python (base): %1").arg(systemPythonPath.trimmed());
+    }
+    process.start("conda env list");
+    process.waitForFinished();
+    QString condaEnvsOutput = process.readAllStandardOutput();
+    auto lines = condaEnvsOutput.split('\n');
+    for (const QString& line : lines.mid(2)) {
+        if (!line.isEmpty()) {
+            auto parts = line.split(' ');
+            QString envName = parts[0];
+            QString envPath = parts.last();
+            interpreters << QString("Python (Conda: %1): %2").arg(envName, envPath);
+        }
+    }
+    process.start("virtualenv --list");
+    process.waitForFinished();
+    QString venvsOutput = process.readAllStandardOutput();
+    lines = venvsOutput.split('\n');
+    for (const QString& line : lines) {
+        if (!line.isEmpty()) {
+            interpreters << QString("Python (VirtualEnv: %1)").arg(line.trimmed());
+        }
+    }
+    return interpreters;
+}
+
+void activateEnvironment(const QString& envType, const QString& envName) {
+    QProcess process;
+    if (envType == "Conda") {
+        process.start("conda --version");
+        process.waitForFinished();
+        if (process.exitCode() != 0) {
+            QMessageBox::warning(nullptr, "Activation Failed", "Conda is not installed on this computer.");
+            return;
+        }
+        if (envName == "base") {
+            process.start("conda deactivate");
+        } else {
+            process.start(QString("conda activate %1").arg(envName));
+        }
+    } else if (envType == "VirtualEnv") {
+        if (envName == "base") {
+            process.start("deactivate");
+        } else {
+            process.start(QString("source %1/bin/activate").arg(envName));
+        }
+    }
+    process.waitForFinished();
+    if (process.exitCode() != 0) {
+        QMessageBox::warning(nullptr, "Activation Failed", "Failed to activate or deactivate environment");
+    }
+}
+void PluginDownloader::replyFinished(QNetworkReply*)
+{
+}
+}
---
+++ avogadro/qtplugins/settings/settings.h
@@ -0,0 +1,80 @@
+/******************************************************************************
+  This source file is part of the Avogadro project.
+  This source code is released under the 3-Clause BSD License, (see "LICENSE").
+******************************************************************************/
+
+#ifndef AVOGADRO_QTPLUGINS_SETTINGS_H
+#define AVOGADRO_QTPLUGINS_SETTINGS_H
+
+#include <QApplication>
+#include <QVBoxLayout>
+#include <QCheckBox>
+#include <QProcess>
+#include <QMessageBox>
+#include <QStringList>
+#include <QString>
+#include <QSysInfo>
+#include <QtCore/QList>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+#include <QAction>
+#include <QtWidgets/QInputDialog>
+#include <QtWidgets/QMessageBox>
+#include <QtWidgets/QProgressDialog>
+
+#include <avogadro/qtgui/extensionplugin.h>
+
+#include <avogadro/core/avogadrocore.h>
+
+#include <QtCore/QString>
+
+class QNetworkAccessManager;
+class QNetworkReply;
+class QProgressDialog;
+
+namespace Avogadro {
+namespace QtPlugins {
+
+class PluginManagerWidget;
+
+/**
+ * @brief Downloads Github repos and extracts their contents into a Avogadro
+ * folder for plugins, molecule data, etc..
+ */
+
+class Settings : public QtGui::ExtensionPlugin
+{
+  Q_OBJECT
+
+public:
+  explicit Settings(QObject* parent = nullptr);
+  ~Settings() override;
+
+  QString name() const override { return tr("Settings"); }
+
+  QString description() const override
+  {
+    return tr("General settings on avogadro");
+  }
+
+  QList<QAction*> actions() const override;
+
+  QStringList menuPath(QAction*) const override;
+
+public slots:
+
+
+private slots:
+  void showDialog();
+
+private:
+  QAction* m_action;
+  QNetworkAccessManager* m_network;
+  QWidget* m_widget;
+  QStringList detectPythonInterpreters();
+  void activateEnvironment(const QString& envType, const QString& envName);
+};
+}
+}
+
+#endif
