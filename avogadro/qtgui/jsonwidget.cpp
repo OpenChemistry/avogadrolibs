36a38e41b287819c997a537b0fd7d55f4c387b33
Geoff Hutchison <geoff.hutchison@gmail.com>
2023-11-25 12:22:51 -0500

    Fix empty window showing up for commands without an option dialog
    Thanks to the forum for reporting
    
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- avogadro/qtgui/jsonwidget.cpp
+++ avogadro/qtgui/jsonwidget.cpp
@@ -32,7 +32,7 @@ namespace Avogadro::QtGui {
 
 JsonWidget::JsonWidget(QWidget* parent_)
   : QWidget(parent_), m_molecule(nullptr), m_currentLayout(nullptr),
-    m_centralWidget(nullptr)
+    m_centralWidget(nullptr), m_empty(true), m_batchMode(false)
 {
 }
 
