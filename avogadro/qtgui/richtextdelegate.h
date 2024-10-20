8566f2c57050ea638cc976958815200856bb5135
Geoff Hutchison <geoff.hutchison@gmail.com>
2024-10-20 14:37:54 -0400

    Move RichTextDelegate to qtgui for use in other tools
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- avogadro/qtgui/richtextdelegate.h
+++ avogadro/qtgui/richtextdelegate.h
@@ -22,21 +22,22 @@
 #include <QTextDocument>
 
 namespace Avogadro {
-namespace QtPlugins {
+namespace QtGui {
 
 class RichTextDelegate : public QStyledItemDelegate
 {
   Q_OBJECT
 
 public:
-  RichTextDelegate(QObject* parent = nullptr)
-    : QStyledItemDelegate(parent){};
+  RichTextDelegate(QObject* parent = nullptr);
+
   QSize sizeHint(const QStyleOptionViewItem& o,
                  const QModelIndex& index) const override;
   void paint(QPainter* p, const QStyleOptionViewItem& o,
              const QModelIndex& index) const override;
 };
-}
-}
 
-#endif // AVOGADRO_QTPLUGINS_RICHTEXTDELEGATE_H
+} // namespace QtGui
+} // namespace Avogadro
+
+#endif // AVOGADRO_QTGUI_RICHTEXTDELEGATE_H
