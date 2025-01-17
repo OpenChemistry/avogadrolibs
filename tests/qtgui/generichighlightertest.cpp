062a38b037cc75ae2cfd71e02e0b5607a4d09197
Vaishnavi Bhandari <vaishnavibhandari.128@gmai.com>
2025-01-17 02:53:09 +0000

    Updated
    Signed-off-by: Vaishnavi Bhandari <vaishnavibhandari.128@gmai.com>

--- tests/qtgui/generichighlightertest.cpp
+++ tests/qtgui/generichighlightertest.cpp
@@ -48,22 +48,44 @@ public:
     end = end.next();
     const int selectionStart = cursor.selectionStart();
     const int endOfDocument = tempDocument->characterCount() - 1;
+    
     for (QTextBlock current = start; current.isValid() && current != end;
          current = current.next()) {
           const QTextLayout* layout(current.layout());
-        foreach (const QTextLayout::FormatRange& range,
-                  layout->formats()) {
-            const int startIdx = current.position() + range.start - selectionStart;
-            const int endIdx = startIdx + range.length;
-            if (endIdx <= 0 || startIdx >= endOfDocument)
-              continue;
-            tempCursor.setPosition(qMax(startIdx, 0));
-            tempCursor.setPosition(qMin(endIdx, endOfDocument),
-                                  QTextCursor::KeepAnchor);
-            tempCursor.setCharFormat(range.format);
-          
+          if(QT_VERSION==6)
+          {
+            QList<QTextLayout::FormatRange> formats = layout->formats(); // Get the current formats
+            for (const QTextLayout::FormatRange& range : formats) 
+            {
+              const int startIdx = current.position() + range.start - selectionStart;
+              const int endIdx = startIdx + range.length;
 
-      }     
+              // Check if the range is valid
+              if (endIdx <= 0 || startIdx >= endOfDocument)
+                  continue;
+
+              // Set the cursor position and apply the character format
+              tempCursor.setPosition(qMax(startIdx, 0));
+              tempCursor.setPosition(qMin(endIdx, endOfDocument), QTextCursor::KeepAnchor);
+              tempCursor.setCharFormat(range.format);
+            } 
+          }
+          else
+          {
+            foreach (const QTextLayout::FormatRange& range,
+                      layout->additionalformats()) {
+                const int startIdx = current.position() + range.start - selectionStart;
+                const int endIdx = startIdx + range.length;
+                if (endIdx <= 0 || startIdx >= endOfDocument)
+                  continue;
+                tempCursor.setPosition(qMax(startIdx, 0));
+                tempCursor.setPosition(qMin(endIdx, endOfDocument),
+                                      QTextCursor::KeepAnchor);
+                tempCursor.setCharFormat(range.format);
+            }
+              
+
+          }     
 
 
 
