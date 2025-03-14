dcf3e00f555b818af753bef3f14f8761a548d2f2
Perminder Singh <127239756+perminder-17@users.noreply.github.com>
2025-03-15 00:23:37 +0530

    not scaling the mesh.
    Signed-off-by: Perminder Singh <127239756+perminder-17@users.noreply.github.com>

--- avogadro/rendering/volume_box_vs.glsl
+++ avogadro/rendering/volume_box_vs.glsl
@@ -9,5 +9,5 @@ void main()
 {
   // Pass the box position along:
   vBoxPos = aPosition; // aPosition assumed in [-1..1]
-  gl_Position = uMVP * vec4(aPosition, 0.3);
+  gl_Position = uMVP * vec4(aPosition, 0.9);
 }
