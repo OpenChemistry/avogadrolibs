08dde3c0a1949294909bac0d7a9ac0da3085732f
Geoff Hutchison <geoff.hutchison@gmail.com>
2025-04-29 17:35:57 -0400

    Fix debugging statements
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- avogadro/io/xyzformat.cpp
+++ avogadro/io/xyzformat.cpp
@@ -89,15 +89,11 @@ bool XyzFormat::read(std::istream& inStream, Core::Molecule& mol)
   if (!buffer.empty())
     mol.setData("name", trimmed(buffer));
 
-  std::cout << " read comment " << buffer << std::endl;
-
   double energy = 0.0;
   std::vector<double> energies;
   if (findEnergy(buffer, energy)) {
     mol.setData("totalEnergy", energy);
     energies.push_back(energy);
-
-    std::cout << " read energy " << energy << std::endl;
   }
 
   // check for Lattice= in an extended XYZ from ASE and company
