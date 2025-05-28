832ae327057bbda280efa01e6848f9749c0bdf3a
Geoff Hutchison <geoff.hutchison@gmail.com>
2025-05-28 14:41:34 -0400

    Fix error
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- scripts/github-actions/repair-linux.sh
+++ scripts/github-actions/repair-linux.sh
@@ -10,7 +10,7 @@ sed -i s/^#.*baseurl=http/baseurl=https/g /etc/yum.repos.d/*.repo
 sed -i s/^mirrorlist=http/#mirrorlist=https/g /etc/yum.repos.d/*.repo
 
 urlgrabber -o ca-certificates.rpm \
- http://archive.kernel.org/centos-vault/centos/7.9.2009/updates/Source/SPackages/ca-certificates-2023.2.60_v>
+ http://archive.kernel.org/centos-vault/centos/7.9.2009/updates/Source/SPackages/ca-certificates-2023.2.60_v7.0.306-72.el7_9.src.rpm
 
 rpm -i ca-certificates.rpm
 
