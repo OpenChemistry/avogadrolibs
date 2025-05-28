ad610e025f7f8fdeef6890290a0eeccc1f19d8c5
Geoff Hutchison <geoff.hutchison@gmail.com>
2025-05-28 14:27:01 -0400

    Another try at the repair script
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- scripts/github-actions/repair-linux.sh
+++ scripts/github-actions/repair-linux.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 set -ev
+
 # This script is used to repair the Linux environment in GitHub Actions
+# CentOS 7 is EOL so mirror.centos.org is offline
+# https://serverfault.com/a/1161921
+
+sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
+sed -i s/^#.*baseurl=http/baseurl=https/g /etc/yum.repos.d/*.repo
+sed -i s/^mirrorlist=http/#mirrorlist=https/g /etc/yum.repos.d/*.repo
+
+urlgrabber -o ca-certificates.rpm \
+ http://archive.kernel.org/centos-vault/centos/7.9.2009/updates/Source/SPackages/ca-certificates-2023.2.60_v>
+
+rpm -i ca-certificates.rpm
+
+yum clean all ; yum makecache
+yum repolist
 yum install -y git eigen3-devel
