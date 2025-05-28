747de2f612ede2819f3adee865095fc8a1b16d51
Geoff Hutchison <geoff.hutchison@gmail.com>
2025-05-28 13:56:05 -0400

    Another attempt to fixup manylinux-2014 image
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- scripts/github-actions/repair-linux.sh
+++ scripts/github-actions/repair-linux.sh
@@ -4,8 +4,13 @@ set -ev
 # CentOS 7 is EOL so mirror.centos.org is offline
 # https://serverfault.com/a/1161921
 
-#sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
-#sed -i s/^#.*baseurl=http/baseurl=https/g /etc/yum.repos.d/*.repo
-#sed -i s/^mirrorlist=http/#mirrorlist=https/g /etc/yum.repos.d/*.repo
+sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
+sed -i s/^#.*baseurl=http/baseurl=https/g /etc/yum.repos.d/*.repo
+sed -i s/^mirrorlist=http/#mirrorlist=https/g /etc/yum.repos.d/*.repo
+
+urlgrabber -o ca-certificates.rpm \
+  http://archive.kernel.org/centos-vault/centos/7.9.2009/updates/Source/SPackages/ca-certificates-2023.2.60_v7.0.306-72.el7_9.src.rpm
+
+rpm -i ca-certificates.rpm
 
 yum install -y git eigen3-devel
