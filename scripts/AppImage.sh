284c70e9025fb20109819ffd97972a84865273c1
brockdyer03 <brockdyer03@gmail.com>
2026-03-21 14:01:19 -0400

    Remove X11 compatibility in AppImage
    Signed-off-by: brockdyer03 <brockdyer03@gmail.com>

--- scripts/AppImage.sh
+++ scripts/AppImage.sh
@@ -9,9 +9,4 @@ HERE="$(dirname "$(readlink -f "${0}")")"
 export LC_NUMERIC=C
 export LD_LIBRARY_PATH="${HERE}/usr/lib:${LD_LIBRARY_PATH}"
 
-# Only force xcb if on Wayland and user hasn't set a preference
-if [ -n "$WAYLAND_DISPLAY" ] && [ -z "$QT_QPA_PLATFORM" ]; then
-    export QT_QPA_PLATFORM=xcb
-fi
-
 exec "${HERE}/usr/bin/avogadro2" "$@"
