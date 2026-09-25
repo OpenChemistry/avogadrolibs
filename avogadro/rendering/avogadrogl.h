68e1f002b7f4145822a7200adf79a8f53c5de276
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2026-09-26 00:34:22 +0200

    Fix GLEW includes for macOS builds
    Signed-off-by: Tim Vandermeersch <tim.vandermeersch@gmail.com>

--- avogadro/rendering/avogadrogl.h
+++ avogadro/rendering/avogadrogl.h
@@ -14,11 +14,14 @@
 
 #ifdef __EMSCRIPTEN__
 #include <GLES3/gl3.h>
-#elif defined(__APPLE__)
-#include <OpenGL/gl.h>
 #else
 #include <GL/glew.h>
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
 #include <GL/gl.h>
+#endif
 #endif
 
 #endif // AVOGADRO_RENDERING_AVOGADROGL_H
